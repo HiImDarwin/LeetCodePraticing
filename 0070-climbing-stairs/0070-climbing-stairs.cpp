a9244486203105d44bfd63fa731f17df2bfcb254
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-05-25 22:12:32 +0800

    Time: 0 ms (100%), Space: 7.7 MB (86.92%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,30 @@
+//TOP-DOWN DP
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if(n==1) return 1;
+//         if(n==2) return 2;
+//         if (memo.find(n) ==memo.end()) {
+//             memo[n]=climbStairs(n-1)+climbStairs(n-2);
+//         }
+//         return memo[n];
+//     }
+// private:
+//     unordered_map<int,int> memo;
+// };
+
+
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n==1) return 1;
+        if(n==1)return 1;
         if(n==2) return 2;
-        if (memo.find(n) ==memo.end()) {
-            memo[n]=climbStairs(n-1)+climbStairs(n-2);
+        int method[n];
+        method[0]=1;
+        method[1]=2;
+        for(int i=2;i<n;i++) {
+            method[i] = method[i-1]+method[i-2];
         }
-        return memo[n];
+        return method[n-1];
     }
-private:
-    unordered_map<int,int> memo;
 };
