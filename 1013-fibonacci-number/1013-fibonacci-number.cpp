301c9006613745410f06493343c2331e81b3d638
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-05-26 17:04:54 +0800

    Time: 3 ms (64.64%), Space: 8.3 MB (12.08%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,17 +1,32 @@
+//top down
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if (n < 2) return n;
+//         vector<int> res(n+1,-1);
+//         res[0] = 0;
+//         res[1] = 1;
+//         return fib(n,res);
+//     }
+//     int fib(int n, vector<int> &arr) {
+//         if (arr[n-1] == -1) arr[n-1] = fib(n-1,arr);
+//         if (arr[n-2] == -1) arr[n-2] = fib(n-2,arr);
+//         arr[n] = arr[n-1]+arr[n-2];
+//         return arr[n];
+//     }
+// };
+
+//bottom up
 class Solution {
 public:
     int fib(int n) {
-        if (n==0) return 0;
-        if (n==1) return 1;
+        if(n<2) return n;
         vector<int> res(n+1,-1);
-        res[0] = 0;
-        res[1] = 1;
-        return fib(n,res);
-    }
-    int fib(int n, vector<int> &arr) {
-        if (arr[n-1] == -1) arr[n-1] = fib(n-1,arr);
-        if (arr[n-2] == -1) arr[n-2] = fib(n-2,arr);
-        arr[n] = arr[n-1]+arr[n-2];
-        return arr[n];
+        res[0]=0;
+        res[1]=1;
+        for(int i=2;i<n+1;i++) {
+            res[i] = res[i-1] + res[i-2];
+        }
+        return res[n];
     }
 };
