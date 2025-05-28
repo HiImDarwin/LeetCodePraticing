57bb647fc6eb76bcb26147d6835a7418bc406307
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-05-28 23:55:19 +0800

    Time: 0 ms (100%), Space: 8.3 MB (74.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
+    // Combination
+    // int uniquePaths(int m, int n) {
+    //     int a,b;
+    //     if(m>n){
+    //         a=m-1;
+    //         b=n-1;
+    //     } else {
+    //         a=n-1;
+    //         b=m-1;
+    //     }
+    //     double res=1;
+    //     for (int i=1;i<=b;i++) {
+    //         res =res*(a+i) /i ;
+    //     }
+    //     return (int)res;
+    // }
+
+    // d[i][j] = j[i-1][j] + d[i][j-1]
     int uniquePaths(int m, int n) {
-        int a,b;
-        if(m>n){
-            a=m-1;
-            b=n-1;
-        } else {
-            a=n-1;
-            b=m-1;
+        vector<int> dp(n,1);
+        for (int i=1; i<m; i++) {
+            for (int j=1; j<n; j++) {
+                dp[j] += dp[j-1];
+            }
         }
-        double res=1;
-        for (int i=1;i<=b;i++) {
-            res =res*(a+i) /i ;
-        }
-        return (int)res;
+        return  dp[n-1];
     }
 };
