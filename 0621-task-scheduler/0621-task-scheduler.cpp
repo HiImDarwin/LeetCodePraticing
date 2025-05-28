a756923eb8ec1904e385b7a3695ed852244f8f12
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-05-28 18:54:00 +0800

    Time: 3 ms (73.48%), Space: 38.3 MB (82.81%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,34 +1,52 @@
 class Solution {
 public:
+    // ***************heap***********************
     // use heap decrease value;
     // minus top n node each round until  heap got empty
     // if node number is not enough stall++;
-    int leastInterval(vector<char>& tasks, int n) {   
-        unordered_map<char,int> mp;
-        int stall_count = 0;
-        for (char &c : tasks) {
-            mp[c]++;
-        }
-        priority_queue<int> maxHp;
-        for (auto &node: mp) {
-            maxHp.push(node.second);
-        }
+    // int leastInterval(vector<char>& tasks, int n) {   
+    //     unordered_map<char,int> mp;
+    //     int stall_count = 0;
+    //     for (char &c : tasks) {
+    //         mp[c]++;
+    //     }
+    //     priority_queue<int> maxHp;
+    //     for (auto &node: mp) {
+    //         maxHp.push(node.second);
+    //     }
 
-        while(!maxHp.empty()){
-            vector<int> tmp;
-            for(int i=0;i<n+1;i++) {
-                if(!maxHp.empty()) {
-                    if(maxHp.top()>1) tmp.push_back(maxHp.top()-1);
-                    maxHp.pop();
-                } else if (!tmp.empty()) {
-                    stall_count++;
-                }
-            }
-            for(int &x: tmp) {
-                maxHp.push(x);
+    //     while(!maxHp.empty()){
+    //         vector<int> tmp;
+    //         for(int i=0;i<n+1;i++) {
+    //             if(!maxHp.empty()) {
+    //                 if(maxHp.top()>1) tmp.push_back(maxHp.top()-1);
+    //                 maxHp.pop();
+    //             } else if (!tmp.empty()) {
+    //                 stall_count++;
+    //             }
+    //         }
+    //         for(int &x: tmp) {
+    //             maxHp.push(x);
 
-            }
+    //         }
+    //     }
+    //     return tasks.size()+stall_count;
+    // }
+    //**************methmatic method******************
+    int leastInterval(vector<char>& tasks, int n) {
+
+        vector<int> map(26,0);
+        for( auto &c: tasks) {
+            map[c-'A']++;
         }
-        return tasks.size()+stall_count;
+        sort(map.begin(),map.end());
+        int maxFreq = map[25];
+        int maxFreqCount = 0;
+        for (int i=25; i>=0 && map[i] == maxFreq; i--) {
+            maxFreqCount++;
+        }
+        int minTime = (maxFreq-1)*(n+1)+maxFreqCount;
+        return max(minTime,(int)tasks.size());
+
     }
 };
