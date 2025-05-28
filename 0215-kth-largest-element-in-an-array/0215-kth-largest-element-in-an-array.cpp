09980766fdef88c7a114319ff35dbc60c9168148
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-05-28 16:32:17 +0800

    Time: 28 ms (65.99%), Space: 61.6 MB (42.78%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     //************* priority queue way ****************//
     //
-    // int findKthLargest(vector<int>& nums, int k) {
-    //     priority_queue<int> maxHp(nums.begin(),nums.end());
-    //     for(int i=1;i<k;i++) {
-    //         maxHp.pop();
-    //     } 
-    //     return maxHp.top();
-    // }
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> maxHp(nums.begin(),nums.end());
+        for(int i=1;i<k;i++) {
+            maxHp.pop();
+        } 
+        return maxHp.top();
+    }
     //*************** arrry heap *********************//
     // int findKthLargest(vector<int>& nums, int k) {
     //     for(int i = nums.size()/2-1;i>=0;i--) {
@@ -39,37 +39,38 @@ public:
     // }
 
     //*************** quick sort *********************//
-    int findKthLargest(vector<int>& nums, int k) {
-        int left = 0, right = nums.size()-1, kth;
-        while(true) {
-            int idx = partition(nums,left,right);
-            if(idx == k-1) {
-                kth = nums[k-1];
-                break;
-            }
-            if(idx < k-1) {
-                left = idx+1;
-            } else {
-                right = idx-1;
-            }
-        }
-        return kth;
+    // super slow
+//     int findKthLargest(vector<int>& nums, int k) {
+//         int left = 0, right = nums.size()-1, kth;
+//         while(true) {
+//             int idx = partition(nums,left,right);
+//             if(idx == k-1) {
+//                 kth = nums[k-1];
+//                 break;
+//             }
+//             if(idx < k-1) {
+//                 left = idx+1;
+//             } else {
+//                 right = idx-1;
+//             }
+//         }
+//         return kth;
         
-    }
+//     }
 
-private: 
-    int partition(vector<int>& nums, int left, int right) {
-        int pivot = nums[left];
-        int l = left+1;
-        int r = right;
-        while(l<=r) {
-            while(l <=r && nums[l]>=pivot) l++;
-            while(l <=r && nums[r]<pivot) r--;
-            if(l<r) {
-                swap(nums[l],nums[r]);
-            }
-        }
-        swap(nums[r],nums[left]);
-        return r;
-    }
+// private: 
+//     int partition(vector<int>& nums, int left, int right) {
+//         int pivot = nums[left];
+//         int l = left+1;
+//         int r = right;
+//         while(l<=r) {
+//             while(l <=r && nums[l]>=pivot) l++;
+//             while(l <=r && nums[r]<pivot) r--;
+//             if(l<r) {
+//                 swap(nums[l],nums[r]);
+//             }
+//         }
+//         swap(nums[r],nums[left]);
+//         return r;
+//     }
 };
