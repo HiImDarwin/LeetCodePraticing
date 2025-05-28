542dd8b3c39620f96df7f24b6b3ac54459552f40
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-05-28 15:13:23 +0800

    Time: 0 ms (100%), Space: 9.9 MB (76.99%) - LeetHub

---
+++ 1127-last-stone-weight/1127-last-stone-weight.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int N = stones.size();
+        for(int i=N/2-1;i>=0;i--) {
+            adjustDown(stones,i);
+        }
+
+        while(stones.size()>1) {
+            int fir = stones[0];
+            stones[0] = stones.back();
+            stones.pop_back();
+            adjustDown(stones,0);
+            int sec  =  stones[0];
+            if(fir == sec) {
+                stones[0] = stones.back();
+                stones.pop_back();
+                adjustDown(stones,0);
+            } else {
+                stones[0] = fir-sec;
+                adjustDown(stones,0);
+            }
+        }
+        return stones.empty() ? 0 : stones[0];
+
+        
+    }
+
+    void adjustDown( vector<int> &hp, int root){
+        int N = hp.size();
+        while(true) {
+            int biggest = root;
+            int l = root*2+1;
+            int r = root*2+2;
+            if(l<N && hp[l]>hp[biggest]) biggest = l;
+            if(r<N && hp[r]>hp[biggest]) biggest = r;
+            if(biggest == root) break;
+            swap(hp[root], hp[biggest]);
+            root = biggest;
+        }
+    }
+};
