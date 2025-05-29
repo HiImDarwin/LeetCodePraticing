ba3cd5e7d1a17f1f0e0dec03779cf2528368f900
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-05-30 02:13:46 +0800

    Time: 0 ms (100%), Space: 11.8 MB (5.33%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {
-        if(nums.size()==0) return 0;
-        int head=0;
-        int tail=nums.size()-1;
-        while(head<tail) {
-            while(head<tail && nums[tail]==val) {
-                tail--;
-            }
-            if(tail == head) break;
-            if(nums[head] == val) {
-                swap(nums[head],nums[tail]);
-            }
-            head++;
-        }
+    // int removeElement(vector<int>& nums, int val) {
+    //     if(nums.size()==0) return 0;
+    //     int head=0;
+    //     int tail=nums.size()-1;
+    //     while(head<tail) {
+    //         while(head<tail && nums[tail]==val) {
+    //             tail--;
+    //         }
+    //         if(tail == head) break;
+    //         if(nums[head] == val) {
+    //             swap(nums[head],nums[tail]);
+    //         }
+    //         head++;
+    //     }
         
-        return nums[head]==val ? head : head+1;
+    //     return nums[head]==val ? head : head+1;
+    // }
+    int removeElement(vector<int>& nums, int val) {
+        int index = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val) {
+                swap(nums[i],nums[index]);
+                index++;
+            }
+        }
+        return index;
     }
 };
