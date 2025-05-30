9b06d7629a539faa8603f47eb62aa529040cb80b
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-05-30 17:02:15 +0800

    Time: 4 ms (92.02%), Space: 51.6 MB (92.53%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,31 +1,59 @@
 class Solution {
 public:
+    //***************************reverse BFS ************************
+    // vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+    //     vector<vector<int>> reverse_adj(graph.size());
+    //     vector<int> indegree(graph.size());
+    //     for(int i=0;i<graph.size();i++) {
+    //         for(auto& node: graph[i]) {
+    //             reverse_adj[node].push_back(i);
+    //             indegree[i]++;
+    //         }
+    //     }
+    //     vector<int> res;
+    //     queue<int> qu;
+    //     for(int i=0;i<graph.size();i++) {
+    //         if(indegree[i] == 0)  qu.push(i);
+    //     }
+    //     while(!qu.empty()) {
+    //         int node = qu.front();
+    //         qu.pop();
+    //         res.push_back(node);
+    //         for(auto& i:reverse_adj[node] ){
+    //             indegree[i]--;
+    //             if(indegree[i]==0){
+    //                 qu.push(i);
+    //             }
+    //         }
+    //     }
+    //     sort(res.begin(),res.end());
+    //     return res;
+    // }
+
+    //*************************** DFS ************************
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        vector<vector<int>> reverse_adj(graph.size());
-        vector<int> indegree(graph.size());
-        for(int i=0;i<graph.size();i++) {
-            for(auto& node: graph[i]) {
-                reverse_adj[node].push_back(i);
-                indegree[i]++;
-            }
-        }
+        vector<bool> visited(graph.size(),false);
+        vector<bool> onPath(graph.size(),false);
         vector<int> res;
-        queue<int> qu;
         for(int i=0;i<graph.size();i++) {
-            if(indegree[i] == 0)  qu.push(i);
-        }
-        while(!qu.empty()) {
-            int node = qu.front();
-            qu.pop();
-            res.push_back(node);
-            for(auto& i:reverse_adj[node] ){
-                indegree[i]--;
-                if(indegree[i]==0){
-                    qu.push(i);
-                }
-            }
+            if(!visited[i]) findSafeNode(graph,i,visited,onPath,res);
         }
         sort(res.begin(),res.end());
         return res;
+    }
+
+    bool findSafeNode(vector<vector<int>>& graph,int node, vector<bool>& visited,vector<bool>& onPath,vector<int>& res) {
+        visited[node] = true;
+        onPath[node] = true;
+
+        for(int& neighbor:graph[node]) {
+            if(onPath[neighbor]) return false;
+            if(!visited[neighbor] && !findSafeNode(graph,neighbor,visited,onPath,res)){
+                return false;
+            }
+        }
+        onPath[node] = false;
+        res.push_back(node);
+        return true;
     }
 };
