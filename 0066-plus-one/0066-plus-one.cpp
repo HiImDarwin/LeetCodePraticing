b3b5150649384c106c1a87e2d8d4a8d1289712e0
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-05-30 14:34:20 +0800

    Time: 0 ms (100%), Space: 11.6 MB (5.57%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int i = digits.size()-1;
+        while(i>=0) {
+            if(digits[i]==9){
+                digits[i] = 0;
+                i--;
+            } else {
+                digits[i]++;
+                break;
+            }
+        }
+        if(i<0){
+            digits.insert(digits.begin(),1);
+        }
+        return digits;
+    }
+};
