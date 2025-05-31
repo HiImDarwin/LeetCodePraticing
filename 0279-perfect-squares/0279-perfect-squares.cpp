ea8e0f924317c71cc2b348c2f19a306051594df3
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-05-31 22:58:23 +0800

    Time: 43 ms (70.18%), Space: 13.3 MB (52.22%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        int x = (int) sqrt(n);
+        vector<int> squ_num(x);
+        for(int i=1;i<=x;i++){
+            squ_num[i-1]=i*i;
+        }
+
+        vector<int> res(n+1,INT_MAX);
+        res[0] = 0;
+        for(int i=1;i<n+1;i++) {
+            for(int num: squ_num) {
+                int index = i-num;
+                if(index<0) break;
+                if(res[index]+1 < res[i]) res[i]=res[index]+1;
+            }
+        }
+        return res[n];
+    }
+    
+};
