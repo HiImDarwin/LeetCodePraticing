84c73c604e27a30e9f55692ff6d5b78c5732627b
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-01 19:07:28 +0800

    Time: 0 ms (100%), Space: 7.8 MB (74.86%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
+    // bool isHappy(int n) {
+    //     unordered_set<int> sumUp;
+    //     while(true){
+    //         int tmp=0;
+    //         while(n>0){
+    //             int t=n%10;
+    //             n = n/10;
+    //             tmp = tmp+t*t;
+    //         }
+    //         if(tmp == 1) return true;
+    //         else if (!sumUp.insert(tmp).second) return false;
+    //         n = tmp;
+    //     }
+    //     return false;
+    // }
     bool isHappy(int n) {
-        unordered_set<int> sumUp;
-        while(true){
-            int tmp=0;
-            while(n>0){
-                int t=n%10;
-                n = n/10;
-                tmp = tmp+t*t;
-            }
-            if(tmp == 1) return true;
-            else if (!sumUp.insert(tmp).second) return false;
-            n = tmp;
+        int slowpointer = n;
+        int fastpointer = nextNum(n);
+        while( fastpointer != 1 && slowpointer != fastpointer) {
+            slowpointer = nextNum(slowpointer);
+            fastpointer = nextNum(nextNum(fastpointer));
         }
-        return false;
+        return fastpointer == 1;
+    }
+
+    int nextNum(int n ){
+        int tmp =0;
+        while(n>0){
+            int x = n%10;
+            n /=10;
+            tmp += x*x;
+        }
+        return tmp;
     }
 };
