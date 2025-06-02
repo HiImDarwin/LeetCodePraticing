7b7503a236a116315c0050dba21331b67608be87
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-03 01:52:36 +0800

    Time: 0 ms (100%), Space: 20.4 MB (62.5%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) {
-        int n= nums.size();
+    // DP
+    // Time Compiler O(N^2)
+    // int jump(vector<int>& nums) {
+    //     int n= nums.size();
         
-        vector<int> minJump(n,INT_MAX);
-        minJump[0]=0;
-        for(int i=0;i<n;i++) {
-            for(int j=1;j<=nums[i];j++){
-                if(i+j<n && minJump[i]+1 <minJump[i+j]){
-                    minJump[i+j] = minJump[i]+1;
-                }
+    //     vector<int> minJump(n,INT_MAX);
+    //     minJump[0]=0;
+    //     for(int i=0;i<n;i++) {
+    //         for(int j=1;j<=nums[i];j++){
+    //             if(i+j<n && minJump[i]+1 <minJump[i+j]){
+    //                 minJump[i+j] = minJump[i]+1;
+    //             }
+    //         }
+    //     }
+    //     return minJump[n-1];
+    // }
+
+    int jump(vector<int>& nums) {
+        int jump=0,end=0,farthest=0;
+        for(int i=0; i<nums.size()-1;i++){
+            farthest = max(farthest,i+nums[i]);
+            if(i==end){
+                jump++;
+                end = farthest;
+                if(end>nums.size()-1) break;
             }
+            
         }
-        return minJump[n-1];
+        return jump;
     }
+
 };
