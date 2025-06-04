9d0399ea180cadcf08d3362a22304a54770ee900
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-04 18:49:57 +0800

    Time: 0 ms (100%), Space: 14.9 MB (43.44%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -10,38 +10,59 @@
  */
 class Solution {
 public:
+    // ListNode* partition(ListNode* head, int x) {
+    //     if(!head) return nullptr;
+    //     queue<ListNode*> lesser;
+    //     ListNode *start=nullptr,*prev=nullptr;
+    //     while(head) {
+    //         if(head->val >= x){
+    //             if(!start) {
+    //                 start = head;
+    //                 prev = head;
+    //             } else {
+    //                 prev->next = head;
+    //                 prev = head;
+    //             }
+    //         }
+    //         if(head->val < x) lesser.push(head);
+    //         head = head->next;
+    //     }
+    //     if(start) prev->next = nullptr;
+
+    //     if(lesser.empty()) {
+    //         head = start;
+    //     } else {
+    //         head = lesser.front();
+    //         lesser.pop();
+    //         ListNode* tmp = head;
+    //         while(!lesser.empty()){
+    //             tmp->next = lesser.front();
+    //             lesser.pop();
+    //             tmp = tmp->next;
+    //         }
+    //         tmp->next = start;
+    //     }
+    //     return head;
+    // }
+
     ListNode* partition(ListNode* head, int x) {
-        if(!head) return nullptr;
-        queue<ListNode*> lesser;
-        ListNode *start=nullptr,*prev=nullptr;
+        ListNode dummySmall(0);
+        ListNode dummyLarge(0);
+        ListNode* small = &dummySmall;
+        ListNode* large = &dummyLarge;
+
         while(head) {
-            if(head->val >= x){
-                if(!start) {
-                    start = head;
-                    prev = head;
-                } else {
-                    prev->next = head;
-                    prev = head;
-                }
+            if(head->val < x) {
+                small->next = head;
+                small = small->next;
+             } else {
+                large->next =head;
+                large = large->next;
             }
-            if(head->val < x) lesser.push(head);
             head = head->next;
         }
-        if(start) prev->next = nullptr;
-
-        if(lesser.empty()) {
-            head = start;
-        } else {
-            head = lesser.front();
-            lesser.pop();
-            ListNode* tmp = head;
-            while(!lesser.empty()){
-                tmp->next = lesser.front();
-                lesser.pop();
-                tmp = tmp->next;
-            }
-            tmp->next = start;
-        }
-        return head;
+        large->next = nullptr;
+        small->next = dummyLarge.next;
+        return dummySmall.next;
     }
 };
