52563c30306c242581e14f69ad2bcd2aa435d3ea
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-04 16:51:33 +0800

    Time: 0 ms (100%), Space: 13.9 MB (90.99%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,30 +1,50 @@
 class Solution {
 public:
+    // vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    //     vector<int> res;
+    //     unordered_set<int> old;
+    //     int m = nums1.size();
+    //     int n = nums2.size();
+    //     if(m==0 || n ==0) return {};
+    //     vector<int> *shorter,*longer;
+    //     if(m<n){
+    //         shorter = &nums1;
+    //         longer = &nums2; 
+    //     } else {
+    //         shorter = &nums2;
+    //         longer = &nums1;
+    //     }
+    //     for(int i=0; i<shorter->size(); i++){
+    //         int val = (*shorter)[i];
+    //         if(old.count(val)) continue;
+    //         for(int j=0; j<longer->size(); j++){
+    //             if((*shorter)[i] == (*longer)[j]){
+    //                 old.insert((*shorter)[i]);
+    //                 res.push_back((*shorter)[i]);
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     return res;   
+    // }
+
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         vector<int> res;
-        unordered_set<int> old;
-        int m = nums1.size();
-        int n = nums2.size();
-        if(m==0 || n ==0) return {};
-        vector<int> *shorter,*longer;
-        if(m<n){
-            shorter = &nums1;
-            longer = &nums2; 
-        } else {
-            shorter = &nums2;
-            longer = &nums1;
-        }
-        for(int i=0; i<shorter->size(); i++){
-            int val = (*shorter)[i];
-            if(old.count(val)) continue;
-            for(int j=0; j<longer->size(); j++){
-                if((*shorter)[i] == (*longer)[j]){
-                    old.insert((*shorter)[i]);
-                    res.push_back((*shorter)[i]);
-                    break;
-                }
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int i=0;
+        int j=0;
+        while( i<nums1.size() && j<nums2.size() ) {
+            if(nums1[i] == nums2[j]){
+                res.push_back(nums1[i]);
+                while(++i<nums1.size() && nums1[i] == nums1[i-1]) {}
+                while(++j<nums2.size() && nums2[j]==nums2[j-1]){}
+            } else if(nums1[i]>nums2[j]){
+                while(++j<nums2.size() && nums2[j]==nums2[j-1]){}
+            } else {
+                while(++i<nums1.size() && nums1[i]==nums1[i-1]){}
             }
         }
-        return res;   
+        return res;
     }
 };
