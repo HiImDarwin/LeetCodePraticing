72e290c93b1eaf755dda096de667bb36f77b3a30
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-04 22:59:17 +0800

    Time: 7 ms (53.16%), Space: 15.1 MB (71.06%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -16,41 +16,62 @@ public:
 
 class Solution {
 public:
-    Node* copyRandomList(Node* head) {
-        if(!head) return head;
-        unordered_map<Node*,int> nodePool; // for new node
-        vector<Node*> linkPool; // for old node link
-        int i=0;
+    // Node* copyRandomList(Node* head) {
+    //     if(!head) return head;
+    //     unordered_map<Node*,int> nodePool; // for new node
+    //     vector<Node*> linkPool; // for old node link
+    //     int i=0;
 
-        //first round
-        Node* curr = head;
-        nodePool.insert({curr,i++});
-        Node* newHead = new Node(curr->val);
-        linkPool.push_back(newHead);
+    //     //first round
+    //     Node* curr = head;
+    //     nodePool.insert({curr,i++});
+    //     Node* newHead = new Node(curr->val);
+    //     linkPool.push_back(newHead);
         
         
-        Node* tmp = newHead;
-        curr =curr->next;
+    //     Node* tmp = newHead;
+    //     curr =curr->next;
+    //     while(curr) {
+    //         nodePool.insert({curr,i++});
+    //         tmp->next = new Node(curr->val);
+    //         tmp = tmp->next;
+    //         linkPool.push_back(tmp);
+    //         curr = curr->next;
+    //     }
+
+    //     curr = head;
+    //     tmp = newHead;
+    //     while(curr) {
+    //         if(!curr->random) {
+    //             tmp -> random = nullptr;
+    //         } else {
+    //             int index = nodePool[curr->random];
+    //             tmp->random = linkPool[index];
+    //         }
+    //         curr = curr-> next;
+    //         tmp = tmp->next;
+    //     }
+    //     return newHead;
+    // }
+
+    Node* copyRandomList(Node* head) {
+        if (!head) return nullptr;
+
+        unordered_map<Node*,Node*> nodeMap;
+        Node *curr = head;
         while(curr) {
-            nodePool.insert({curr,i++});
-            tmp->next = new Node(curr->val);
-            tmp = tmp->next;
-            linkPool.push_back(tmp);
+            nodeMap[curr] = new Node(curr->val);
             curr = curr->next;
         }
 
+        Node* newHead = nodeMap[head];
         curr = head;
-        tmp = newHead;
         while(curr) {
-            if(!curr->random) {
-                tmp -> random = nullptr;
-            } else {
-                int index = nodePool[curr->random];
-                tmp->random = linkPool[index];
-            }
-            curr = curr-> next;
-            tmp = tmp->next;
+            newHead -> next = nodeMap[curr->next];
+            newHead -> random = curr->random ? nodeMap[curr->random] : nullptr;
+            newHead = newHead->next;
+            curr = curr->next;
         }
-        return newHead;
+        return nodeMap[head];
     }
 };
