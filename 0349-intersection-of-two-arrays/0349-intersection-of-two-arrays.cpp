4cef3c11b52ddabcdd2b72ec25abb427db581b4e
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-04 16:56:12 +0800

    Time: 0 ms (100%), Space: 14.5 MB (30.77%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -28,23 +28,35 @@ public:
     //     return res;   
     // }
 
+    // time complexity O(MlogM+NlogN)
+
+    // vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    //     vector<int> res;
+    //     sort(nums1.begin(),nums1.end());
+    //     sort(nums2.begin(),nums2.end());
+    //     int i=0;
+    //     int j=0;
+    //     while( i<nums1.size() && j<nums2.size() ) {
+    //         if(nums1[i] == nums2[j]){
+    //             res.push_back(nums1[i]);
+    //             while(++i<nums1.size() && nums1[i] == nums1[i-1]) {}
+    //             while(++j<nums2.size() && nums2[j]==nums2[j-1]){}
+    //         } else if(nums1[i]>nums2[j]){
+    //             while(++j<nums2.size() && nums2[j]==nums2[j-1]){}
+    //         } else {
+    //             while(++i<nums1.size() && nums1[i]==nums1[i-1]){}
+    //         }
+    //     }
+    //     return res;
+    // }
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> res;
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        int i=0;
-        int j=0;
-        while( i<nums1.size() && j<nums2.size() ) {
-            if(nums1[i] == nums2[j]){
-                res.push_back(nums1[i]);
-                while(++i<nums1.size() && nums1[i] == nums1[i-1]) {}
-                while(++j<nums2.size() && nums2[j]==nums2[j-1]){}
-            } else if(nums1[i]>nums2[j]){
-                while(++j<nums2.size() && nums2[j]==nums2[j-1]){}
-            } else {
-                while(++i<nums1.size() && nums1[i]==nums1[i-1]){}
+        unordered_set<int> tmp1(nums1.begin(),nums1.end());
+        unordered_set<int> res;
+        for(int &x : nums2){
+            if(tmp1.count(x)) {
+                res.insert(x);
             }
         }
-        return res;
-    }
+        return vector<int> (res.begin(),res.end());
+    }   
 };
