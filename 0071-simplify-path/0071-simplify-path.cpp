6f9422d623cd3ec18c5fc8a27d8759742b3c4f4b
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-05 21:37:31 +0800

    Time: 4 ms (36.25%), Space: 10.9 MB (60.44%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,36 +1,55 @@
 class Solution {
 public:
+    // string simplifyPath(string path) {
+    //     vector<string> st;
+    //     int index = 0;
+    //     while(index < path.length()){
+    //         while(index < path.length() && path[index] == '/') {
+    //             index++;
+    //         }
+    //         if(index == path.length()) break;
+
+    //         int startPos=index;
+
+    //         while(index < path.length() && path[index] != '/') {
+    //             index++;
+    //         }
+    //         string part = path.substr(startPos,index-startPos);
+
+    //         if(part ==".") {
+    //             continue;
+    //         } else if (part == "..") {
+    //             if(!st.empty())st.pop_back();
+    //         } else {
+    //             st.push_back(part);
+    //         }
+    //     }
+    //     string res;
+
+    //     for(string x: st) {
+    //         res += "/";
+    //         res = res  + x;
+    //     }
+    //     return res.empty() ? "/" : res; 
+        
+    // }
+
     string simplifyPath(string path) {
         vector<string> st;
-        int index = 0;
-        while(index < path.length()){
-            while(index < path.length() && path[index] == '/') {
-                index++;
-            }
-            if(index == path.length()) break;
-
-            int startPos=index;
-
-            while(index < path.length() && path[index] != '/') {
-                index++;
-            }
-            string part = path.substr(startPos,index-startPos);
-
-            if(part ==".") {
-                continue;
-            } else if (part == "..") {
-                if(!st.empty())st.pop_back();
+        stringstream ss(path);
+        string comp;
+        while(getline(ss, comp, '/')) {
+            if(comp == "" || comp == ".") continue;
+            if(comp == ".."){
+                if(!st.empty()) st.pop_back();
             } else {
-                st.push_back(part);
+                st.push_back(comp);
             }
         }
-        string res;
-
-        for(string x: st) {
-            res += "/";
-            res = res  + x;
+        stringstream  res;
+        for( string &s :st) {
+            res << "/" << s;
         }
-        return res.empty() ? "/" : res; 
-        
+        return res.str().empty() ? "/" : res.str();
     }
 };
