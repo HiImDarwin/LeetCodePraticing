a64e20354d09f0a59a9cb0e7a1f6a04dbad89b33
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-06 03:29:15 +0800

    Time: 17 ms (79.92%), Space: 81.3 MB (80.09%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,40 +1,61 @@
 class Solution {
 public:
+    // int largestRectangleArea(vector<int>& heights) {
+    //     //monotonic increase
+    //     int n = heights.size();
+    //     int maximum = 0;
+    //     stack<int> st;
+    //     for(int i=0;i<n;i++) {
+    //         if(st.empty() || heights[i]>=st.top()) {
+    //             st.push(heights[i]);
+    //         } else {
+    //             maximum = max(maximum,cleanST(st,heights[i]));
+    //             st.push(heights[i]);
+    //         }
+    //     }
+    //     maximum = max(maximum,cleanST(st));
+    //     return maximum;
+    // }
+
+    // int cleanST(stack<int> &st,int val=-1) {
+    //     int maxi =0;
+    //     int count=0;
+    //     while(!st.empty() && st.top()>val) {
+    //         int x = st.top();
+    //         st.pop();
+    //         count++;
+    //         while(!st.empty() && st.top() == x) {
+    //             st.pop();
+    //             count++;
+    //         }
+    //         maxi = max(maxi,x*count);
+    //     }
+    //     if(val == -1) return maxi;
+    //     while(count>0){
+    //         st.push(val);
+    //         count--;
+    //     }
+    //     return maxi;
+    // }
+
+    //這題精妙的地方在於 他塞進stack的是index，因此很好查找寬度
     int largestRectangleArea(vector<int>& heights) {
         //monotonic increase
+        // when counter heights[i] < heights[st.top()] means can start cal the area
+        heights.push_back(0);
         int n = heights.size();
-        int maximum = 0;
         stack<int> st;
-        for(int i=0;i<n;i++) {
-            if(st.empty() || heights[i]>=st.top()) {
-                st.push(heights[i]);
-            } else {
-                maximum = max(maximum,cleanST(st,heights[i]));
-                st.push(heights[i]);
-            }
-        }
-        maximum = max(maximum,cleanST(st));
-        return maximum;
-    }
+        int maxArea = 0;
 
-    int cleanST(stack<int> &st,int val=-1) {
-        int maxi =0;
-        int count=0;
-        while(!st.empty() && st.top()>val) {
-            int x = st.top();
-            st.pop();
-            count++;
-            while(!st.empty() && st.top() == x) {
+        for(int i=0; i<n; ++i) {
+            while(!st.empty()&&heights[i]<heights[st.top()]) {
+                int h = heights[st.top()];
                 st.pop();
-                count++;
+                int width = st.empty() ? i: (i-1)-st.top();
+                maxArea = max(maxArea, h*width);
             }
-            maxi = max(maxi,x*count);
+            st.push(i);
         }
-        if(val == -1) return maxi;
-        while(count>0){
-            st.push(val);
-            count--;
-        }
-        return maxi;
+        return maxArea;
     }
 };
