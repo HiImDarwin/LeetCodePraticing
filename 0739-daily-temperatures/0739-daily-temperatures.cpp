97a5341d51198422312a969ae6ed1ce57608a451
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-06 21:29:09 +0800

    Time: 10 ms (95.1%), Space: 102.6 MB (98.41%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
+    // vector<int> dailyTemperatures(vector<int>& temperatures) {
 
-        int n = temperatures.size();
-        vector<int> res(n,0);
-        stack<int> st;
-        for(int i=0; i<n; i++) {
-            if(st.empty() || temperatures[i]<=temperatures[st.top()] ) {
-                st.push(i);
-            } else {
-                while(!st.empty() && temperatures[st.top()] < temperatures[i]){
-                    int index = st.top();
-                    st.pop();
-                    res[index] = i-index;
-                }
-                st.push(i);
+    //     int n = temperatures.size();
+    //     vector<int> res(n,0);
+    //     stack<int> st;
+    //     for(int i=0; i<n; i++) {
+    //         if(st.empty() || temperatures[i]<=temperatures[st.top()] ) {
+    //             st.push(i);
+    //         } else {
+    //             while(!st.empty() && temperatures[st.top()] < temperatures[i]){
+    //                 int index = st.top();
+    //                 st.pop();
+    //                 res[index] = i-index;
+    //             }
+    //             st.push(i);
+    //         }
+    //     }
+    //     return res;
+    // }
+
+    vector<int> dailyTemperatures(vector<int>& arr) {
+        int n=arr.size();
+        vector<int> st, ans(n, 0);
+        for(int i=n-1; i>=0; i--){
+            while(st.size() && arr[st.back()]<=arr[i])
+                st.pop_back();
+            if(st.size()){
+                ans[i]=st.back()-i;
             }
+            st.push_back(i);
         }
-        return res;
+        return ans;
     }
 };
