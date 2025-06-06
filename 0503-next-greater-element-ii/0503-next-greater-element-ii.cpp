82e00d0be92fa035d1941613d6dc497988579c78
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-07 01:18:09 +0800

    Time: 0 ms (100%), Space: 29 MB (16.42%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
+    // vector<int> nextGreaterElements(vector<int>& nums) {
+    //     int n = nums.size();
+    //     int i=0;
+    //     bool secRound =false;
+    //     vector<int> res(n,-1),st;
+        
+    //     for(int i=0;i<n*2;i++){
+    //         int idx = i%n;
+    //         while(!st.empty() && nums[idx]>nums[st.back()]){
+    //             int index = st.back();
+    //             st.pop_back();
+    //             res[index] = nums[idx];
+    //         }
+    //         if(i<n) st.push_back(i);            
+    //     }
+    //     return res;
+    // }
+
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n = nums.size();
-        int i=0;
-        bool secRound =false;
-        vector<int> res(n,-1),st;
-        
-        for(int i=0;i<n*2;i++){
-            int idx = i%n;
-            while(!st.empty() && nums[idx]>nums[st.back()]){
-                int index = st.back();
-                st.pop_back();
-                res[index] = nums[idx];
-            }
-            if(i<n) st.push_back(i);            
+        vector<int> ans(n,-1);
+        stack<int> st;
+        for(int i=n-1;i>=0;i--){
+            st.push(nums[i]);
         }
-        return res;
-    }
+        for(int i=n-1;i>=0;i--){
+       while( !st.empty() && st.top()<=nums[i]){
+             st.pop();
+
+       }
+       if( !st.empty() ) ans[i]=st.top();
+       st.push( nums[i] );
+       
+
+     }
+     return ans;
+     }
 };
