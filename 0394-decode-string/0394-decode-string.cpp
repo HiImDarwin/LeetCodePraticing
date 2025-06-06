258791c4ab3a9f35136be927a859c0a1fe5ac0e3
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-06 16:24:05 +0800

    Time: 2 ms (26.42%), Space: 9.2 MB (73.27%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,29 +1,55 @@
 class Solution {
 public:
+    // string decodeString(string s) {
+    //     int i=0;
+    //     return decode(s,i);
+    // }
+    // string decode(string &s, int &i) {
+    //     string res;
+    //     int num = 0;
+    //     while(i<s.size()) {
+    //         char c =s[i];
+
+    //         if(isdigit(c)) {
+    //             num = num*10+(c-'0');
+    //             i++;
+    //         } else if(c=='[') {
+    //             i++; //skip [
+    //             string inner = decode(s,i);
+    //             while(num-- > 0) res += inner;
+    //             num=0;
+    //         } else if(c==']') {
+    //             i++;
+    //             return res;
+    //         } else {
+    //             res+=c;
+    //             i++;
+    //         }
+    //     }
+    //     return res;
+    // }
+
     string decodeString(string s) {
-        int i=0;
-        return decode(s,i);
+        stringstream ss(s);
+        return decode(ss);
+    
     }
-    string decode(string &s, int &i) {
+
+    string decode(stringstream &ss) {
         string res;
         int num = 0;
-        while(i<s.size()) {
-            char c =s[i];
-
+        char c;
+        while(ss >> noskipws >> c) {
             if(isdigit(c)) {
                 num = num*10+(c-'0');
-                i++;
-            } else if(c=='[') {
-                i++; //skip [
-                string inner = decode(s,i);
-                while(num-- > 0) res += inner;
-                num=0;
-            } else if(c==']') {
-                i++;
+            } else if( c =='[') {
+                string inner = decode(ss);
+                while(num-- >0) res +=inner;
+                num =0;
+            } else if(c ==']'){
                 return res;
             } else {
-                res+=c;
-                i++;
+                res += c;
             }
         }
         return res;
