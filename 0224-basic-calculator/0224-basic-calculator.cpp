7b0433a64cd89d011eb7a819109ccc9218a759ff
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-07 02:49:24 +0800

    Time: 2 ms (50.65%), Space: 10.6 MB (75.86%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,93 +1,32 @@
 class Solution {
 public:
     int calculate(string s) {
-        stack<int> nums;
-        stack<char> ops;
-        int i = 0;
-        int n =s.length();
-        while(i < s.length()){
+        int res=0;
+        int num=0;
+        int sign =1;
+        stack<int> st;
+
+        for(int i=0; i<s.length(); ++i) {
             char c=s[i];
-            if(c == ' '){
-                i++;
-                continue;
+            if(isdigit(c)){
+                num = num*10+ (c-'0');
+            } else if( c=='+'||c=='-') {
+                res += sign*num;
+                num = 0;
+                sign = (c=='-') ? -1: 1;
+            } else if( c =='(') {
+                st.push(res);
+                st.push(sign);
+                res =0;
+                sign =1;
+            } else if( c==')') {
+                res += num*sign;
+                num = 0;
+                res *= st.top(); st.pop();
+                res += st.top(); st.pop();
             }
-
-            if (isdigit(c) || (c == '-' && isUnaryMinus(s, i))) {
-                int sign =1;
-                if(c == '-') {
-                    sign = -1;
-                    i++;
-                }
-                while (i < n && s[i] == ' ') i++;
-                if (i < n && isdigit(s[i])) {
-                    int num = 0;
-                    while(i<n && isdigit(s[i])) {
-                        num = num*10+(s[i]-'0');
-                        i++;
-                    }
-                    nums.push(sign*num);
-                } else if(i < n && s[i] == '(') {
-                    nums.push(0);
-                    ops.push('-');
-                }
-                continue;
-            }
-
-            if(c =='(') {
-                ops.push(c);
-            }else if (c == ')') {
-                while (!ops.empty() && ops.top() != '(') {
-                    computeTop(nums, ops);
-                }
-                ops.pop(); // pop '('
-            } else if(isOp(c)) {
-                while(!ops.empty() && precedence(ops.top())>= precedence(c)) {
-                    computeTop(nums,ops);
-                }
-                ops.push(c);
-            }
-            i++;
         }
-        while (!ops.empty()) {
-            computeTop(nums, ops);
-        }
-
-        return nums.top();
-           
-
-    }
-private:
-    bool isOp(char c) {
-        return c == '+' || c == '-' || c == '*';
-    }
-
-    int precedence(char op) {
-        if (op == '+' || op == '-') return 1;
-        if (op == '*') return 2;
-        return 0;
-    }
-
-    void computeTop(stack<int>& nums, stack<char>& ops) {
-        int b = nums.top(); nums.pop();
-        int a = nums.top(); nums.pop();
-        char op = ops.top(); ops.pop();
-        nums.push(applyOp(a, b, op));
-    }
-
-    int applyOp(int a, int b, char op) {
-        if (op == '+') return a + b;
-        if (op == '-') return a - b;
-        if (op == '*') return a * b;
-        return 0;
-    }
-    bool isUnaryMinus(const string& s, int i) {
-        
-        if (i == 0) return true;
-
-       
-        int j = i - 1;
-        while (j >= 0 && s[j] == ' ') j--;
-
-        return (j < 0 || s[j] == '(' || isOp(s[j]));
+        res += sign * num;
+        return res;
     }
 };
