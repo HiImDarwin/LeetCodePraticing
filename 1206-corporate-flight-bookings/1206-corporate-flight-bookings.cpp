c1b0b3a9940b42ec9650e3b85058e66938b91142
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-07 17:16:43 +0800

    Time: 29 ms (28.49%), Space: 78.6 MB (20.41%) - LeetHub

--- 1206-corporate-flight-bookings/1206-corporate-flight-bookings.cpp
+++ 1206-corporate-flight-bookings/1206-corporate-flight-bookings.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-        vector<int> res(n,0);
+        vector<int> res(n+1,0);
         for(auto x:bookings) {
             int i = x[0];
             int m = x[1];
             int num = x[2];
-            while(i<=m){
-                res[i-1] +=num;
-                i++;
-            }
+            res[i-1] += num;
+            res[m] -= num; 
         }
+        for(int i=1; i<n;i++) {
+            res[i] = res[i]+res[i-1];
+        }
+        res.pop_back();
         return res;
     }
 };
