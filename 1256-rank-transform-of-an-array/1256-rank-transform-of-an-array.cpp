c269c7ae71b12e377cddf7343c623f2683624275
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-09 20:20:14 +0800

    Time: 35 ms (86.12%), Space: 43.8 MB (63.3%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
+    // vector<int> arrayRankTransform(vector<int>& arr) {
+    //     map<int,vector<int>> bucket;
+    //     for(int i=0; i<arr.size();i++) {
+    //         if(bucket.count(arr[i])) {
+    //             bucket[arr[i]].push_back(i);
+    //         } else {
+    //             bucket[arr[i]] = vector<int>(1,i); 
+    //         }
+    //     }
+    //     auto bucketIt = bucket.begin();
+    //     int i=1;
+    //     while(bucketIt!= bucket.end()) {
+    //         for(int &idx : (*bucketIt).second ) {
+    //             arr[idx] = i;
+    //         }
+    //         bucketIt++;
+    //         i++;
+    //     }
+    //     return arr;
+    // }
+    
     vector<int> arrayRankTransform(vector<int>& arr) {
-        map<int,vector<int>> bucket;
-        for(int i=0; i<arr.size();i++) {
-            if(bucket.count(arr[i])) {
-                bucket[arr[i]].push_back(i);
-            } else {
-                bucket[arr[i]] = vector<int>(1,i); 
+        vector<int> rank = arr;
+        sort(rank.begin(),rank.end());
+        unordered_map<int,int> tmp;
+        int rankIdx=1;
+        for(int&x : rank) {
+            if(tmp.count(x) ==0) {
+                tmp[x] = rankIdx++;
             }
         }
-        auto bucketIt = bucket.begin();
-        int i=1;
-        while(bucketIt!= bucket.end()) {
-            for(int &idx : (*bucketIt).second ) {
-                arr[idx] = i;
-            }
-            bucketIt++;
-            i++;
+
+        for(int i =0;i<arr.size();i++) {
+            arr[i] = tmp[arr[i]];
         }
+
         return arr;
     }
 };
