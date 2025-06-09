1b150681eb46726664d992ade87414fb6d0d2bdb
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-09 19:41:12 +0800

    Time: 0 ms (100%), Space: 9 MB (99.85%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
+    // char findTheDifference(string s, string t) {
+    //     vector<int> bucket(26,0);
+
+    //     for(char &c:t ) {
+    //         bucket[c-'a']++;
+
+    //     }
+    //     for(char& c:s) {
+    //         bucket[c-'a']--;
+    //     }
+
+    //     for(int i=0;i<26;i++){
+    //         if(bucket[i]!=0) return i+'a';
+    //     }
+    //     return 'c'; // dummy
+    // }
+
+    // useing ascii num
     char findTheDifference(string s, string t) {
-        vector<int> bucket(26,0);
-
-        for(char &c:t ) {
-            bucket[c-'a']++;
-
+        int ans=0;
+        for(int i=0; i<s.size(); i++) {
+            ans -=s[i];
+            ans += t[i];
         }
-        for(char& c:s) {
-            bucket[c-'a']--;
-        }
-
-        for(int i=0;i<26;i++){
-            if(bucket[i]!=0) return i+'a';
-        }
-        return 'c';
+        ans+=t[s.size()];
+        return ans;
     }
 };
