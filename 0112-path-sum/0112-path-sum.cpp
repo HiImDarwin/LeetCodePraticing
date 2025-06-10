8975f9ce5ad7855c65a392da21f23f3292458409
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-10 21:45:04 +0800

    Time: 0 ms (100%), Space: 21.6 MB (5.28%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -11,23 +11,39 @@
  */
 class Solution {
 public:
-    bool hasPathSum(TreeNode* root, int targetSum) {
-        int sum=0;
-        if(!root) return false;
-        return DFS(root,targetSum,sum);
+    // bool hasPathSum(TreeNode* root, int targetSum) {
+    //     int sum=0;
+    //     if(!root) return false;
+    //     return DFS(root,targetSum,sum);
         
-    }
-    bool DFS(TreeNode* root, int targetSum, int &currSum){
-        bool result=false;
-        currSum += root->val;
-        if(!root->left&& !root->right) {
-            if(currSum == targetSum) return true;
-            currSum-= root->val;
-            return result;
+    // }
+    // bool DFS(TreeNode* root, int targetSum, int &currSum){
+    //     bool result=false;
+    //     currSum += root->val;
+    //     if(!root->left&& !root->right) {
+    //         if(currSum == targetSum) return true;
+    //         currSum-= root->val;
+    //         return result;
+    //     }
+    //     if(root->left) result |= DFS(root->left,targetSum,currSum);
+    //     if(root->right) result |= DFS(root->right,targetSum,currSum);
+    //     currSum-= root->val;
+    //     return result;
+    // }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(!root) return false;
+        stack<pair<TreeNode*, int>> st;
+        st.push({root,0});
+        while(!st.empty()){
+            auto [node,sum] = st.top();
+            st.pop();
+            sum+=node->val;
+            if(!node->left && !node->right) {
+                if(sum == targetSum) return true;
+            }
+            if(node->right) st.push({node->right,sum});
+            if(node->left)  st.push({node->left,sum});
         }
-        if(root->left) result |= DFS(root->left,targetSum,currSum);
-        if(root->right) result |= DFS(root->right,targetSum,currSum);
-        currSum-= root->val;
-        return result;
+        return false;
     }
 };
