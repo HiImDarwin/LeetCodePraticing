4a8c747efcc422713c9856be88bf5896a489dc66
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-10 19:38:51 +0800

    Time: 0 ms (100%), Space: 27 MB (92.12%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,19 +11,50 @@
  */
 class Solution {
 public:
+    // TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+    //     return buildTree(inorder,postorder,0,inorder.size()-1,0,inorder.size()-1);
+    // }
+    // TreeNode* buildTree(vector<int> &inorder, vector<int> &postorder,int ileft,int iright,int pleft,int pright) {
+    //     if(ileft >iright || pleft>pright) return nullptr;
+    //     if(ileft -iright ==0) return new TreeNode(inorder[ileft]);
+    //     int rootVal = postorder[pright];
+    //     int idx = ileft;
+    //     while(inorder[idx]!=rootVal && idx<=iright) idx++;
+    //     TreeNode* root = new TreeNode(rootVal);
+    //     int left_length = idx-ileft;
+    //     root->left = buildTree(inorder,postorder,ileft,idx-1,pleft,pleft+left_length-1);
+    //     root->right = buildTree(inorder,postorder,idx+1,iright,pleft+left_length,pright-1);
+    //     return root;
+    // }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        return buildTree(inorder,postorder,0,inorder.size()-1,0,inorder.size()-1);
-    }
-    TreeNode* buildTree(vector<int> &inorder, vector<int> &postorder,int ileft,int iright,int pleft,int pright) {
-        if(ileft >iright || pleft>pright) return nullptr;
-        if(ileft -iright ==0) return new TreeNode(inorder[ileft]);
-        int rootVal = postorder[pright];
-        int idx = ileft;
-        while(inorder[idx]!=rootVal && idx<=iright) idx++;
-        TreeNode* root = new TreeNode(rootVal);
-        int left_length = idx-ileft;
-        root->left = buildTree(inorder,postorder,ileft,idx-1,pleft,pleft+left_length-1);
-        root->right = buildTree(inorder,postorder,idx+1,iright,pleft+left_length,pright-1);
+        if(inorder.empty() || postorder.empty()) return nullptr;
+
+        int inIdx = inorder.size()-1;
+        int postIdx = postorder.size()-1;
+
+        TreeNode* root = new TreeNode(postorder[postIdx--]);
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(postIdx >=0) {
+            TreeNode* node = new TreeNode(postorder[postIdx]);
+            TreeNode *top = st.top();
+
+            if(top->val != inorder[inIdx]) {// 這行代表還有右子點
+                top->right =node;
+            } else {
+                while(!st.empty() && st.top()->val ==inorder[inIdx]) {
+                    top = st.top();
+                    st.pop();
+                    inIdx--;
+                }
+                top->left = node;
+            }
+            st.push(node);
+            postIdx--;
+        }
         return root;
     }
+
+
 };
