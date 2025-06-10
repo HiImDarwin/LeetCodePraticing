0f4633e3fa25322cf3d8c8b918222efb3b9b180c
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-11 01:22:17 +0800

    Time: 1 ms (62.27%), Space: 146.7 MB (73.83%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -11,18 +11,25 @@
  */
 class Solution {
 public:
+    // int minDepth(TreeNode* root) {
+    //     if(!root) return 0;
+    //     return DPS(root, 0);
+    // }
+
+    // int DPS(TreeNode* root, int deepth) {
+    //     deepth++;
+    //     int left = INT_MAX;
+    //     int right = INT_MAX;
+    //     if(!root->left && !root->right) return deepth;
+    //     if(root->left) left = DPS(root->left,deepth);
+    //     if(root->right) right = DPS(root->right, deepth);
+    //     return min(left,right);
+    // }
     int minDepth(TreeNode* root) {
         if(!root) return 0;
-        return DPS(root, 0);
-    }
-
-    int DPS(TreeNode* root, int deepth) {
-        deepth++;
-        int left = INT_MAX;
-        int right = INT_MAX;
-        if(!root->left && !root->right) return deepth;
-        if(root->left) left = DPS(root->left,deepth);
-        if(root->right) right = DPS(root->right, deepth);
-        return min(left,right);
+        if(!root->left && !root->right) return 1;
+        if(!root->left) return minDepth(root->right)+1;
+        if(!root->right) return minDepth(root->left)+1;
+        return min(minDepth(root->left)+1,minDepth(root->right)+1);
     }
 };
