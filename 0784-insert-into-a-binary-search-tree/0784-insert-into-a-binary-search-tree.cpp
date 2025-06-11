4451138c34609c7f9db059ba62ac4c300ddbdb52
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-11 17:03:15 +0800

    Time: 0 ms (100%), Space: 59.4 MB (44.92%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
@@ -11,18 +11,38 @@
  */
 class Solution {
 public:
+    // TreeNode* insertIntoBST(TreeNode* root, int val) {
+    //     if(!root) return new TreeNode(val);
+    //     insertion(root, val);
+    //     return root;
+    // }
+    // void insertion(TreeNode* root, int val) {
+    //     if(root->val >=val) {
+    //         if(!root->left) root->left = new TreeNode(val);
+    //         else insertion(root->left,val);
+    //     } else if(root->val <val ){
+    //         if (!root->right) root->right = new TreeNode(val);
+    //         else insertion(root->right,val);
+    //     }
+    // }
     TreeNode* insertIntoBST(TreeNode* root, int val) {
         if(!root) return new TreeNode(val);
-        insertion(root, val);
-        return root;
-    }
-    void insertion(TreeNode* root, int val) {
-        if(root->val >=val) {
-            if(!root->left) root->left = new TreeNode(val);
-            else insertion(root->left,val);
-        } else if(root->val <val ){
-            if (!root->right) root->right = new TreeNode(val);
-            else insertion(root->right,val);
+        TreeNode* tmp = root;
+        while(true) {
+            if(tmp->val > val){
+                if(!tmp->left) {
+                    tmp->left = new TreeNode(val);
+                    break;
+                }
+                tmp = tmp->left;
+            } else {
+                 if(!tmp->right) {
+                    tmp->right = new TreeNode(val);
+                    break;
+                }
+                tmp = tmp->right;
+            }
         }
+        return root;
     }
 };
