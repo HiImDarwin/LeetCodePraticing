f2a761adfd632b058ec5d0a3b1f965fab5b6640f
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-11 20:52:45 +0800

    Time: 13 ms (51.14%), Space: 15.4 MB (89.81%) - LeetHub

--- 0775-n-ary-tree-preorder-traversal/0775-n-ary-tree-preorder-traversal.cpp
+++ 0775-n-ary-tree-preorder-traversal/0775-n-ary-tree-preorder-traversal.cpp
@@ -20,16 +20,31 @@ public:
 
 class Solution {
 public:
+    // vector<int> preorder(Node* root) {
+    //     if(!root) return {};
+    //     vector<int> res;
+    //     preorder(root, res);
+    //     return res;
+    // }
+    // void preorder(Node* root, vector<int> &res) {
+    //     res.push_back(root->val);
+    //     for(auto &x: root->children){
+    //         preorder(x,res);
+    //     }
+    // }
     vector<int> preorder(Node* root) {
         if(!root) return {};
         vector<int> res;
-        preorder(root, res);
-        return res;
-    }
-    void preorder(Node* root, vector<int> &res) {
-        res.push_back(root->val);
-        for(auto &x: root->children){
-            preorder(x,res);
+        stack<Node*> st;
+        st.push(root);
+        while(!st.empty()) {
+            Node* tmp = st.top();
+            st.pop();
+            res.push_back(tmp->val);
+            for(int i=tmp->children.size()-1;i>=0;--i) {
+                st.push(tmp->children[i]);
+            }
         }
+        return res;
     }
 };
