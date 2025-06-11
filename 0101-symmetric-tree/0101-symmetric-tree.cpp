ea62821da2765f61ce320e87777b2bffc1c02e82
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-11 15:37:55 +0800

    Time: 0 ms (100%), Space: 18.3 MB (86.01%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,24 +11,34 @@
  */
 class Solution {
 public:
-    bool isSymmetric(TreeNode* root) {
-        if(!root) return true;
-        unordered_map<int,int> left,right;
-        leftChild(root->left,left,1);
-        rightChild(root->right,right,1);
-        return left == right;
-    }
+    // bool isSymmetric(TreeNode* root) {
+    //     if(!root) return true;
+    //     unordered_map<int,int> left,right;
+    //     leftChild(root->left,left,1);
+    //     rightChild(root->right,right,1);
+    //     return left == right;
+    // }
 
-    void leftChild(TreeNode* root, unordered_map<int,int> &mp,int num) {
-        if(!root) return;
-        mp[num] = root->val;
-        leftChild(root->left,mp,num*2);
-        leftChild(root->right,mp,num*2+1);
+    // void leftChild(TreeNode* root, unordered_map<int,int> &mp,int num) {
+    //     if(!root) return;
+    //     mp[num] = root->val;
+    //     leftChild(root->left,mp,num*2);
+    //     leftChild(root->right,mp,num*2+1);
+    // }
+    // void rightChild(TreeNode* root,unordered_map<int,int> &mp,int num){
+    //     if(!root) return;
+    //     mp[num] = root->val;
+    //     rightChild(root->left,mp,num*2+1);
+    //     rightChild(root->right,mp,num*2);
+    // }
+
+    bool isSymmetric(TreeNode* root) {
+        return isMirror(root->left,root->right);
     }
-    void rightChild(TreeNode* root,unordered_map<int,int> &mp,int num){
-        if(!root) return;
-        mp[num] = root->val;
-        rightChild(root->left,mp,num*2+1);
-        rightChild(root->right,mp,num*2);
+private:
+    bool isMirror(TreeNode* t1, TreeNode*t2) {
+        if(!t1 && !t2) return true;
+        if(!t1 || !t2) return false;
+        return (t1->val == t2->val) && isMirror(t1->left,t2->right) && isMirror(t1->right,t2->left);
     }
 };
