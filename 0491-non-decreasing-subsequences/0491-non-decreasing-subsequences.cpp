333f898bff2ca2e6e7d1f9108931ca3d38fa2e74
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-12 20:15:05 +0800

    Time: 19 ms (93.7%), Space: 26.4 MB (72.49%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -8,15 +8,14 @@ public:
     }
 
     void backtracking(vector<int>& nums, int start, vector<int> &tmp,vector<vector<int>> &res){
-        if(tmp.size()>=2) res.push_back(tmp);
-
-        
-        unordered_set<int> used;
-        for(int i=start; i<nums.size(); ++i) {
-            if((!tmp.empty() && nums[i]< tmp.back()) || used.count(nums[i])) continue;
-            used.insert(nums[i]);
+        if(tmp.size()>=2) res.push_back(tmp); 
+        unordered_set<int> st;
+        for(int i=start;i<nums.size();i++){
+            if(st.count(nums[i])) continue;
+            if(!tmp.empty() && nums[i]<tmp.back()) continue;
+            st.insert(nums[i]);
             tmp.push_back(nums[i]);
-            backtracking(nums, i+1,tmp,res);
+            backtracking(nums,i+1,tmp,res);
             tmp.pop_back();
         }
     }
