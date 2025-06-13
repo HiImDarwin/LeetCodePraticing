c5efda811507dcc57c8c06facffbcac58f9ddf4c
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-13 18:19:56 +0800

    Time: 0 ms (100%), Space: 10.8 MB (45.07%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> res;
+        vector<string> sol;
+        backtracking(s,0,res,sol,0);
+        return res;
+    }
+
+    void backtracking(string s, int start, vector<string> &res ,vector<string> &sol,int dotCount) {
+        if(start == s.length() && dotCount == 4) {
+            res.push_back(sol[0]+"."+sol[1]+"."+sol[2]+"."+sol[3]);
+            return;
+        }
+        if(dotCount > 3 || start >= s.length()) return;
+        string tmp;
+        for(int i=start; i<s.length() && i<start+3 ;++i) {
+            tmp += s[i];
+            if((tmp.length()>1 && tmp[0] == '0') || stoi(tmp) >255 ) break;
+            sol.push_back(tmp);
+            backtracking(s,i+1,res,sol,dotCount+1);
+            sol.pop_back();
+        }
+    }
+};
