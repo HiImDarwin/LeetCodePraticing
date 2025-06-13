3692961979b3fb02f17c4e82ee95c4629d1254df
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-14 00:30:21 +0800

    Time: 0 ms (100%), Space: 8.7 MB (28.09%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
+    // bool isPalindrome(int x) {
+    //     string tmp = to_string(x);
+    //     int left =0,right = tmp.length()-1;
+    //     while(left<=right){
+    //         if(tmp[left++] != tmp[right--]) return false;
+    //     }
+    //     return true;
+    // }
     bool isPalindrome(int x) {
-        string tmp = to_string(x);
-        int left =0,right = tmp.length()-1;
-        while(left<=right){
-            if(tmp[left++] != tmp[right--]) return false;
+        if (x<0 || (x%10==0 && x!=0)) return false;
+        if (x<10) return true;
+        int y =0;
+        while (y<x) {
+            y =y*10+x%10;
+            x/=10;
         }
-        return true;
+        return (y == x) || (y/10 ==x);
+    
     }
 };
