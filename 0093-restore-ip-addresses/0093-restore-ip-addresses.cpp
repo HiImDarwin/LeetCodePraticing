2023903ab6c0e38dc4d4caa659844da31c3c64f8
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-13 18:25:40 +0800

    Time: 0 ms (100%), Space: 10.6 MB (62.77%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -2,14 +2,31 @@ class Solution {
 public:
     vector<string> restoreIpAddresses(string s) {
         vector<string> res;
-        vector<string> sol;
-        backtracking(s,0,res,sol,0);
+        //vector<string> sol;
+        //backtracking(s,0,res,sol,0);
+        string path;
+        backtracking(s,0,res,path,0);
         return res;
     }
 
-    void backtracking(string s, int start, vector<string> &res ,vector<string> &sol,int dotCount) {
+    // void backtracking(string s, int start, vector<string> &res ,vector<string> &sol,int dotCount) {
+    //     if(start == s.length() && dotCount == 4) {
+    //         res.push_back(sol[0]+"."+sol[1]+"."+sol[2]+"."+sol[3]);
+    //         return;
+    //     }
+    //     if(dotCount > 3 || start >= s.length()) return;
+    //     string tmp;
+    //     for(int i=start; i<s.length() && i<start+3 ;++i) {
+    //         tmp += s[i];
+    //         if((tmp.length()>1 && tmp[0] == '0') || stoi(tmp) >255 ) break;
+    //         sol.push_back(tmp);
+    //         backtracking(s,i+1,res,sol,dotCount+1);
+    //         sol.pop_back();
+    //     }
+    // }
+    void backtracking(string s, int start, vector<string> &res ,string &path,int dotCount) {
         if(start == s.length() && dotCount == 4) {
-            res.push_back(sol[0]+"."+sol[1]+"."+sol[2]+"."+sol[3]);
+            res.push_back(path.substr(0,path.length()-1));
             return;
         }
         if(dotCount > 3 || start >= s.length()) return;
@@ -17,9 +34,10 @@ public:
         for(int i=start; i<s.length() && i<start+3 ;++i) {
             tmp += s[i];
             if((tmp.length()>1 && tmp[0] == '0') || stoi(tmp) >255 ) break;
-            sol.push_back(tmp);
-            backtracking(s,i+1,res,sol,dotCount+1);
-            sol.pop_back();
+            int len = path.length();
+            path += tmp +'.';
+            backtracking(s,i+1,res,path,dotCount+1);
+            path.erase(len);
         }
     }
 };
