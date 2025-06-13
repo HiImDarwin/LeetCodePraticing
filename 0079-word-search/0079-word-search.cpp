82e414c252d84e8f02637596a92e6c1a11d8a317
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-13 22:01:07 +0800

    Time: 194 ms (83.54%), Space: 10.5 MB (86.89%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        vector<vector<bool>> used(board.size(),vector<bool> (board[0].size(),false));
         bool res = false;
         for(int i=0; i<board.size(); ++i) {
             for( int j =0; j<board[0].size(); ++j){
-                if(board[i][j] == word[0]) res=trace(board,word,i,j,0,used);
+                if(board[i][j] == word[0]) res=trace(board,word,i,j,0);
                 if(res) return true;
             }
         } 
         return false;
     }
-    bool trace(vector<vector<char>>& board, string &word,int i,int j,int index, vector<vector<bool>> &used){
+    bool trace(vector<vector<char>>& board, string &word,int i,int j,int index){
 
         if(index==word.length()) return true;
         if(i<0 || i>=board.size() || j<0 || j>=board[0].size()) return false;
-        if(used[i][j] || board[i][j]!=word[index]) return false;
-        used[i][j] = true;
-        bool res =  trace(board,word,i+1,j,index+1,used) ||
-                    trace(board,word,i-1,j,index+1,used) ||
-                    trace(board,word,i,j+1,index+1,used) ||
-                    trace(board,word,i,j-1,index+1,used);
-        used[i][j] =false;
+        if(board[i][j]!=word[index]) return false;
+        char c=board[i][j];
+        board[i][j] = '#';
+        bool res =  trace(board,word,i+1,j,index+1) ||
+                    trace(board,word,i-1,j,index+1) ||
+                    trace(board,word,i,j+1,index+1) ||
+                    trace(board,word,i,j-1,index+1);
+        board[i][j] =c;
         return res;
     }
 };
