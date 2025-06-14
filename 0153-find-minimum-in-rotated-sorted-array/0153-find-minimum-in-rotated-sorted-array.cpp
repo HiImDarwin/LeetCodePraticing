17cd768394753c4e3a4870164ff36b74e503b62d
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-14 23:29:22 +0800

    Time: 0 ms (100%), Space: 14 MB (79.15%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    int findMin(vector<int>& nums) {
-        int n = nums.size();
-        int left =0;
-        int right = n;
-        while(left < right) {
-            int mid = left + (right-left)/2;
-            if(nums[mid] > nums[left]) left =mid;
-            else right = mid;
-        }
-        return nums[(left+1)%n];
-    }
+    // this way is find the pivot point
     // int findMin(vector<int>& nums) {
-    //     int right = nums.size()-1;
-    //     int left = 0;
+    //     int n = nums.size();
+    //     int left =0;
+    //     int right = n;
     //     while(left < right) {
-    //         int mid = left+(right-left)/2;
-
-    //         if(nums[mid]>nums[right]) {
-    //             left = mid+1;
-    //         } else {
-    //             right = mid;
-    //         }
+    //         int mid = left + (right-left)/2;
+    //         if(nums[mid] > nums[left]) left =mid;
+    //         else right = mid;
     //     }
-    //     return nums[left];
+    //     return nums[(left+1)%n];
     // }
+    int findMin(vector<int>& nums) {
+        int right = nums.size()-1;
+        int left = 0;
+        while(left < right) {
+            int mid = left+(right-left)/2;
+
+            if(nums[mid]>nums[right]) {
+                left = mid+1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
 };
