0ffd5eacd34a11b597fb11edff8770015bce6d81
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-14 17:49:04 +0800

    Time: 0 ms (100%), Space: 63.3 MB (99.97%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
+    // int peakIndexInMountainArray(vector<int>& arr) {
+    //     int right = arr.size()-1;
+    //     int left =0;
+    //     while(left <= right) {
+    //         int mid = left+(right-left)/2;
+    //         if((mid == arr.size()-1 || arr[mid] > arr[mid+1]) && (mid ==0 || arr[mid] >arr[mid-1])) return mid;
+    //         else if((mid ==0 || arr[mid-1] < arr[mid]) &&  arr[mid] < arr[mid+1]) { //accessending
+    //             left = mid+1;
+    //         } else if ( arr[mid-1] >arr[mid] && (mid == arr.size()-1  ||arr[mid] > arr[mid+1])) { //decessending
+    //             right = mid-1;
+    //         }
+    //     }
+    //     return -1; //dummy 
+    // }
+
     int peakIndexInMountainArray(vector<int>& arr) {
         int right = arr.size()-1;
-        int left =0;
-        while(left <= right) {
-            int mid = left+(right-left)/2;
-            if((mid == arr.size()-1 || arr[mid] > arr[mid+1]) && (mid ==0 || arr[mid] >arr[mid-1])) return mid;
-            else if((mid ==0 || arr[mid-1] < arr[mid]) &&  arr[mid] < arr[mid+1]) { //accessending
+        int left = 0;
+        
+        while (left <right) {
+            int mid = left +(right-left)/2;
+            if(arr[mid] < arr[mid+1]) {
                 left = mid+1;
-            } else if ( arr[mid-1] >arr[mid] && (mid == arr.size()-1  ||arr[mid] > arr[mid+1])) { //decessending
-                right = mid-1;
+            } else {
+                right = mid;
             }
         }
-        return -1; //dummy 
+        return left;
     }
 };
