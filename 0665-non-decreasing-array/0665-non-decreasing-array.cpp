1ded72a321872459cc83381d513e0f4480bd6ea6
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-16 04:23:17 +0800

    Time: 0 ms (100%), Space: 30.9 MB (40.41%) - LeetHub

---
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+      bool modified = false;
+      int idx=-1;
+      for(int i=0; i < nums.size()-1; ++i) {
+        if(nums[i+1] < nums[i]) {
+          if(modified) return false;
+          modified = true;
+          idx = i+1;
+        }
+      }
+      if(modified && 1<idx && idx <nums.size()-1 && nums[idx-1] > nums[idx+1] && nums[idx-2]>nums[idx])
+        return false;
+      return true; 
+    }
+};
