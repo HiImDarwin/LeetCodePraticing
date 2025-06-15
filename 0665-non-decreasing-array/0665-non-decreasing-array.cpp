4b59a08223328dba8e96e107d04213a9ef5c1a3a
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-16 04:31:47 +0800

    Time: 0 ms (100%), Space: 30.8 MB (40.41%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.cpp
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
+    // bool checkPossibility(vector<int>& nums) {
+    //   bool modified = false;
+    //   int idx=-1;
+    //   for(int i=0; i < nums.size()-1; ++i) {
+    //     if(nums[i+1] < nums[i]) {
+    //       if(modified) return false;
+    //       modified = true;
+    //       idx = i+1;
+    //     }
+    //   }
+    //   if(modified && 1<idx && idx <nums.size()-1 && nums[idx-1] > nums[idx+1] && nums[idx-2]>nums[idx])
+    //     return false;
+    //   return true; 
+    // }
     bool checkPossibility(vector<int>& nums) {
       bool modified = false;
-      int idx=-1;
-      for(int i=0; i < nums.size()-1; ++i) {
-        if(nums[i+1] < nums[i]) {
-          if(modified) return false;
+      for (int i = 0; i < nums.size() - 1; ++i) {
+        if (nums[i] > nums[i + 1]) {
+          if (modified) return false;
           modified = true;
-          idx = i+1;
+
+          if( i > 0 && i < nums.size()-1 ) {
+            if (nums[i-1]>nums[i+1]) {
+              nums[i+1] = nums[i];
+            } else {
+              nums[i] = nums[i-1];
+            }
+          }
         }
       }
-      if(modified && 1<idx && idx <nums.size()-1 && nums[idx-1] > nums[idx+1] && nums[idx-2]>nums[idx])
-        return false;
-      return true; 
+      return true;
     }
+
 };
