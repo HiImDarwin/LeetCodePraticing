8d0792047ac9fac98d77e1efe48ca0fad361de3b
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-17 14:50:52 +0800

    Time: 0 ms (100%), Space: 7.8 MB (53.85%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     bool judgeSquareSum(int c) {
         if(c == 1) return true;
-        int left = 0;
-        int right = sqrt(c);
+        long long left = 0;
+        long long right = sqrt(c);
         while(left <= right) {
-            long long sumUp = 1LL * left * left + 1LL * right * right;
+            long long sumUp = left * left + right * right;
             if(sumUp == c) return true;
             else if( sumUp > c) {
               right--;
