4998071f38cdecffae2e0b69ab87e96f528522d1
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-17 16:18:54 +0800

    Time: 29 ms (12.79%), Space: 19 MB (92.14%) - LeetHub

--- 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.cpp
+++ 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.cpp
@@ -1,26 +1,18 @@
 class Solution {
   public:
     string findLongestWord(string s, vector<string>& dictionary) {
-      int maxLenght = 0;
-      string ans;
+      string ans="";
       for(string &word: dictionary) {
-        if(isSubsequence(s,word)) {
-          if(word.length() > ans.length() || 
-            (word.length() == ans.length() &&  word < ans)) {
-            ans = word;
-          }
+        int w_idx = 0, s_idx = 0;
+        while(w_idx < word.length() && s_idx < s.length()) {
+          if(s[s_idx] == word[w_idx]) ++w_idx;
+          ++s_idx;
+        }
+        if( w_idx == word.length() && 
+            (word.length()> ans.length() || (word.length() == ans.length() && word < ans)) ) {
+          ans = word;
         }
       }
       return ans;
-    }
-    bool isSubsequence (const string& s, const string& word) {
-      int s_index = 0, w_index = 0;
-      while(s_index < s.length() && w_index < word.length()) {
-        if(s[s_index] == word[w_index]) {
-          ++w_index;
-        }
-        ++s_index;
-      }
-      return w_index == word.length();
     }
 };
