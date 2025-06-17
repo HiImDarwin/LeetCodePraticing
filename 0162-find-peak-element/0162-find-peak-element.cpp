4aeb5892e46c4a20621fb50b94bac68b2c3c925c
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-17 17:41:28 +0800

    Time: 0 ms (100%), Space: 12.3 MB (92.12%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,27 +1,15 @@
 class Solution {
   public: 
     int findPeakElement(vector<int>& nums) {
-    int n = nums.size();
-    if (n == 1) {
-        return 0;
-    }
-    if (nums[0] > nums[1]) {
-        return 0;
-    }
-    if (nums[n - 1] > nums[n - 2]) {
-        return n - 1;
-    }
-    int l = 1, r = n - 2, mid;
-    while (l <= r) {
-        mid = l + (r - l) / 2;
-        if (nums[mid] > nums[mid + 1] && nums[mid] > nums[mid - 1]) {
-            return mid;
-        } else if (nums[mid] > nums[mid - 1]) {
-            l = mid + 1;
+      int left = 0, right = nums.size() - 1;
+      while(left < right) {
+        int mid = left + (right - left) / 2;
+        if( nums[mid] < nums[mid+1]) {// 右邊一定至少有一個peak
+          left = mid+1;
         } else {
-            r = mid - 1;
+          right = mid;
         }
+      }
+      return left;
     }
-    return -1;
-}
 };
