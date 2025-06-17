285127aec17bffd71312bc91033f50d602e4cfa5
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-17 16:46:46 +0800

    Time: 0 ms (100%), Space: 17.6 MB (52.76%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,67 +1,35 @@
 class Solution {
 public:
-    // vector<int> searchRange(vector<int>& nums, int target) {
-    //     int start=0,end=nums.size()-1,mid;
-    //     while (start<=end) {
-    //         mid = start+(end-start)/2;
-    //         if(nums[mid] == target) {
-    //             break;
-    //         } else if(nums[mid] > target) {
-    //             end = mid-1;
-    //         } else {
-    //             start = mid+1;
-    //         }
-    //     }
-    //     if(start > end) return {-1,-1} ;
-    //     start = mid;
-    //     end = mid;
-    //     while(start >=0 && nums[mid] == nums[start] ){start--;}
-    //     while(end<nums.size() && nums[mid] == nums[end]){end++;}
-    //     return {start+1,end-1};
-    // }
-
-    // two BinarySearch
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first = findfirst(nums,target);
-        int last = findlast(nums,target);
-        return {first,last};
+      if(nums.size() == 0) return {-1,-1};
+      vector<int> res(2,-1);
+      res[0] = lowerBound(nums, target);
+      res[1] = upperBound(nums, target);
+      return res;
     }
 
-    int findfirst(vector<int>& nums, int target) {
-        int first = 0;
-        int last = nums.size()-1;
-        int mid;
-        int res = -1;
-        while(first<=last) {
-            mid = first +(last-first)/2;
-            if(nums[mid] < target) {
-                first = mid+1;
-            } else {
-                if(nums[mid] == target) {
-                    res = mid;
-                }
-                last = mid-1;
-            }
+    int lowerBound (vector<int>& nums, int target) {
+      int left = 0, right = nums.size()-1;
+      while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] < target) {
+          left = mid + 1;
+        } else {
+          right  = mid;
         }
-        return res;
+      }
+      return nums[left] == target ? left : -1 ;
     }
-
-    int findlast(vector<int>& nums, int target) {
-        int first = 0;
-        int last = nums.size()-1;
-        int mid;
-        int res = -1;
-        while(first<=last) {
-            mid = first +(last-first)/2;
-            if(nums[mid] > target) {
-                last = mid-1;
-            } else {
-                if(nums[mid] == target) {
-                    res = mid;
-                }
-                first = mid+1;
-            }
+    int upperBound (vector<int>& nums, int target) {
+      int left = 0, right = nums.size()-1;
+      while (left < right) {
+        int mid = right - (right - left) / 2;
+        if( nums[mid] > target ){
+          right = mid-1;
+        } else {
+          left = mid;
         }
-        return res;
+      }
+      return nums[right] == target ? right : -1;
     }
 };
