b6d37ac50f9e4894c8d8123f8381aafc4e568ebd
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-21 21:43:08 +0800

    Time: 183 ms (94.75%), Space: 10.4 MB (94.63%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,27 +1,31 @@
 class Solution {
-public:
+  public:
     bool exist(vector<vector<char>>& board, string word) {
-        bool res = false;
-        for(int i=0; i<board.size(); ++i) {
-            for( int j =0; j<board[0].size(); ++j){
-                if(board[i][j] == word[0]) res=trace(board,word,i,j,0);
-                if(res) return true;
-            }
-        } 
-        return false;
+      for(int i = 0; i < board.size(); ++i) {
+        for(int j = 0; j < board[0].size(); ++j) {
+          if(board[i][j] == word[0] && DFS(board, word, i, j, 0)) 
+            return true;
+        }
+      }
+      return false;
     }
-    bool trace(vector<vector<char>>& board, string &word,int i,int j,int index){
+  private:
+    bool DFS(vector<vector<char>>& board, string& word, int xIdx, int yIdx, int index) {
+      if(xIdx <0 || xIdx == board.size() || yIdx < 0 || yIdx == board[0].size()) return false;
+      if(board[xIdx][yIdx] == word[index]) {
+        if (index == word.size()-1) return true;
+      } else {
+        return false;
+      }
+      char c = board[xIdx][yIdx] ;
+      board[xIdx][yIdx] = '#';
 
-        if(index==word.length()) return true;
-        if(i<0 || i>=board.size() || j<0 || j>=board[0].size()) return false;
-        if(board[i][j]!=word[index]) return false;
-        char c=board[i][j];
-        board[i][j] = '#';
-        bool res =  trace(board,word,i+1,j,index+1) ||
-                    trace(board,word,i-1,j,index+1) ||
-                    trace(board,word,i,j+1,index+1) ||
-                    trace(board,word,i,j-1,index+1);
-        board[i][j] =c;
-        return res;
+      bool result = false;
+      result = DFS(board,word,xIdx+1,yIdx,index+1) ||
+               DFS(board,word,xIdx-1,yIdx,index+1) ||
+               DFS(board,word,xIdx,yIdx+1,index+1) ||
+               DFS(board,word,xIdx,yIdx-1,index+1);
+      board[xIdx][yIdx] = c;
+      return result;
     }
 };
