1811c75e17e511886f433eaf34bd3a4bd71396b5
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-23 04:22:03 +0800

    Time: 0 ms (100%), Space: 9.8 MB (71.85%) - LeetHub

--- 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.cpp
+++ 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
-    vector<string> divideString(string s, int k, char fill) {
-        vector<string> res;
-        int startIdx = 0;
-        while (startIdx < s.length()) {
-            string tmp;
-            if ((startIdx + k) > s.length() ) {
-                    tmp = s.substr(startIdx);
-                    while (tmp.size() < k) {
-                        tmp += fill;
-                    }
-                }
-            else {
-                tmp = s.substr(startIdx, k);
-            }
-            res.push_back(tmp);
-            startIdx += k;
-        }
-        return res;
+    // vector<string> divideString(string s, int k, char fill) {
+    //     vector<string> res;
+    //     int startIdx = 0;
+    //     while (startIdx < s.length()) {
+    //         string tmp;
+    //         if ((startIdx + k) > s.length() ) {
+    //                 tmp = s.substr(startIdx);
+    //                 while (tmp.size() < k) {
+    //                     tmp += fill;
+    //                 }
+    //             }
+    //         else {
+    //             tmp = s.substr(startIdx, k);
+    //         }
+    //         res.push_back(tmp);
+    //         startIdx += k;
+    //     }
+    //     return res;
+    // }
+  vector<string> divideString(string s, int k, char fill) {
+    vector<string> res;
+    for(int i = 0; i< s.length(); i+=k) {
+      string tmp = "";
+      for(int j = 0; j < k; ++j) {
+        tmp += (i+j)<s.length() ? s[i+j] : fill;
+      }
+      res.push_back(tmp);
     }
+    return res;
+  }
 };
