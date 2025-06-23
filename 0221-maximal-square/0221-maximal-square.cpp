9b7b62ede743552ab9e72eac5ce8c599de305f3e
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-23 20:40:03 +0800

    Time: 489 ms (5.01%), Space: 28 MB (87.95%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+      int m = matrix.size();
+      int n = matrix[0].size(); 
+      int max = 0;
+
+      vector<vector<bool>> tmp(m, vector<bool>(n,0));
+      vector<vector<bool>> tmp2(m, vector<bool>(n,0));
+      for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+          tmp[i][j] = (matrix[i][j] == '1') ? true : false;
+          if(tmp[i][j]) max = 1;
+        }
+      }
+
+      bool foundLarger;
+      for (int size = 2; size <= min(m, n); ++size) {
+        foundLarger = false;
+        for (int i = 0; i <= m - size; ++i) {
+          for (int j = 0; j <= n - size; ++j) {
+            tmp2[i][j] = tmp[i][j] && tmp[i+1][j] && tmp[i][j+1] && tmp[i+1][j+1];
+            if(tmp2[i][j]) foundLarger = true;
+          }
+        }
+        tmp = tmp2;
+        if(foundLarger) max++;
+        else break;
+      }
+
+      return max*max;
+    }
+};
