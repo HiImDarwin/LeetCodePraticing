22109b92ab0ff5b21b3e805eb0d407f0e79e0838
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-25 21:17:09 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
+    // int minSteps(int n) {
+    //     vector<int> dp(n+1,INT_MAX);
+    //     dp[0] = 0;
+    //     dp[1] = 0;
+    //     for(int size = 1; size < n; ++size){
+    //       for(int i = size * 2; i <= n; i+=size){
+    //         if (i % size != 0) continue;
+    //         dp[i] = min(dp[i], (dp[size]+1) + (i-size)/size);
+    //       }
+    //     }
+    //     return dp[n];
+    // }
     int minSteps(int n) {
-        vector<int> dp(n+1,INT_MAX);
-        dp[0] = 0;
-        dp[1] = 0;
-        for(int size = 1; size < n; ++size){
-          for(int i = size * 2; i <= n; i+=size){
-            if (i % size != 0) continue;
-            dp[i] = min(dp[i], (dp[size]+1) + (i-size)/size);
-          }
-        }
-        return dp[n];
+      int count = 0;
+      while(n>1) {
+        int hcf = gd(n);
+        count+=(n/hcf);
+        n = hcf;
+      }
+      return count;
+    }
+
+    int gd(int n) {
+      for(int i=n/2; i>=1; i--){
+        if(n%i == 0) return i;
+      }
     }
 };
