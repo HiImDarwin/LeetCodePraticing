1d896054587e07490b7233fe87059e4a82910008
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-26 00:52:14 +0800

    Time: 0 ms (100%), Space: 17.2 MB (47.16%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-      int low_price = prices[0];
-      int high_price  = prices[0];
       int res = 0;
+      int low_price = prices[0];
       for(int i = 1; i < prices.size(); ++i) {
-        if(prices[i] < high_price) {
-          res += high_price-low_price;
-          low_price = prices[i];
-          high_price = prices[i];
-        } else {
-          high_price = prices[i];
-        }
+        if (prices[i] > prices[i-1]) continue;
+        res += prices[i-1] < low_price ? 0 : prices[i-1] - low_price;
+        low_price = prices[i];
       }
-      res += high_price -low_price;
+      res += prices[prices.size()-1] < low_price ? 
+             0 : 
+             prices[prices.size()-1] - low_price;
       return res;
     }
 };
