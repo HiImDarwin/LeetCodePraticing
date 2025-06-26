8f6f5ad506045dcd18fd7aa378ba6d3da7361fd8
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-27 02:45:22 +0800

    Time: 0 ms (100%), Space: 8.7 MB (30.53%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
+    // int integerBreak(int n) {
+    //   if(n <= 3) return n-1; 
+    //   vector<int> dp(n+1,0);
+    //   dp[1] = 1;
+    //   dp[2] = 2;
+    //   dp[3] = 3;
+    //   for(int i = 4; i <= n; ++i) {
+    //     for(int j = 1; j <= i/2; ++j) {
+    //       dp[i] = max(dp[i], dp[i-j]*j);
+    //     }
+    //   }
+    //   return dp[n];
+    // }
     int integerBreak(int n) {
-      vector<int> dp(n+1,0);
-      if(n == 1 || n == 2) return 1;
-      if(n == 3) return 2; 
-      dp[1] = 1;
-      dp[2] = 2;
-      dp[3] = 3;
-      for(int i = 4; i <= n; ++i) {
-        for(int j = 1; j <= i/2; ++j) {
-          dp[i] = max(dp[i], dp[i-j]*j);
-        }
-      }
-      return dp[n];
+      if(n<=3) return n-1;
+      memo.resize(n+1, 0);
+      return dp(n);
     }
+
+    int dp(int n) {
+      if(n <= 3) return n;
+      if(memo[n] != 0) return memo[n];
+
+      int ans = 0;
+      for(int i = 2; i <= n/2; ++i) {
+        ans = max(ans, dp(n-i)*i);
+      }
+      memo[n] = ans;
+      return memo[n];
+    }
+    vector<int> memo;
 };
