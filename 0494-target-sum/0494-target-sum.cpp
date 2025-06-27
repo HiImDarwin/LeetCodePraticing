122be62bf4a9a20eff490aac29194491b54419da
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-27 19:52:06 +0800

    Time: 132 ms (38.83%), Space: 54 MB (13.86%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
+    // int findTargetSumWays(vector<int>& nums, int target) {
+    //   int n = nums.size();
+    //   int total = 0;
+
+    //   unordered_map<int,int> map;
+    //   backtracking(map, nums, 0, 0);
+
+    //   return map[target];
+    // }
+
+    // void backtracking( unordered_map<int,int> &map, vector<int>& nums, int idx, int sum) {
+    //   if(idx == nums.size()) {
+    //     map[sum]++;
+    //     return;
+    //   }
+    //   backtracking(map, nums, idx+1, sum + nums[idx]);
+    //   backtracking(map, nums, idx+1, sum - nums[idx]);
+    // }
+
+
     int findTargetSumWays(vector<int>& nums, int target) {
-      int n = nums.size();
-      int total = 0;
-
-      unordered_map<int,int> map;
-      backtracking(map, nums, 0, 0);
-
-      return map[target];
-    }
-
-    void backtracking( unordered_map<int,int> &map, vector<int>& nums, int idx, int sum) {
-      if(idx == nums.size()) {
-        map[sum]++;
-        return;
+      unordered_map<int,int> counter;
+      counter[0] = 1;
+      for(int num: nums){
+        unordered_map<int,int> tmp;
+        for(auto [value, count]: counter) {
+          tmp[value+num] += count;
+          tmp[value-num] += count;
+        }
+        counter = tmp;
       }
-      backtracking(map, nums, idx+1, sum + nums[idx]);
-      backtracking(map, nums, idx+1, sum - nums[idx]);
+      return counter[target];
     }
 };
