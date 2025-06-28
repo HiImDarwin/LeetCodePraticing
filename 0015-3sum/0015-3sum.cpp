a0a01b20a813d59a484e0b0601b7e3bb01af9bfc
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-28 16:46:54 +0800

    Time: 49 ms (59.51%), Space: 29 MB (73.61%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
-        vector<vector<int>> res;
-
-        for(int i=0; i<n; ++i) {
-            if(i>0 && nums[i]==nums[i-1]) continue;
-
-            int left = i+1;
-            int right = n-1;
-
-            while(left<right) {
-                int sum = nums[i]+nums[left]+nums[right];
-                if(sum == 0) {
-                    vector<int> tmp = {nums[i], nums[left], nums[right]};
-                    res.push_back(tmp);
-                    do {
-                        left++;
-                    } while(left<right && nums[left]==nums[left-1]);
-                     do {
-                        right--;
-                    } while(left<right &&nums[right]==nums[right+1]);
-                } else if(sum > 0){
-                    do {
-                        right--;
-                    } while(left<right &&nums[right]==nums[right+1]);
-                } else {
-                    do {
-                        left++;
-                    } while(left<right &&nums[left]==nums[left-1]);
-                }
+      sort(nums.begin(),nums.end());
+      int n = nums.size();
+      vector<vector<int>> res;
+      for(int target = 0; target < n - 2; ++target) {
+        int targetNum = -nums[target];
+        int left = target+1;
+        int right = n-1;
+        while(left < right){
+          if(nums[left] + nums[right] < targetNum) left++;
+          else if(nums[left] + nums[right] > targetNum ) right--;
+          else {
+            res.push_back({nums[target],nums[left],nums[right]});
+            left++;
+            right--;
+            while(left < right && nums[left] == nums[left-1]) {
+              left++;
+            } 
+            while(left < right && nums[right+1] == nums[right]) {
+              right --;
             }
+          }
         }
-        
-        return res;
+        while(target < n-2 && nums[target] == nums[target+1]) target++;
+      }
+      return res;
     }
 };
+
+
+// i + j + k = 0
+// change the thought to  i + j = -k become a two sum problem and can check wtih two pointer
