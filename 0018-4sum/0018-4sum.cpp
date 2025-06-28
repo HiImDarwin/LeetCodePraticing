4604d0be91cec3f82cc162934a6a0c0c4e963416
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-28 17:32:25 +0800

    Time: 15 ms (92.37%), Space: 17.4 MB (58.52%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,32 +2,44 @@ class Solution {
 public:
     //這題難再剪枝 如何刪去不需要重覆地部分
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        if(nums.size()<4) return {};
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> res;
-        for(int i=0;i<nums.size()-3;++i){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            for(int j=i+1;j<nums.size()-2;++j) {
-                if(j>i+1 && nums[j]==nums[j-1]) continue;
-                int left = j+1;
-                int right = nums.size()-1;
-                long long int sum;
-                while(left<right){
-                    sum = (long long int)nums[i]+ nums[j]+nums[left]+nums[right];
-                    if (sum == target) {
-                        res.push_back({nums[i],nums[j],nums[left],nums[right]});
-                        while(left<right && nums[left]==nums[left+1]) ++left;
-                        while(left<right && nums[right]==nums[right-1]) --right;
-                        ++left;
-                        --right;
-                    } else if( sum > target) {
-                        --right;
-                    } else {
-                        ++left;
-                    }
-                }
+      int n  = nums.size();
+      sort(nums.begin(), nums.end());
+      vector<vector<int>> res;
+      for(int leftFix = 0; leftFix < n-3 ; ++leftFix) {
+        for( int rightFix = n-1; rightFix > leftFix + 2; --rightFix) {
+          int left = leftFix + 1;
+          int right = rightFix -1;
+          long long targetNum = (long long)target - nums[leftFix] - nums[rightFix];
+          while(left < right) {
+            if(nums[left] + nums[right] > targetNum) {
+              right--;
+            } else if (nums[left] + nums[right] < targetNum) {
+              left++;
+            } else {
+              res.push_back({nums[leftFix],nums[left],nums[right],nums[rightFix]});
+              left++;
+              right--;
+              while(left < right && nums[left] == nums[left-1]){
+                left++;
+              }
+              while(left < right && nums[right] == nums[right+1]){
+                right--;
+              }
             }
+          }
+          while(rightFix > leftFix + 2 && nums[rightFix] == nums[rightFix-1]){
+            rightFix--;
+          }
         }
-        return res;
+        while(leftFix < n-3 && nums[leftFix] == nums[leftFix+1]){
+            leftFix++;
+        }
+      }
+      return res;
     }
 };
+
+
+
+// corner case which will come up with multi same result
+// [2,2,2,2,2,2]
