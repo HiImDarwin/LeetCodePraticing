5d09a6bdb7c95e29b221689dfd4f8595b58bb13a
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-29 16:27:02 +0800

    Time: 568 ms (57.47%), Space: 48.1 MB (82.77%) - LeetHub

---
+++ 3913-partition-array-to-minimize-xor/3913-partition-array-to-minimize-xor.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minXor(vector<int>& nums, int k) {
+        int n  = nums.size();
+        vector<int> xorTable(n+1,0);
+        vector<vector<int>> dp(k+1,vector<int>(n+1,INT_MAX));
+        dp[1][0] = 0;
+        for(int i = 1,tmp = 0; i<=n; i++) {
+          tmp ^= nums[i-1];
+          xorTable[i] = tmp;
+          dp[1][i] = tmp;
+        }
+        
+        for(int i = 2; i <= k; ++i) {
+          for(int j = i; j <= n; ++j) {
+            for(int l = i; l <= j; ++l) {
+              dp[i][j] = min(dp[i][j],max(dp[i-1][l-1],xorTable[l-1]^xorTable[j]));
+            }
+          }
+        }
+       
+        return dp[k][n];
+    }
+};
+
+/*
+利用xor特性算出區間 xor
+vector<int> xorTable(n);
+
+dp[i][j] > minimum possible max_XOR 當把 i 個 element 分 j 組
+這邊要注意 dp[i][j] 最大值不一定是在j group 可能在 j group 前
+dp[i][j] = min(dp[i][j], max(dp[t][j-1]))
+
+
+
+dp[i] 自己一組 = dp[i] = max(dp[i-1][k-1],xorTable[i-1]^xorTable[i])
+dp[i] 跟前面的一組 = max(dp[i-1][k], dp[i-1][k]^nums[i] )
+
+
+*/
