9e01ddf45ba27e42eb926df0a364ed48dcd04eb4
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-06-29 16:33:27 +0800

    Time: 535 ms (62.08%), Space: 47.9 MB (87.01%) - LeetHub

--- 3913-partition-array-to-minimize-xor/3913-partition-array-to-minimize-xor.cpp
+++ 3913-partition-array-to-minimize-xor/3913-partition-array-to-minimize-xor.cpp
@@ -3,15 +3,15 @@ public:
     int minXor(vector<int>& nums, int k) {
         int n  = nums.size();
         vector<int> xorTable(n+1,0);
-        vector<vector<int>> dp(k+1,vector<int>(n+1,INT_MAX));
-        dp[1][0] = 0;
+        
         for(int i = 1,tmp = 0; i<=n; i++) {
           tmp ^= nums[i-1];
           xorTable[i] = tmp;
-          dp[1][i] = tmp;
         }
-        
-        for(int i = 2; i <= k; ++i) {
+        vector<vector<int>> dp(k+1,vector<int>(n+1,INT_MAX));
+        dp[0][0] = 0; 
+
+        for(int i = 1; i <= k; ++i) {
           for(int j = i; j <= n; ++j) {
             for(int l = i; l <= j; ++l) {
               dp[i][j] = min(dp[i][j],max(dp[i-1][l-1],xorTable[l-1]^xorTable[j]));
@@ -29,7 +29,7 @@ vector<int> xorTable(n);
 
 dp[i][j] > minimum possible max_XOR 當把 i 個 element 分 j 組
 這邊要注意 dp[i][j] 最大值不一定是在j group 可能在 j group 前
-dp[i][j] = min(dp[i][j], max(dp[t][j-1]))
+dp[i][j] = min(dp[i][j], max(dp[l-1][j-1],xorTable[l]^xorTable[j]))
 
 
 
