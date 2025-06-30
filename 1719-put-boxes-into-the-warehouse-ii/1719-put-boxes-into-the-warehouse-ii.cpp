fe1455c3cf9207db3bbb701edd030e5adbcb798f
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-01 02:23:42 +0800

    Time: 19 ms (87.5%), Space: 64.2 MB (81.25%) - LeetHub

--- 1719-put-boxes-into-the-warehouse-ii/1719-put-boxes-into-the-warehouse-ii.cpp
+++ 1719-put-boxes-into-the-warehouse-ii/1719-put-boxes-into-the-warehouse-ii.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     int maxBoxesInWarehouse(vector<int>& boxes, vector<int>& warehouse) {
-      int m = warehouse.size();
-      int n = boxes.size();
+      int n = boxes.size(), m = warehouse.size();
       sort(boxes.begin(), boxes.end());
-      
       int right = m-1, left = 0, idx = n-1;
       int res = 0;
-      while(left <= right && idx >= 0) {
-        if(warehouse[right] > warehouse[left]) {
-          while(idx >= 0 && boxes[idx] > warehouse[right]) 
-            idx--;
-          if(idx < 0) break;
-          idx--;
-          right--;
+      for (int i = n - 1; i >= 0; --i) {
+        if(left > right) break;
+        if(warehouse[left] >= boxes[i] ) {
           res++;
-        } else {
-          while(idx >= 0 && boxes[idx] > warehouse[left] ) 
-            idx--;
-          if(idx < 0) break;
-          idx--;
           left++;
+        } else if (warehouse[right] >= boxes[i] ) {
+          right--;
           res++;
         }
       }
