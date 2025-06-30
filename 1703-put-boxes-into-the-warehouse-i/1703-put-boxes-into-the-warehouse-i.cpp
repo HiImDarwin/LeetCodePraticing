930cbb392040a9b5f4795a70686263bbbbd03659
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-01 01:36:41 +0800

    Time: 17 ms (47.62%), Space: 64.3 MB (66.67%) - LeetHub

---
+++ 1703-put-boxes-into-the-warehouse-i/1703-put-boxes-into-the-warehouse-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxBoxesInWarehouse(vector<int>& boxes, vector<int>& warehouse) { 
+      int n = boxes.size();
+      int m = warehouse.size();
+      sort(boxes.begin(), boxes.end());
+      int minHeight = warehouse[0];
+      int i = n - 1;
+      int j = 0;
+      int res = 0;
+      while(i >= 0 && j < m) {
+        if(boxes[i] <= minHeight) {
+          res++;
+          j++;
+          minHeight = j < m ? min(minHeight,warehouse[j]): minHeight;
+        } 
+        i--;
+      }
+      return res;
+    }
+};
+
+/*
+push the box from little size
+*/
