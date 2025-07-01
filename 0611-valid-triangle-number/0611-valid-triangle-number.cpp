0aa17894d33cef85ec1c76dbbffd0b44407fea8f
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-01 16:53:09 +0800

    Time: 27 ms (91.22%), Space: 16.6 MB (40.97%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.cpp
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
+    // int triangleNumber(vector<int>& nums) {
+    //   sort(nums.begin(), nums.end());
+    //   int n = nums.size();
+    //   int res = 0;
+    //   for (int i = 0; i< n-2; i++) {
+    //     for(int j = i + 1; j < n-1; j++) {
+    //       for(int k = j + 1; k < n; k++) {
+    //         if(nums[i] + nums[j] <= nums[k]) break;
+    //         res++;
+    //       }
+    //     }
+    //   }
+    //   return res;
+    // }
     int triangleNumber(vector<int>& nums) {
-      sort(nums.begin(), nums.end());
-      int n = nums.size();
       int res = 0;
-      for (int i = 0; i< n-2; i++) {
-        for(int j = i + 1; j < n-1; j++) {
-          for(int k = j + 1; k < n; k++) {
-            if(nums[i] + nums[j] <= nums[k]) break;
-            res++;
-          }
+      int n = nums.size();
+      sort(nums.begin(), nums.end());
+      int k = 0;
+      while( k < n && nums[k] == 0) k++;
+      for(int i = n-1; i >= k+2; --i) {
+        int target = nums[i];
+        int l = k;
+        int r = i-1;
+        while(l < r) {
+          if(nums[l] + nums[r] > target) {
+            res += r-l;
+            r--;
+          } else l++;
         }
       }
       return res;
     }
+
 };
 
 
