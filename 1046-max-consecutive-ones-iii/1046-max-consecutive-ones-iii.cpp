23456c8b455dbe88768a3777e1a17890de3801ef
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-02 01:42:28 +0800

    Time: 0 ms (100%), Space: 59.2 MB (59.3%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -1,24 +1,24 @@
-class Solution {
-public:
-    int longestOnes(vector<int>& nums, int k) {
-      unordered_map<int,int> mp;
-      mp[0] = -1;
-      int count = 0;
-      for(int i = 0; i < nums.size(); ++i) {
-        if(nums[i] == 0) {
-          count++;
-          mp[count] = i;
-        }
-      }
-      mp[++count] = nums.size();
-      int maxLen = 0;
-      if(count-1 <= k ) return nums.size();
-      for(int i = k+1; i <= count; i++) {
-        maxLen = max(maxLen,mp[i] - mp[i-k-1] -1);
-      }
-      return maxLen;
-    }
-};
+// class Solution {
+// public:
+    // int longestOnes(vector<int>& nums, int k) {
+    //   unordered_map<int,int> mp;
+    //   mp[0] = -1;
+    //   int count = 0;
+    //   for(int i = 0; i < nums.size(); ++i) {
+    //     if(nums[i] == 0) {
+    //       count++;
+    //       mp[count] = i;
+    //     }
+    //   }
+    //   mp[++count] = nums.size();
+    //   int maxLen = 0;
+    //   if(count-1 <= k ) return nums.size();
+    //   for(int i = k+1; i <= count; i++) {
+    //     maxLen = max(maxLen,mp[i] - mp[i-k-1] -1);
+    //   }
+    //   return maxLen;
+    // }
+// };
 
 
 /* used prefix sum
@@ -32,4 +32,44 @@ for ex  if k = 3
 so the max length is index of 5 - index of 3 -1     
 
 coner case when 0 number is less than k
+*/
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) 
+    {
+      int n = nums.size();
+      int count = 0;
+      int i = 0;
+      int ret = 0;
+      for (int j=0; j<n; j++){
+        if (nums[j] == 1) {
+          ret = max(ret, j-i+1);
+          continue;
+        }
+            
+        count++;
+        while (count>k) {
+          if (nums[i]==0)
+            count--;
+          i++;
+        }
+        ret = max(ret, j-i+1);
+      }
+      return ret;
+    }
+};
+/* two pointer
+left and right
+when k's 0 in window extend right if right is 1
+when k+1 0 in window move left
+
+edge case > when k = 0 what will happend
+
+
+[1 1 1 1 0 1 1 0 1 1 0 0 1 ]
+
+
+
+
 */
