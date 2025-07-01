b69cd3359821d4b850e372d80d995b7d33966fa8
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-01 21:22:44 +0800

    Time: 15 ms (49.17%), Space: 9.4 MB (54.17%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,71 +1,34 @@
-// class Solution {
-//   public:
-//     int longestSubstring(string s, int k) {
-//       int res = 0;
-//       for(int charCount = 1; charCount <=26; ++charCount) {
-//         res = max(res, helper(s,charCount,k));
-//       }
-//       return res;
-//     }
-
-//     int helper (string& s, int charCount, int k) {
-//       int j = 0;
-//       unordered_map<char,int> mp;
-//       int count = 0; // num for characters whose freq >=k
-//       int maxLen = 0;
-//       for (int i = 0; i < s.size(); i++) {
-        
-//         while (j < s.size() && mp.size() <= charCount) {
-//           if(++mp[s[j]] ==k) count++;
-//           if(mp.size() == charCount && count == charCount) maxLen = (maxLen, j-i+1);
-//           j++;
-//         }
-//         mp[s[i]]--;
-//         if(mp[s[i]] == k-1) count--;
-//         if(mp[s[i]] == 0) mp.erase(s[i]);
-        
-//       }
-//       return maxLen;
-//     }
-// };
 class Solution {
-public:
-    int helper(string& s, int t, int k)
-    {
-        int j = 0;
-        unordered_map<char, int>Map;
-        int count = 0;
-        int ret = 0;
-        for (int i=0; i<s.size(); i++)
-        {
-            while (j<s.size() && Map.size() <= t)
-            {
-                Map[s[j]]++;
-                if (Map[s[j]]==k)
-                    count++;
-                j++;
-
-                if (Map.size()==t && count == t)
-                    ret = max(ret, j-i);
-            }
-            
-            Map[s[i]]--;
-            if (Map[s[i]]==k-1)
-                count--;
-            if (Map[s[i]]==0)
-                Map.erase(s[i]);
-        }
-        return ret;
+  public:
+    int longestSubstring(string s, int k) {
+      int res = 0;
+      for(int charCount = 1; charCount <=26; ++charCount) {
+        res = max(res, helper(s,charCount,k));
+      }
+      return res;
     }
 
-    int longestSubstring(string s, int k) 
-    {
-        int ret = 0;
-        for (int t=1; t<=26; t++)
-            ret = max(ret, helper(s, t, k));
-        return ret;
+    int helper (string& s, int charCount, int k) {
+      int j = 0;
+      unordered_map<char,int> mp;
+      int count = 0; // num for characters whose freq >=k
+      int maxLen = 0;
+      for (int i = 0; i < s.size(); i++) {
+        
+        while (j < s.size() && mp.size() <= charCount) {
+          if(++mp[s[j]] ==k) count++;
+          if(mp.size() == charCount && count == charCount) maxLen = max(maxLen, j-i+1);
+          j++;
+        }
+        mp[s[i]]--;
+        if(mp[s[i]] == k-1) count--;
+        if(mp[s[i]] == 0) mp.erase(s[i]);
+        
+      }
+      return maxLen;
     }
 };
+
 /*
 In this question
 we can't find a normal way for two pointer because it's hard
