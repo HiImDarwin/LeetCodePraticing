31a8fa9ddda4d0583c210798869133d28414963d
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-01 18:55:40 +0800

    Time: 0 ms (100%), Space: 34 MB (37.04%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -2,36 +2,85 @@ class Solution {
 public:
     // prefix Sum + hash 
     // int numSubarraysWithSum(vector<int>& nums, int goal) {
-    //     unordered_map<int,int> map;
-    //     map[0]=1;
-    //     int count=0;
-    //     int res=0;
-    //     for(int &x :nums){
-    //         if(x==1) {
-    //             count++;
-    //         }
-    //         if(map.count(count-goal)){
-    //             res += map[count-goal];
-    //         }
-    //         map[count]++;
+    //   int n = nums.size();
+    //   unordered_map<int,int> map; //prefix -> count of idx
+    //   map[0] = 1; // 0個1 的 prefix count
+    //   int res = 0;
+    //   int preSum = 0;
+    //   for(int i = 0 ; i < n; ++i) {
+    //     preSum += nums[i];
+    //     if(map.count(preSum - goal)) {
+    //       res += map[preSum - goal];
     //     }
-    //     return res;
+    //     map[preSum]++;
+    //   }
+    //   return res;
     // }
 
-    // Sliding window
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return func(nums,goal) - func(nums,goal-1);
-    }
-    // 這是計算子陣列和 <= goal 的數量
-    int func (vector<int>& nums, int goal) {
-        if(goal<0) return 0;
-        int l=0, r=0, sum=0, count=0;
-        while(r<nums.size()) {
-            sum += nums[r];
-            while(sum>goal) sum-=nums[l++];
-            count += r-l+1;
-            r++;
+
+/*
+prefixSum + hash
+0 0 0 0 [1 XXXXX 1] 0 0 0 0 
+                    r
+we wnat all the possible of subarray
+we already know how many left side zero
+we dont know how many right side is
+so for each round we fixed r and count the left
+
+*/
+
+    //Sliding window
+    int numSubarraysWithSum(vector<int>& A, int S) 
+    {       
+        int n = A.size();
+        vector<int>postZeros(n);
+        int count = 0;
+        for (int i=n-1; i>=0; i--)
+        {
+            postZeros[i] = count;
+            if (A[i]==0)            
+                count++;            
+            else
+                count = 0;
         }
-        return count;
+        
+        int j = 0, sum = 0;
+        int ret = 0;
+        for (int i=0; i<n; i++)
+        {
+            while (j<=i || (j<n && sum < S))
+            {
+                sum += A[j];
+                j++;
+            }
+            
+            if (sum==S)
+                ret += postZeros[j-1]+1;
+            sum -= A[i]; 
+        }
+        
+        return ret;        
     }
 };
+
+
+
+/*
+sliding window
+window should be total subarray for last 1 index in subarray
+so when we right is fixed, find all possible sub
+
+while (right <　n)
+ find the rihgt
+ fixed right found all possible sub
+ res += all possible sub
+ [x x x x x x x x x x x]
+    l               r
+l is 0 and l-1 is another 1 that 
+right is (goal + 1 ) 1's
+
+
+conor case when goal is 0 and nums is 0 
+
+
+*/
