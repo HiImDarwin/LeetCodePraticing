ceed99240108a93ff494d9af88a0c16839bf9314
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-03 01:46:40 +0800

    Time: 14 ms (43.17%), Space: 11.7 MB (26.73%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,29 +1,52 @@
 class Solution {
 public:
-  string minWindow(string s, string t) {
-    if(s.empty() || t.empty() || s.size() < t.size()) return "";
-    vector<int> map(128,0);
-    int count = t.length();
-    int start = 0, end = 0, minLen = INT_MAX, startIndex = 0;
-    for(char &c : t) {
-      map[c]++;
-    }
-    while(end < s.length()) {
-      if(map[s[end++]]-- > 0) {
-        count--;
+    string minWindow(string s, string t) {
+      int m = s.length();
+      unordered_map<char,int> table;
+      for(char c: t) {
+        table[c]++;
       }
-
-      while(count == 0) {
-        if(end-start < minLen) {
-          startIndex = start;
-          minLen = end-start;
+      int start = 0, len = INT_MAX, count = 0;
+      unordered_map<char,int> record;
+      for(int r = 0, l = 0; r < m; r++) {
+        if(table.count(s[r])) {
+          record[s[r]]++;
+          if(record[s[r]] == table[s[r]]) count++;
         }
-        if(map[s[start++]]++ == 0) {
-          count++;
+        
+        if(count == table.size()) {
+          while(l <= r && count == table.size()) {
+            if(r-l+1 < len) {
+              start = l;
+              len = r-l+1;
+            }
+            if(table.count(s[l])) {
+              record[s[l]]--;
+              if(record[s[l]] < table[s[l]]) 
+                count--;
+            }
+            l++;
+          }
         }
       }
+      return len == INT_MAX ? "" : s.substr(start, len);
     }
-
-    return minLen == INT_MAX ? "": s.substr(startIndex, minLen);
-  }
 };
+
+/*
+s m
+t n 
+
+a minimum window that t is included in s (duplicate?)
+
+1. for a brute way fixed the window size and go for n-window run  
+not find and increase the window size.
+
+2. use character num in window. t got 7 char
+so i go from 7 ~ 26 char in window  O(26N) 
+
+3. just slide the window when the need charactor in window 
+and the minmize the window from left
+
+
+*/
