8ba59610cad591144369b08c2aadb441ac684612
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-03 17:44:37 +0800

    Time: 0 ms (100%), Space: 15.9 MB (99.66%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,17 +1,58 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-      int left = 0, right = nums.size() - 1;
+      int right = nums.size()-1;
+      int left = 0;
       while(left < right) {
+        while(left < right && nums[left] == nums[left+1]){
+          left++;
+        }
+        while(left < right && nums[right] == nums[right-1]){
+          right--;
+        }
+        if(left == right) break;
+        
         int mid = left + (right - left) / 2;
-        if (nums[mid] > nums[right]) {
-          left = mid+1;
-        } else if (nums[mid] < nums[right]) {
+        
+        if(nums[mid] < nums[right]) {
           right = mid;
         } else {
-          right--;
+          left = mid + 1;
         }
       }
       return nums[left];
     }
 };
+
+
+
+
+/*
+array is rotated
+
+[1 2 3 4 4 5 5 6 7]
+
+[6 7 1 2 3 4 4 5 5]
+
+[4 1 4 4 4 4 4]
+ l   r         r
+
+[4 4 4 4 4 5 6 7 1 4]
+ l       l          r
+
+
+the value of array is in accesding
+i will make the mid indx on left
+
+question 
+ * Value duplicate? Yes
+ * value range ?
+ * array size ?
+ * 
+
+
+
+
+
+
+*/
