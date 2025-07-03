fc77e88a4fc3b430a59412ce318bfdb357ac73f2
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-04 03:11:24 +0800

    Time: 10 ms (77.26%), Space: 28.9 MB (98.97%) - LeetHub

---
+++ 0475-heaters/0475-heaters.cpp
@@ -0,0 +1,65 @@
+// class Solution {
+//   public:
+//     int findRadius(vector<int>& houses, vector<int>& heaters) {
+//       sort(houses.begin(),houses.end());
+//       sort(heaters.begin(), heaters.end());
+//       int m  = houses.size();
+//       int n  = heaters.size();
+//       int res = 0;
+//       for(int i = 0, j =0; i < m; ++i) {
+//         int tmp = abs(houses[i] - heaters[j]);
+//         while(j+1 < n && abs(houses[i] - heaters[j+1]) <= tmp) {
+//           j++;
+//           tmp = abs(houses[i] - heaters[j]);
+//         }
+//         res = max(res, tmp);
+//       }
+//       return res;
+//     }
+// };
+
+class Solution {
+public:
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        sort(houses.begin(), houses.end());
+        sort(heaters.begin(), heaters.end());
+
+        int closestHeater = 0, maxDist = INT_MIN;
+        for (int i = 0; i < houses.size(); i++) {
+            while (closestHeater + 1 < heaters.size() && abs(houses[i] - heaters[closestHeater]) >= abs(houses[i] - heaters[closestHeater + 1])) {
+                closestHeater++;
+            }
+            maxDist = max(maxDist, abs(houses[i] - heaters[closestHeater]));
+        }
+
+        return maxDist;
+    }
+};
+
+
+/*
+
+x   x   x  x   x        x  x          house
+---------------------------
+h  h      h   h   h    h   h          heater
+
+the qeustion is to find the maximum 
+range for a house to nearest heater
+
+
+This can go with two pointer
+
+fixed each house
+iterat the  heater
+
+TimeComplexity
+O(N log N) + O(M log M) + O(N) +O(M)
+
+
+
+ question
+ number of house
+ range of house distance and heater dist
+ complexity limit
+
+*/
