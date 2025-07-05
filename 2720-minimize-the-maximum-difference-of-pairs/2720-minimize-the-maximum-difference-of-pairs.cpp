5be229be5b4bcd2a9f3e4527b3750f6c9a27e3fa
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-05 22:09:28 +0800

    Time: 34 ms (12.65%), Space: 86.7 MB (79.53%) - LeetHub

---
+++ 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int minimizeMax(vector<int>& nums, int p) {
+      if (p == 0) return 0;
+      int biggest = *max_element(nums.begin(), nums.end());
+      sort(nums.begin(), nums.end());
+      int left = 0, right = biggest;
+      while(left < right) {
+        int mid = left + (right - left) / 2;
+        if(enoughPair(nums, p, mid)) {
+          right = mid;
+        } else {
+          left = mid + 1;
+        }
+      }
+      return left;
+    }
+
+    bool enoughPair(vector<int>& nums, int p, int diff) {
+      int count = 0;
+      for(int i = 0; i <nums.size()-1; i++) {
+        if(nums[i+1] - nums[i] <= diff){
+          count++;
+          i = i+1;
+        }
+      }
+      return count >= p;
+    }
+
+};
+
+/*
+find p pairs of indice {a,b}
+no index appears more than once
+
+if we brute force find the pair
+it gona be O(n^2)
+
+
+
+Qusetion:
+will the value duplicate? yes
+
+
+
+*/
