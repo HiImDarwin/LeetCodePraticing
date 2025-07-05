e38e83edf469ec9a5215bf590ff624fde9b4890d
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-05 16:24:38 +0800

    Time: 196 ms (4.82%), Space: 12.5 MB (5.36%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,42 +1,56 @@
+// class Solution {
+// public:
+//     int splitArray(vector<int>& nums, int k) {
+//         long minK = *max_element(nums.begin(), nums.end());
+//         long maxK = accumulate(nums.begin(),nums.end(),0);
+//         while(minK <maxK) {
+//           long mid = minK + (maxK - minK) / 2;
+//           if(isOK(nums, k, mid)) {
+//             maxK = mid;
+//           } else {
+//             minK = mid + 1;
+//           }
+//         }
+//         return minK;
+//     }
+
+//     bool isOK(vector<int> &nums, int k , long val) {
+//       int count = 0;
+//       for (int i = 0; i<nums.size(); ++i) {
+//         if(nums[i] > val) return false;
+
+//         int j = i;
+//         long sum = 0;
+//         while(j < nums.size() && sum + nums[j] <= val) {
+//           sum += nums[j];
+//           j++;
+//         }
+//         count++;
+//         i = j-1;
+//       }
+//       return count <= k;
+//     }
+
+// };
 class Solution {
-public:
+  public:
     int splitArray(vector<int>& nums, int k) {
-        long minK = *max_element(nums.begin(), nums.end());
-        long maxK = accumulate(nums.begin(),nums.end(),0);
-        while(minK <maxK) {
-          long mid = minK + (maxK - minK) / 2;
-          if(isOK(nums, k, mid)) {
-            maxK = mid;
-          } else {
-            minK = mid + 1;
+      int n = nums.size();
+      nums.insert(nums.begin(),0);
+      vector<vector<long>> dp(n+1,vector<long>(k+1,INT_MAX));
+      dp[0][0] = 0;
+      for(int i = 1; i <= n; ++i) {
+        for(int x=1; x<=min(k,i);x++) {
+          long sum = 0;
+          for(int j =i; j>=x; j--) {
+            sum+=nums[j];
+            dp[i][x] = min(dp[i][x], max(dp[j-1][x-1], sum));
           }
         }
-        return minK;
-    }
-
-    bool isOK(vector<int> &nums, int k , long val) {
-      int count = 0;
-      for (int i = 0; i<nums.size(); ++i) {
-        if(nums[i] > val) return false;
-
-        int j = i;
-        long sum = 0;
-        while(j < nums.size() && sum + nums[j] <= val) {
-          sum += nums[j];
-          j++;
-        }
-        count++;
-        i = j-1;
       }
-      return count <= k;
+      return dp[n][k];
     }
-
-
-
 };
-
-
-
 /*
 splite array into k non-empty subarray
 largest sum of any array i minimized
@@ -45,6 +59,14 @@ largest sum of any array i minimized
 
 way 1 DP
 
+DP[k][i] means max value when divided array into k group from 0 ~i element 
+
+Dp initialization
+  when 0 group dp[0][i] should all be 0
+  there are some case that array can't divied 
+  we used -1 to represent it
+Dp status transfer
+  dp[i][j] =max( min(dp[i-1][j-1]: dp[i-1][j-n]) )
 
 
 way 2 Binary Seach
