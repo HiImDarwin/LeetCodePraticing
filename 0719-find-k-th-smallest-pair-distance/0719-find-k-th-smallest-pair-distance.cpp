70e040da71bf8f89d402746c8a39cfcb164e07c9
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-06 16:47:17 +0800

    Time: 801 ms (5.92%), Space: 13.9 MB (92.28%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -1,24 +1,50 @@
 class Solution {
 public:
+    // Bucket Sort
+    // int smallestDistancePair(vector<int>& nums, int k) {
+    //     int n = nums.size();
+    //     int maxVal = *max_element(nums.begin(), nums.end());
+    //     vector<int> bucket(maxVal+1,0);
+
+
+    //     for(int i = 0; i < n-1; ++i) {
+    //       for(int j = i+1;j <n; ++j) {
+    //         int dist = abs(nums[i] - nums[j]);
+    //         bucket[dist]++;
+    //       }
+    //     }
+    //     int i = 0;
+    //     int count = 0;
+    //     while(i <= maxVal && count < k) {
+    //       count += bucket[i];
+    //       i++;
+    //     }
+    //     return i-1;
+    // }
+    // Binary Search + DP
     int smallestDistancePair(vector<int>& nums, int k) {
-        int n = nums.size();
-        int maxVal = *max_element(nums.begin(), nums.end());
-        vector<int> bucket(maxVal+1,0);
-
-
-        for(int i = 0; i < n-1; ++i) {
-          for(int j = i+1;j <n; ++j) {
-            int dist = abs(nums[i] - nums[j]);
-            bucket[dist]++;
-          }
-        }
-        int i = 0;
+      int n = nums.size();
+      sort(nums.begin(), nums.end());
+      int maxVal = nums.back();
+      int left = 0;
+      int right = maxVal-nums[0];
+      while(left < right) {
+        int mid = left + (right - left) / 2;
         int count = 0;
-        while(i <= maxVal && count < k) {
-          count += bucket[i];
-          i++;
+        for (int i = 0,j = 0; i < n; ++i) {
+          j = i;
+          while(j+1 < n && nums[j+1] - nums[i] <= mid) {
+            j++;
+          }
+          count += j-i;
         }
-        return i-1;
+        if(count < k) {
+          left = mid + 1;
+        } else {
+          right = mid;
+        }
+      }
+      return left;
     }
 };
 
