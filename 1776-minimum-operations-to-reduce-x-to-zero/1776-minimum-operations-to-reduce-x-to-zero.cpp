4225911f693578667e041de62464b019dd0cf1fc
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-09 22:36:49 +0800

    Time: 0 ms (100%), Space: 102.3 MB (66.87%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,28 +1,49 @@
 class Solution {
 public:
+    // int minOperations(vector<int>& nums, int x) {
+    //   int n = nums.size(); 
+    //   unordered_map<int,int> backward;
+    //   backward[0] = n;
+    //   int preSum = 0;
+
+    //   for(int i = n-1 ; i >= 0; --i) {
+    //     preSum += nums[i];
+    //     backward[preSum] = i;
+    //   }
+    //   if(preSum < x) return -1;
+
+    //   int res = INT_MAX;
+    //   if(backward.find(x)!= backward.end())
+    //     res = min(res,n-backward[x]);     
+    //     preSum = 0;
+    //   for(int i = 0; i < n; ++i) {
+    //     preSum += nums[i];
+    //     if(backward.find(x-preSum) != backward.end() && i < backward[x-preSum]) {
+    //       res = min(res,(n-backward[x-preSum]) + i + 1);
+    //     }
+    //   }
+    //   return res == INT_MAX ? -1 : res;
+    // }
     int minOperations(vector<int>& nums, int x) {
-      int n = nums.size(); 
-      unordered_map<int,int> backward;
-      backward[0] = n;
-      int preSum = 0;
+      int n = nums.size();
+      int totalSum = accumulate(nums.begin(),nums.end(),0);
+      int target = totalSum - x;
+      if(target < 0) return -1;
+      if(totalSum == x) return n;
 
-      for(int i = n-1 ; i >= 0; --i) {
-        preSum += nums[i];
-        backward[preSum] = i;
-      }
-      if(preSum < x) return -1;
-
-      int res = INT_MAX;
-      if(backward.find(x)!= backward.end())
-        res = min(res,n-backward[x]);     
-        preSum = 0;
-      for(int i = 0; i < n; ++i) {
-        preSum += nums[i];
-        if(backward.find(x-preSum) != backward.end()) {
-          res = min(res,(n-backward[x-preSum]) + i + 1);
+      int sum = 0;
+      int res = -1; 
+      for(int right = 0,left = 0; right < n; ++right) {
+        sum += nums[right];
+        while(left <= right && sum > target) {
+          sum -= nums[left++];
+        }
+        if(sum == target) {
+          res = max(res ,right - left+1);
         }
       }
-      return res == INT_MAX ? -1 : res;
+
+      return res == -1 ? res : n - res;
     }
 };
 
