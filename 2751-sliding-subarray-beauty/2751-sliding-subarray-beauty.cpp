9a6cea6509462c6e278a2a9525aaa866e3b29709
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-10 23:03:41 +0800

    Time: 1010 ms (14.29%), Space: 385.9 MB (5.12%) - LeetHub

---
+++ 2751-sliding-subarray-beauty/2751-sliding-subarray-beauty.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) {
+      multiset<int> set1,set2;
+      vector<int> res;
+
+      for(int i = 0; i< nums.size(); ++i) {
+        if(set1.size() < x)
+          set1.insert(nums[i]);
+        else {
+          int v = *set1.rbegin();
+          if(v > nums[i]) {
+            set1.erase(set1.find(v));
+            set2.insert(v);
+            set1.insert(nums[i]);
+          } else {
+            set2.insert(nums[i]);
+          }
+        }
+
+        if(i >= k - 1) {
+          int v = *set1.rbegin();
+          res.push_back(min(v,0));
+
+          int u = nums[i-k+1];
+          if(set1.find(u) != set1.end()) {
+            set1.erase(set1.find(u));
+            if(!set2.empty()) {
+              set1.insert(*set2.begin());
+              set2.erase(set2.begin());
+            }
+          } else {
+            set2.erase(set2.find(u));
+          }
+        } 
+      }
+      return res;
+    }
+};
+
+
+/*
+法一
+less than x element <= 0
+k element sub array
+使用一個 SET 來做sorting並且傳值 > TLE
+
+
+法一時間複雜度
+每次的插入予刪除 O(log n)
+每次找第x個元素 O(x) * (n-k+1)
+所以最差情況下 O(n log k + n k) 
+
+
+
+
+法二
+使用兩個set 省去 查到第x個元素時間
+
+法三
+bucket sort
+
+*/
