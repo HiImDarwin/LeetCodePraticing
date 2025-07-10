ba7d679fd63b8f1d915d5527bd1c4733ea5f1948
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-10 23:10:10 +0800

    Time: 46 ms (71.86%), Space: 184.2 MB (93.82%) - LeetHub

--- 2751-sliding-subarray-beauty/2751-sliding-subarray-beauty.cpp
+++ 2751-sliding-subarray-beauty/2751-sliding-subarray-beauty.cpp
@@ -1,38 +1,54 @@
 class Solution {
 public:
-    vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) {
-      multiset<int> set1,set2;
-      vector<int> res;
+    // vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) {
+    //   multiset<int> set1,set2;
+    //   vector<int> res;
 
-      for(int i = 0; i< nums.size(); ++i) {
-        if(set1.size() < x)
-          set1.insert(nums[i]);
-        else {
-          int v = *set1.rbegin();
-          if(v > nums[i]) {
-            set1.erase(set1.find(v));
-            set2.insert(v);
-            set1.insert(nums[i]);
-          } else {
-            set2.insert(nums[i]);
+    //   for(int i = 0; i< nums.size(); ++i) {
+    //     if(set1.size() < x)
+    //       set1.insert(nums[i]);
+    //     else {
+    //       int v = *set1.rbegin();
+    //       if(v > nums[i]) {
+    //         set1.erase(set1.find(v));
+    //         set2.insert(v);
+    //         set1.insert(nums[i]);
+    //       } else {
+    //         set2.insert(nums[i]);
+    //       }
+    //     }
+
+    //     if(i >= k - 1) {
+    //       int v = *set1.rbegin();
+    //       res.push_back(min(v,0));
+
+    //       int u = nums[i-k+1];
+    //       if(set1.find(u) != set1.end()) {
+    //         set1.erase(set1.find(u));
+    //         if(!set2.empty()) {
+    //           set1.insert(*set2.begin());
+    //           set2.erase(set2.begin());
+    //         }
+    //       } else {
+    //         set2.erase(set2.find(u));
+    //       }
+    //     } 
+    //   }
+    //   return res;
+    // }
+    vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) {
+      vector<int> res(nums.size() - k + 1), counter(50);
+      for (int i = 0; i < nums.size(); ++i) {
+        if(nums[i] < 0) counter[nums[i] + 50]++;
+        if(i - k >= 0 && nums[i - k] < 0) counter[nums[i - k] + 50]--;
+        if (i - k + 1 < 0) continue;
+        for(int j = 0, count = 0; j < 50; ++j) {
+          count += counter[j];
+          if (count >= x) {
+            res[i - k + 1] = j - 50;
+            break;
           }
         }
-
-        if(i >= k - 1) {
-          int v = *set1.rbegin();
-          res.push_back(min(v,0));
-
-          int u = nums[i-k+1];
-          if(set1.find(u) != set1.end()) {
-            set1.erase(set1.find(u));
-            if(!set2.empty()) {
-              set1.insert(*set2.begin());
-              set2.erase(set2.begin());
-            }
-          } else {
-            set2.erase(set2.find(u));
-          }
-        } 
       }
       return res;
     }
@@ -56,6 +72,7 @@ k element sub array
 
 法二
 使用兩個set 省去 查到第x個元素時間
+O(n log k)
 
 法三
 bucket sort
