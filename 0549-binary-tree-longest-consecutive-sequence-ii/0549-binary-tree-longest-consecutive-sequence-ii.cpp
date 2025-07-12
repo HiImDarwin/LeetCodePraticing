54a874a396deda4c9c591637a29760543c99e14f
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-12 16:31:40 +0800

    Time: 0 ms (100%), Space: 24 MB (29.73%) - LeetHub

--- 0549-binary-tree-longest-consecutive-sequence-ii/0549-binary-tree-longest-consecutive-sequence-ii.cpp
+++ 0549-binary-tree-longest-consecutive-sequence-ii/0549-binary-tree-longest-consecutive-sequence-ii.cpp
@@ -16,15 +16,16 @@ public:
       dfs(root,res);
       return res;
     }
-    pair<int,int> dfs(TreeNode* root, int &ret,int p = 0) {
+    pair<int,int> dfs(TreeNode* root, int &res,int p = 0) {
       if(!root) return {0,0};
-      auto [l1,l2] = dfs(root->left, ret, root->val);
-      auto [r1,r2] = dfs(root->right, ret, root->val);
-      ret = max({ret, l1 + 1 + r2, l2 + 1 + r1});
-      if (p - root->val == 1)
-        return {max(l1, r1)+1, 0};
-      if (p - root->val == -1)
-        return {0, max(l2,r2) + 1};
+      auto [l1,l2] = dfs(root->left, res, root->val);
+      auto [r1,r2] = dfs(root->right, res, root->val);
+      //consider is the longest is us
+      res = max({res,l1 + 1 + r2, l2 + 1 + r1}); 
+      if(root->val - p == 1)
+        return {max(l1,r1)+1,0};
+      if(root->val - p == -1)
+        return {0,max(l2,r2)+1};
       return {0,0};
     }
 };
