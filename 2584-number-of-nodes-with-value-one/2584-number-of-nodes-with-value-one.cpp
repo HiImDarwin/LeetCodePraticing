714d868be9a5f168f91ea451291117a8e9c97063
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-13 02:04:07 +0800

    Time: 246 ms (18.18%), Space: 172.7 MB (18.18%) - LeetHub

---
+++ 2584-number-of-nodes-with-value-one/2584-number-of-nodes-with-value-one.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    // int numberOfNodes(int n, vector<int>& queries) {
+    //   vector<int> nodeValu(n+1,0);
+    //   for(auto &qu : queries) {
+    //     flip(nodeValu, qu);
+    //   }
+    //   return accumulate(nodeValu.begin(),nodeValu.end(),0);
+    // }
+
+    // void flip(vector<int> &res, int n) {
+    //   if(n >= res.size()) return;
+    //   if(res[n] == 0) res[n] = 1;
+    //   else res[n] = 0;
+    //   flip(res, 2 * n);
+    //   flip(res, 2 * n + 1);
+    // }
+
+    int numberOfNodes(int n, vector<int>& queries) {
+      unordered_map<int,int> mp;
+      int res = 0;
+      for(auto &qu : queries) {
+        mp[qu]++;
+      }
+      dfs(mp, 1, 0, res, n);
+      return res;
+    }
+
+    void dfs(unordered_map<int,int> &mp, int idx, int flip, int &res, int nodeNum) {
+      if(idx > nodeNum) return;
+      flip += mp[idx];
+      if(flip%2 == 1) {
+        res++;
+      }
+      dfs(mp, idx*2, flip, res, nodeNum);
+      dfs(mp, idx*2+1, flip, res, nodeNum);
+    }
+
+};
+
+/*
+ For the first method it will TLE when node 
+ 
+*/
