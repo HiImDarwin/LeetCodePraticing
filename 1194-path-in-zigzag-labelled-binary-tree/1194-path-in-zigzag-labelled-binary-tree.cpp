d365b9bd3b1b1220f15633b3b545768e749ec011
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-13 03:00:13 +0800

    Time: 0 ms (100%), Space: 8.6 MB (13.57%) - LeetHub

---
+++ 1194-path-in-zigzag-labelled-binary-tree/1194-path-in-zigzag-labelled-binary-tree.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    vector<int> pathInZigZagTree(int label) {
+      stack<int> st;
+      vector<int> res;
+      int depth = 1;
+      int original = 1, mirror = 1;
+      res.push_back(1);
+      int height = 1;
+      while(label >= pow(2,height)) {
+        height++;
+      }
+      if (height%2 == 0) {
+        int y = pow(2,height)-1 - label;
+        label = pow(2,height-1) + y;
+      }
+      while(label > 1) {
+        st.push(label & 1);
+        label = label >> 1;
+      }
+      while(!st.empty()) {
+        int way = st.top(); st.pop();
+        if(way == 0) {
+          original = original * 2;
+          mirror = mirror * 2 + 1;
+        } else {
+          original = original * 2 + 1;
+          mirror = mirror * 2;
+        }
+        if(++depth%2 == 0) {
+          res.push_back(mirror);
+        } else {
+          res.push_back(original);
+        }
+      }
+      return res;
+    }
+};
+
+
+/*
+
+triverse tree from mirror route and take the node from two route
+in even level take the mirror node
+in the odd level take the original route node
+how to count out the original route
+use stack store the binary bit of label
+
+
+          1
+     3          2
+  4     5     6    7
+15 14 13 12 11 10 9 8 
+
+
+
+case 1 label in odd line >　calculate as normal
+case 2 label in even line > mirror the label num
+
+*/
