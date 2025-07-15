e2b80febcfe41af39fe606eca7e1df7b77de60f8
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-15 21:34:25 +0800

    Time: 11 ms (43.56%), Space: 29.4 MB (36.44%) - LeetHub

--- 0536-construct-binary-tree-from-string/0536-construct-binary-tree-from-string.cpp
+++ 0536-construct-binary-tree-from-string/0536-construct-binary-tree-from-string.cpp
@@ -1,63 +1,96 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     TreeNode* str2tree(string s) {
+//       if(s.length() == 0) return  nullptr;
+//       int index = 0;
+//       return buildTree(s,index);
+//     }
+
+//     TreeNode* buildTree(string s , int &index) {
+//       int num = 0;
+//       int sign = 1;
+//       if(s[index] == '-') {
+//         sign = -1;
+//         index++;
+//       }
+//       while(index < s.length() && isdigit(s[index])) {
+//         num = num * 10 + (s[index] - '0');
+//         index++;
+//       }
+
+//       TreeNode* node = new TreeNode(num*sign);
+
+//       // while(index < s.length() && s[index] == '(') {
+//       //   index++;
+//       //   if(!node->left) {
+//       //     node->left = buildTree(s,index);
+//       //   } else {
+//       //     node->right = buildTree(s,index);
+//       //   }
+//       // }
+//       if(index < s.length() && s[index] == '(') {
+//         index++;
+//         node->left = buildTree(s,index);
+//       }
+
+//       if(index < s.length() && s[index] == '(') {
+//         index++;
+//         node->right = buildTree(s,index);
+//       }
+
+
+//       if(s[index] == ')'){
+//         index++;
+//       }
+//       return node;
+//     }
+// };
+
 class Solution {
 public:
     TreeNode* str2tree(string s) {
-      if(s.length() == 0) return  nullptr;
-      int index = 0;
-      return buildTree(s,index);
+        if (s.empty()) {
+            return NULL;
+        }
+        int i = 0;
+        return build(s, i);
     }
 
-    TreeNode* buildTree(string s , int &index) {
-      int num = 0;
-      int sign = 1;
-      if(s[index] == '-') {
-        sign = -1;
-        index++;
-      }
-      while(index < s.length() && isdigit(s[index])) {
-        num = num * 10 + (s[index] - '0');
-        index++;
-      }
-
-      TreeNode* node = new TreeNode(num*sign);
-
-      // while(index < s.length() && s[index] == '(') {
-      //   index++;
-      //   if(!node->left) {
-      //     node->left = buildTree(s,index);
-      //   } else {
-      //     node->right = buildTree(s,index);
-      //   }
-      // }
-      if(index < s.length() && s[index] == '(') {
-        index++;
-        node->left = buildTree(s,index);
-      }
-
-      if(index < s.length() && s[index] == '(') {
-        index++;
-        node->right = buildTree(s,index);
-      }
-
-
-      if(s[index] == ')'){
-        index++;
-      }
-      return node;
-    }
+private:
+    TreeNode* build(string& s, int& i) {
+        int start = i;
+        if (s[i] == '-') {
+            ++i;
+        }
+        while (i < s.size() && isdigit(s[i])) {
+            ++i;
+        }
+        int num = stoi(s.substr(start, i - start));
+        TreeNode* root = new TreeNode(num);
+        if (s[i] == '(') {
+            ++i;
+            root->left = build(s, i);
+            ++i;
+        }
+        if (s[i] == '(') {
+            ++i;
+            root->right = build(s, i);
+            ++i;
+        }
+        return root;
+    } 
 };
-
-
 /*
 so
 
