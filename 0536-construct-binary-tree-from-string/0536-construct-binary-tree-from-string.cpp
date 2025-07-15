76062209f70e2fcaa053c1d1fc7f3bcb7c4f3abe
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-15 21:30:53 +0800

    Time: 70 ms (15.07%), Space: 202.3 MB (11.23%) - LeetHub

---
+++ 0536-construct-binary-tree-from-string/0536-construct-binary-tree-from-string.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* str2tree(string s) {
+      if(s.length() == 0) return  nullptr;
+      int index = 0;
+      return buildTree(s,index);
+    }
+
+    TreeNode* buildTree(string s , int &index) {
+      int num = 0;
+      int sign = 1;
+      if(s[index] == '-') {
+        sign = -1;
+        index++;
+      }
+      while(index < s.length() && isdigit(s[index])) {
+        num = num * 10 + (s[index] - '0');
+        index++;
+      }
+
+      TreeNode* node = new TreeNode(num*sign);
+
+      while(index < s.length() && s[index] == '(') {
+        index++;
+        if(!node->left) {
+          node->left = buildTree(s,index);
+        } else {
+          node->right = buildTree(s,index);
+        }
+      }
+
+      if(s[index] == ')'){
+        index++;
+      }
+      return node;
+    }
+};
+
+
+/*
+so
+
+question
+1. is there always two child? if only one can i construct left node?
+2. the input is like
+  4(1(2)(3))(3)
+3. i suggest the paenthesis will always be valid pair ()
+*/
