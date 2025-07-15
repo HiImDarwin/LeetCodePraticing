4d410b4a3fd45f2d14e6c3dac1a9bbd37f184571
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-16 00:53:44 +0800

    Time: 3 ms (86.57%), Space: 14 MB (61.03%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,31 +1,50 @@
 class Solution {
 public:
+    // string removeDuplicates(string s, int k) {
+    //   stack<pair<char,int>> st;
+    //   int left = 0, right = 0;
+    //   while(right < s.length()) {
+    //     int charCount = 1;
+    //     if(!st.empty() && st.top().first == s[left]) {
+    //       charCount += st.top().second;
+    //       st.pop();
+    //     }
+    //     while(right+1 < s.length() && s[right] == s[right+1]) {
+    //       right++;
+    //       charCount++;
+    //     }
+    //     if((charCount)%k != 0) {
+    //       st.push({s[left],charCount%k});
+    //     }
+    //     right++;
+    //     left = right;
+    //   }
+    //   string res;
+    //   while(!st.empty()) {
+    //     res += string(st.top().second,st.top().first);
+    //     st.pop();
+    //   }
+    //   reverse(res.begin(),res.end());
+    //   return res;        
+    // }
     string removeDuplicates(string s, int k) {
-      stack<pair<char,int>> st;
-      int left = 0, right = 0;
-      while(right < s.length()) {
-        int charCount = 1;
-        if(!st.empty() && st.top().first == s[left]) {
-          charCount += st.top().second;
-          st.pop();
+      int i = 0;
+      int j = 0;
+      vector<int>count(s.size());
+      while(j < s.size()) {
+        s[i] = s[j];
+        count[i] = 1;
+        if(i>0 && s[i-1] == s[i]) {
+          count[i] += count[i-1];
         }
-        while(right+1 < s.length() && s[right] == s[right+1]) {
-          right++;
-          charCount++;
+        if(count[i] == k) {
+          i = i-k;
         }
-        if((charCount)%k != 0) {
-          st.push({s[left],charCount%k});
-        }
-        right++;
-        left = right;
+        i++;
+        j++;
       }
-      string res;
-      while(!st.empty()) {
-        res += string(st.top().second,st.top().first);
-        st.pop();
-      }
-      reverse(res.begin(),res.end());
-      return res;        
+      return s.substr(0,i);
+    
     }
 };
 
