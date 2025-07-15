b376f15e08875547ec9ec796c4db9bead863eb81
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-15 16:18:18 +0800

    Time: 0 ms (100%), Space: 31.2 MB (93.75%) - LeetHub

--- 1883-find-distance-in-a-binary-tree/1883-find-distance-in-a-binary-tree.cpp
+++ 1883-find-distance-in-a-binary-tree/1883-find-distance-in-a-binary-tree.cpp
@@ -9,40 +9,65 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int findDistance(TreeNode* root, int p, int q) {
+//         bool found = false;
+//         int pathLength = pathlongToLca(root, p, q, found);
+//         return found ? pathLength : 0;
+//     }
+        
+
+//     int pathlongToLca(TreeNode* root, int p, int q, bool &found) {
+//       if(!root) return 0;
+//       int left = pathlongToLca(root->left, p ,q, found);
+//       int right = pathlongToLca(root->right, p, q, found);
+//       if(found) return max(left,right);
+//       if(left > 0 && right > 0) {
+//         found = true;
+//         return left + right;
+//       }
+//       int dist = -1;
+//       if(root->val == p || root->val == q) {
+//         dist = 0;
+//       }
+//       if(dist == 0 && (left || right)) {
+//         found = true;
+//         return max(left,right);
+//       }
+//       if(left > 0 || right > 0 || dist == 0) {
+//         return max({left,right,dist})+1;
+//       }
+//       return  0;
+
+//     }
+// };
 class Solution {
 public:
     int findDistance(TreeNode* root, int p, int q) {
-        bool found = false;
-        int pathLength = pathlongToLca(root, p, q, found);
-        return found ? pathLength : 0;
-    }
-        
-
-    int pathlongToLca(TreeNode* root, int p, int q, bool &found) {
-      if(!root) return 0;
-      int left = pathlongToLca(root->left, p ,q, found);
-      int right = pathlongToLca(root->right, p, q, found);
-      if(found) return max(left,right);
-      if(left > 0 && right > 0) {
-        found = true;
-        return left + right;
-      }
-      int dist = -1;
-      if(root->val == p || root->val == q) {
-        dist = 0;
-      }
-      if(dist == 0 && (left || right)) {
-        found = true;
-        return max(left,right);
-      }
-      if(left > 0 || right > 0 || dist == 0) {
-        return max({left,right,dist})+1;
-      }
-      return  0;
+        auto lca = [&](this auto&& lca, TreeNode* root, int p, int q)->TreeNode*{
+            if (!root || root->val == p || root->val == q) return root;
+            auto l = lca(root->left, p, q);
+            auto r = lca(root->right, p, q);
+            if (!l) return r;
+            if (!r) return l;
+            return root;
+        };
+        auto _lca = lca(root, p, q);
+        auto get_depth = [&](this auto &&get_depth, TreeNode* root, int val, int depth) ->int {
+            if (!root) return -1;
+            if (root->val == val) {
+                return depth;
+            }
+            int l = get_depth(root->left, val, depth + 1);
+            int r = get_depth(root->right, val, depth + 1);
+            return (l == -1) ? r : l;
+            
+        };
+        return get_depth(_lca, p, 0) + get_depth(_lca, q, 0);
 
     }
 };
-
 /*
 two node meet and dont need to add length
   left and right occur  
