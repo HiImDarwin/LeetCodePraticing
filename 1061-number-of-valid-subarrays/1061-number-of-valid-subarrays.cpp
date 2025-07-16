5c409c26b4244771fc536e5a4be55853f654de9c
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-17 03:17:19 +0800

    Time: 6 ms (88%), Space: 47 MB (76%) - LeetHub

--- 1061-number-of-valid-subarrays/1061-number-of-valid-subarrays.cpp
+++ 1061-number-of-valid-subarrays/1061-number-of-valid-subarrays.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
+    // int validSubarrays(vector<int>& nums) {
+    //   nums.push_back(INT_MIN);
+    //   stack<int> st;
+    //   int count = 0;
+    //   for(int i = 0; i < nums.size(); ++i) {
+    //     while (!st.empty() && nums[i] < nums[st.top()]) {
+    //       count += i - st.top();
+    //       st.pop();
+    //     }
+    //     st.push(i);
+    //   }
+    //   return count;
+    // }
     int validSubarrays(vector<int>& nums) {
-      nums.push_back(INT_MIN);
-      stack<int> st;
+      stack<int> indexSt;
       int count = 0;
-      for(int i = 0; i < nums.size(); ++i) {
-        while (!st.empty() && nums[i] < nums[st.top()]) {
-          count += i - st.top();
-          st.pop();
+      for(int i = nums.size()-1; i >= 0; i--) {
+        while(!indexSt.empty() && nums[i] <= nums[indexSt.top()]) {
+          indexSt.pop();
         }
-        st.push(i);
+        if(indexSt.empty()) {
+          count += nums.size()-i;
+        } else {
+          count += indexSt.top() - i;
+        }
+        indexSt.push(i);
       }
       return count;
     }
+      
 };
 
 
 /*
-1 2 3 4 9 6 7 8 
-
-a sub array is valid must be strictly increasing
-
+  find next smaller element;
 
 
 */
