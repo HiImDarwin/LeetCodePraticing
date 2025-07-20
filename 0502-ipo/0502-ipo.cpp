a1a4cbe2ee841cb40f8b29f20e61496ebf51857f
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-20 17:30:05 +0800

    Time: 75 ms (57.96%), Space: 131.4 MB (49.27%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -5,18 +5,14 @@ public:
       priority_queue<int> pq;
 
       for(int i = 0; i < capital.size(); ++i) {
-        project.push_back({capital[i],i});
+        project.push_back({capital[i],profits[i]});
       }
       sort(project.begin(),project.end());
-      vector<pair<int,int>>::iterator prev_index = project.begin();
-      vector<pair<int,int>>::iterator it = project.begin();
+      int i = 0;
       while(k--) {
-        it = upper_bound(it, project.end(), w, [](const int& w, const pair<int,int> &a) {
-          return w < a.first;
-        });
-        while(prev_index != it) {
-          pq.push(profits[prev_index -> second]);
-          prev_index++;
+        while(i < profits.size() && project[i].first <= w) {
+          pq.push(project[i].second);
+          i++;
         }
         if (pq.empty()) break;
         w += pq.top();
