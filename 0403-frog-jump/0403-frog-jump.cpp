c28e54fe431ca68b452bf77dbf354d2c8241ac28
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-24 19:14:27 +0800

    Time: 78 ms (67.31%), Space: 33.9 MB (72.63%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,11 +1,14 @@
-class Solution {
-  unordered_map<string, bool> memo;
-public:
-    bool canCross(vector<int>& stones) {
+class Solution 
+{
+    unordered_map<string, bool> memo;
+  public:
+    bool canCross(vector<int>& stones) 
+    {
       unordered_set<int> stoneSet(stones.begin(), stones.end());
       return dfs(stones.back(), stoneSet, 0, 0);
     }
-    bool dfs(const int target, unordered_set<int> &stoneSet, int pos, int step) {
+    bool dfs(const int target, unordered_set<int> &stoneSet, int pos, int step) 
+    {
       string key = to_string(pos) + ',' + to_string(step);
       if (memo.find(key) != memo.end()) {
         return memo[key];
