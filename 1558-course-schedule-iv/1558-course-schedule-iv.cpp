55df65a3a46a3f6b53e666e3fb6000d4c342fbb8
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-28 22:01:07 +0800

    Time: 47 ms (63.43%), Space: 76.1 MB (27.19%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.cpp
+++ 1558-course-schedule-iv/1558-course-schedule-iv.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-      
+      const int n = numCourses;
+      vector<bitset<100>> pre(n);
       vector<unordered_set<int>> next(numCourses);
       vector<int> inDegree(numCourses,0);
-      vector<unordered_set<int>> preSet(numCourses);
+      //vector<unordered_set<int>> preSet(numCourses);
 
       for (auto edge : prerequisites) {
         next[edge[0]].insert(edge[1]);
@@ -12,9 +13,16 @@ public:
       }
 
       queue<int> q;
-      for (int i = 0; i < numCourses; ++i) {
-        preSet[i].insert(i);
-        if(inDegree[i] == 0) {
+      // for (int i = 0; i < numCourses; ++i) {
+      //   preSet[i].insert(i);
+      //   if(inDegree[i] == 0) {
+      //     q.push(i);
+      //   }
+      // }
+
+      for (int i = 0; i < n; ++i) {
+        pre[i].set(i);
+        if (inDegree[i] == 0) {
           q.push(i);
         }
       }
@@ -24,9 +32,10 @@ public:
         q.pop();
 
         for (int next: next[cur]) {
-          for (auto x : preSet[cur]) {
-            preSet[next].insert(x);
-          }
+          // for (auto x : preSet[cur]) {
+          //   preSet[next].insert(x);
+          // }
+          pre[next] |= pre[cur];
           inDegree[next]--;
           if (inDegree[next] == 0) {
             q.push(next);
@@ -35,7 +44,8 @@ public:
       }
       vector<bool> res;
       for(auto query : queries) {
-        res.push_back(preSet[query[1]].find(query[0]) != preSet[query[1]].end());
+        //res.push_back(preSet[query[1]].find(query[0]) != preSet[query[1]].end());
+        res.push_back(pre[query[1]].test(query[0]));
       }
       return res;
     }
