590a6539b3e840db1f6ae4a60bb4b465753b172e
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-29 01:39:11 +0800

    Time: 53 ms (88.73%), Space: 75 MB (73.63%) - LeetHub

---
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) 
+    {
+      vector<bitset<1000>> ancestorMap(n);
+      vector<int> inDegree(n,0);
+      vector<vector<int>> next(n);
+
+      for (auto edge : edges) {
+        int from = edge[0];
+        int to   = edge[1];
+        inDegree[to]++;
+        next[from].push_back(to);
+      }
+
+      queue<int> nodeQueue;
+      //vector<bool> visited(n);
+
+      for (int i = 0; i < n; ++i) {
+        ancestorMap[i].set(i,true);
+        if (inDegree[i] == 0) {
+          nodeQueue.push(i);
+          //visited[i] = true;
+        }
+      }
+
+      while (!nodeQueue.empty()) {
+        int curr = nodeQueue.front();
+        nodeQueue.pop();
+        
+        for (int nextNode : next[curr]) {
+          inDegree[nextNode]--;
+          ancestorMap[nextNode] |= ancestorMap[curr];
+          if(inDegree[nextNode] == 0) {
+            nodeQueue.push(nextNode);
+          }
+        }
+      }
+      vector<vector<int>> res;
+      for (int i = 0; i < n; ++i) {
+        vector<int> tmp;
+        for (int j = 0; j < n; ++j) {
+          if (i == j) continue;
+          if(ancestorMap[i][j]) {
+            tmp.push_back(j);
+          }
+        }
+        res.push_back(tmp);
+      }
+      return res;
+    }
+};
+
+/*
+
+direct 
+acyclic
+
+*/
