6d6f8c50453bb925505097b25b1b7e52d171e3bb
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-07-30 04:30:18 +0800

    Time: 0 ms (100%), Space: 18.7 MB (76.92%) - LeetHub

--- 1651-shuffle-string/1651-shuffle-string.cpp
+++ 1651-shuffle-string/1651-shuffle-string.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
+    // string restoreString(string s, vector<int>& indices) {
+    //   string res = s;
+    //   for(int i = 0; i < s.length(); ++i) {
+    //     res[indices[i]] = s[i];
+    //   }
+    //   return res;
+    // }
     string restoreString(string s, vector<int>& indices) {
-      string res = s;
-      for(int i = 0; i < s.length(); ++i) {
-        res[indices[i]] = s[i];
+      for (int i = 0; i < s.length(); ++i) {
+        while(indices[i] != i) {
+          swap(s[i], s[indices[i]]);
+          swap(indices[i],indices[indices[i]]);
+        }
       }
-      return res;
+      return s;
     }
 };
