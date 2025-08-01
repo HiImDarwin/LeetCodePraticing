df6c9c5bbceb76d98ed4d49de77e2f92054ee993
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-02 02:34:09 +0800

    Time: 18 ms (89.58%), Space: 12.9 MB (74.19%) - LeetHub

--- 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -33,28 +33,47 @@ class Solution {
 //       return res;
 //     }
   public:
-    int numberOfWays(int n, int x) 
-    {
-      int MOD = 1e9 + 7;
-      int y = 1;
-      while(pow(y,x) <= n) {
-        y++;
-      }
+    // int numberOfWays(int n, int x) 
+    // {
+    //   int MOD = 1e9 + 7;
+    //   int y = 1;
+    //   while(pow(y,x) <= n) {
+    //     y++;
+    //   }
       
-      vector<vector<int>> dp(n+1,vector<int>(y,0));
-      // for (int j = 0; j < y; ++j) {
-      //   dp[0][j] = 1;
-      // }
-      dp[0][0] = 1;
-      for(int i = 0; i <=n; ++i) {
-        for(int j = 1; j < y; j++) {
-          dp[i][j] = dp[i][j - 1];
-          if(i - (int)pow(j,x) >= 0) {
-            dp[i][j] = (dp[i][j] + dp[i - (int)pow(j,x)][j-1]) % MOD;
-          }
+    //   vector<vector<int>> dp(n+1,vector<int>(y,0));
+    //   // for (int j = 0; j < y; ++j) {
+    //   //   dp[0][j] = 1;
+    //   // }
+    //   dp[0][0] = 1;
+    //   for(int i = 0; i <=n; ++i) {
+    //     for(int j = 1; j < y; j++) {
+    //       dp[i][j] = dp[i][j - 1];
+    //       if(i - (int)pow(j,x) >= 0) {
+    //         dp[i][j] = (dp[i][j] + dp[i - (int)pow(j,x)][j-1]) % MOD;
+    //       }
+    //     }
+    //   }
+    //   return dp[n][y-1];
+    // }
+
+    int numberOfWays(int n, int x)  {
+      int MOD = 1e9 + 7;
+      vector<long long> dp(n+1,0);
+      dp[0] = 1;
+
+      for (int i = 1; i <= n; i++) { // 循環factor
+        long long num = 1;
+        for (int t = 0; t < x; t++) {
+          num *= i;
+        }
+        // 一維dp 從尾巴往前， 避免新值把舊值刷掉
+        for (int s = n; s >= num; s--) { // 循環用這個factor可刷新的sum
+          dp[s] += dp[s-num]; 
+          dp[s] %= MOD;
         }
       }
-      return dp[n][y-1];
+      return dp[n];
     }
 };
 
@@ -65,4 +84,10 @@ class Solution {
 dp[i][j] is the unique splite ways of num i , s.t. j is the maximum factor is not larger than j
 dp[i][j] = dp[i][j-1].     // 不用j
 dp[i][j] += dp[i-j^x][j-1] // 用j
+*/
+
+
+/*
+
+
 */
