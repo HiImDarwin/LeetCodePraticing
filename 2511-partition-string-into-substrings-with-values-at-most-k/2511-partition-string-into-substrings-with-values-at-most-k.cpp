8c80444a360f8b70831c93770e8f424c55683147
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-04 21:29:34 +0800

    Time: 27 ms (24.46%), Space: 16.6 MB (11.41%) - LeetHub

---
+++ 2511-partition-string-into-substrings-with-values-at-most-k/2511-partition-string-into-substrings-with-values-at-most-k.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minimumPartition(string s, int k) {
+      int n = s.length();
+      int m = to_string(k).length();
+      vector<int> dp(n+1,INT_MAX);
+      
+      for(int i = 0; i < n; ++i) {
+        if(m == 1 && s[i] - '0' > k){
+          return -1;
+        } 
+      }
+
+      s = "#" +s ;
+      dp[0] = 0;
+
+      for (int i = 1; i <= n; ++i) {
+        if (i - m + 1 >=1 && stoi(s.substr(i-m+1,m)) <= k){
+          dp[i] = dp[i-m] + 1;
+        } else {
+          dp[i] = dp[max(0, i - m +1)] + 1;
+        }
+      }
+      return dp[n];
+    }
+    
+};
+
+
+/*
+first though is greedy 
+is there any condition that greedy may have problem?
+
+"16 54 6 2", k = 60
+
+"1 6 54 6 2"      
+
+"16 54 6 2", k = 66
+
+"16 54 62"
+"1 65 46 2"
+
+*/
