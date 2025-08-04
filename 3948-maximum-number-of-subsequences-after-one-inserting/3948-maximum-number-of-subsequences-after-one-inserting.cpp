7b3cefe44a6b225009752f11b5e1115dfbdfac4d
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-04 17:16:36 +0800

    Time: 324 ms (5%), Space: 152.4 MB (5.02%) - LeetHub

---
+++ 3948-maximum-number-of-subsequences-after-one-inserting/3948-maximum-number-of-subsequences-after-one-inserting.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    long long numOfSubsequences(string s) {
+      int n = s.length();
+      vector<vector<long long>> memo(n+1,vector<long long>(4,0));
+      long long total = 0;
+      
+      for (int i = 1; i <= n; ++i) {
+        memo[i][0] = memo[i-1][0];
+        memo[i][1] = memo[i-1][1];
+        if(s[i-1] == 'L') {
+          memo[i][0]++;
+        } else if (s[i-1] == 'C') {
+          memo[i][1] += memo[i][0];
+        } else if (s[i-1] == 'T') {
+          total += memo[i][1];
+        }
+        //cout <<  memo[i][0] << " " << memo[i][1] << endl;
+      }
+      //cout << "total " << total << endl;
+      for (int i = n-1; i >= 0 ; --i) {
+        memo[i][2] = memo[i+1][2];
+        memo[i][3] = memo[i+1][3];
+        if(s[i] == 'T') {
+            memo[i][3]++;
+        } else if (s[i] == 'C') {
+            memo[i][2] += memo[i][3];
+        }
+        //cout <<  memo[i][2] << " " << memo[i][3] << endl;
+      }
+      
+      long long maxIncrease = 0;
+      for (int i = 0; i <= n; i++) {
+        long long ifL = memo[i][2];
+        long long ifC = memo[i][0] * memo[i][3];
+        long long ifT = memo[i][1];
+        //cout << memo[i][0] << " " << memo[i][1] << " " <<  memo[i][2] << " " << memo[i][3] << endl;
+        //cout << ifL << " " << ifC << " " <<  ifT << endl;
+        maxIncrease = max({maxIncrease, ifL, ifC, ifT});
+      }
+      //cout << "maxIncrease " << maxIncrease << endl;
+      return total + maxIncrease;
+    }
+};
+
+/*
+
+L CC LL TTT CC T
+
+
+
+at each position you need to know the leagel C and T behind you
+T LC組合會再翻倍
+C 前面的L 與後面的T可以跟我搭配
+L 後面的CT組合會再翻倍
+
+紀錄L > forward
+紀錄LC > forward
+
+紀錄CT < backward
+紀錄T < backward
+
+*/
