a715471ca572f49fa24baa9b8f106fef92211dbb
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-10 02:00:13 +0800

    Time: 0 ms (100%), Space: 11.2 MB (82.26%) - LeetHub

---
+++ 1228-minimum-cost-tree-from-leaf-values/1228-minimum-cost-tree-from-leaf-values.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int mctFromLeafValues(vector<int>& nums) {
+      int n = nums.size();
+      vector<int> prevGreater(n,INT_MAX),nextGreater(n,INT_MAX);
+      stack<int> st1,st2;
+      for (int i = 0; i < n; i++) {
+        // in eaqual satuation, we set right side be bigger 
+        while (!st1.empty() && nums[i] >= st1.top()) { 
+          st1.pop();
+        }
+        if (!st1.empty()) {
+          prevGreater[i] = st1.top();
+        }
+        st1.push(nums[i]);
+      }
+      for (int i = n - 1; i >= 0; i--) {
+         // in eaqual satuation, we set right side be bigger 
+        while (!st2.empty() && nums[i] > st2.top()) { 
+          st2.pop();
+        }
+        if (!st2.empty()) {
+          nextGreater[i] = st2.top();
+        }
+        st2.push(nums[i]);
+      }
+      int total = 0;
+      for(int i = 0; i < n; i++) {
+        int mini = min(prevGreater[i],nextGreater[i]);
+        if(mini == INT_MAX) {
+          continue;
+        }
+        total += nums[i] * mini;
+      }
+      
+      return total;
+    }
+};
+
+/*
+基本解法
+[i X X X X k] [X X X X X X j]
+      Y1              Y2
+dp[i][j] = min (max[i][k] * max[k+1][j] + dp[i][k] + dp[k+1][j])
+
+for int len = ....
+  for int i = ....
+    int j = i + len - 1;
+    for k = i:j-1
+      dp[i][j] = ...
+
+
+monotonic stack
+除了最大值以外，每個值會被他左邊較大或是右邊較大給消掉
+3 1 2 4 
+1可能被3消掉 可能被2消掉 那麼你想讓cost最小 那就是選 1*2
+所以這題就是求每個點的 previous/next greater num
+*/
