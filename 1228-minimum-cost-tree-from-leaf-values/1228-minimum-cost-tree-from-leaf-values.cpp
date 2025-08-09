6049f5e05f132c22299b94fed803b649bd96db4b
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-10 02:22:16 +0800

    Time: 4 ms (74.45%), Space: 12.4 MB (61.71%) - LeetHub

--- 1228-minimum-cost-tree-from-leaf-values/1228-minimum-cost-tree-from-leaf-values.cpp
+++ 1228-minimum-cost-tree-from-leaf-values/1228-minimum-cost-tree-from-leaf-values.cpp
@@ -1,39 +1,60 @@
 class Solution {
 public:
+    // int mctFromLeafValues(vector<int>& nums) {
+    //   int n = nums.size();
+    //   vector<int> prevGreater(n,INT_MAX),nextGreater(n,INT_MAX);
+    //   stack<int> st1,st2;
+    //   for (int i = 0; i < n; i++) {
+    //     // in eaqual satuation, we set right side be bigger 
+    //     while (!st1.empty() && nums[i] >= st1.top()) { 
+    //       st1.pop();
+    //     }
+    //     if (!st1.empty()) {
+    //       prevGreater[i] = st1.top();
+    //     }
+    //     st1.push(nums[i]);
+    //   }
+    //   for (int i = n - 1; i >= 0; i--) {
+    //      // in eaqual satuation, we set right side be bigger 
+    //     while (!st2.empty() && nums[i] > st2.top()) { 
+    //       st2.pop();
+    //     }
+    //     if (!st2.empty()) {
+    //       nextGreater[i] = st2.top();
+    //     }
+    //     st2.push(nums[i]);
+    //   }
+    //   int total = 0;
+    //   for(int i = 0; i < n; i++) {
+    //     int mini = min(prevGreater[i],nextGreater[i]);
+    //     if(mini == INT_MAX) {
+    //       continue;
+    //     }
+    //     total += nums[i] * mini;
+    //   }
+      
+    //   return total;
+    // }
+
     int mctFromLeafValues(vector<int>& nums) {
       int n = nums.size();
-      vector<int> prevGreater(n,INT_MAX),nextGreater(n,INT_MAX);
-      stack<int> st1,st2;
-      for (int i = 0; i < n; i++) {
-        // in eaqual satuation, we set right side be bigger 
-        while (!st1.empty() && nums[i] >= st1.top()) { 
-          st1.pop();
-        }
-        if (!st1.empty()) {
-          prevGreater[i] = st1.top();
-        }
-        st1.push(nums[i]);
+      auto dp = vector<vector<int>>(n,vector<int>(n,INT_MAX));
+      auto large = vector<vector<int>>(n,vector<int>(n,INT_MIN));
+      for (int i = 0; i < n; ++i) {
+        dp[i][i] = 0;
+        large[i][i] = nums[i];
       }
-      for (int i = n - 1; i >= 0; i--) {
-         // in eaqual satuation, we set right side be bigger 
-        while (!st2.empty() && nums[i] > st2.top()) { 
-          st2.pop();
+
+      for (int len = 2; len <= n; len++) {
+        for (int i = 0; i <= n - len; ++i) {
+          int j = i + len - 1;
+          for (int k = i; k < j; k++) {
+             dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] + large[i][k] * large[k+1][j]);
+             large[i][j] = max(large[i][k],large[k+1][j]);
+          }
         }
-        if (!st2.empty()) {
-          nextGreater[i] = st2.top();
-        }
-        st2.push(nums[i]);
       }
-      int total = 0;
-      for(int i = 0; i < n; i++) {
-        int mini = min(prevGreater[i],nextGreater[i]);
-        if(mini == INT_MAX) {
-          continue;
-        }
-        total += nums[i] * mini;
-      }
-      
-      return total;
+      return dp[0][n-1];
     }
 };
 
