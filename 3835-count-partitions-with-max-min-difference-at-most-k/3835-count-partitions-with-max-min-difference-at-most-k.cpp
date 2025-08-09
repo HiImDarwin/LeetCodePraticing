cb6d673203682d8c9e0636ec1b3283cd40728121
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-09 16:34:33 +0800

    Time: 61 ms (71.22%), Space: 155.8 MB (63.03%) - LeetHub

--- 3835-count-partitions-with-max-min-difference-at-most-k/3835-count-partitions-with-max-min-difference-at-most-k.cpp
+++ 3835-count-partitions-with-max-min-difference-at-most-k/3835-count-partitions-with-max-min-difference-at-most-k.cpp
@@ -3,68 +3,62 @@ public:
     int countPartitions(vector<int>& nums, int k) {
       int n = nums.size();
       nums.insert(nums.begin(),0);
-
-      long M = 1e9 + 7;
-
-      vector<int> dp(n+1);
-      vector<int> presum(n+1);
+      int MOD = 1e9 + 7;
+      vector<int> dp(n+1,0);
+      vector<long long> prefixSum(n+1);
+      prefixSum[0] = 1;
       dp[0] = 1;
-      presum[0] = 1;
-
-      deque<int> dq1;  // maximum in slinding window
-      deque<int> dq2;  // minimum in sliding window
-      int L = 1;
-      for(int i = 1; i <= n; ++i) {
-        // update queue
-        while(!dq1.empty() && nums[dq1.back()] < nums[i]) {
-          dq1.pop_back();
+      deque<int> maxQueue,minQueue;
+      int left = 1;
+      
+      for(int i = 1; i < nums.size(); i++) {
+        while (!maxQueue.empty() && nums[maxQueue.back()] < nums[i]) {
+          maxQueue.pop_back();
         }
-        dq1.push_back(i);
-        while(!dq2.empty() && nums[dq2.back()] > nums[i]) {
-          dq2.pop_back();
+        while (!minQueue.empty() && nums[minQueue.back()] > nums[i]) {
+          minQueue.pop_back();
         }
-        dq2.push_back(i);
-        //find L
-        while(L < i && nums[dq1.front()] - nums[dq2.front()] > k) {
-          if(dq1.front() == L) dq1.pop_front();
-          if(dq2.front() == L) dq2.pop_front();
-          L++;
+        maxQueue.push_back(i);
+        minQueue.push_back(i);
+
+        while(left <=i && nums[maxQueue.front()] - nums[minQueue.front()] > k) {
+          if(maxQueue.front() == left) {
+            maxQueue.pop_front();
+          }
+          if(minQueue.front() == left) {
+            minQueue.pop_front();
+          }
+          left++;
         }
+        // 如果我是 i 的合法區間 [j...i] 那麼我需要的和是 j-1 + j +... + i-1 所以是prefixSum[i-1] - prefixSum[j-2]
+        //left是對應最左邊的 element
 
-        dp[i] = presum[i-1] - (L >= 2 ? presum[L-2] : 0);
-        dp[i] = (dp[i] + M)% M;
-        presum[i] = presum[i-1]+ dp[i];
-        presum[i] = (presum[i] + M) % M; 
 
+        dp[i] = prefixSum[i-1] - (left - 2 >= 0 ? prefixSum[left - 2] : 0);
+        prefixSum[i] = prefixSum[i - 1] + dp[i];
+
+        dp[i] = (dp[i] + MOD) % MOD;
+        prefixSum[i] = (prefixSum[i] + MOD) % MOD;          
       }
+
       return dp[n];
     }
 };
 
-
 /*
-1. How to calculate 
-for each i, we can find a farest left element at L can make it a segment
-when we use L~i segment we got [1....L-1] + [L~1] = dp[L-1]
-dp[i] = dp[L-1]+ dp[L] + dp[L+1] + .... + dp[i-1];
 
-2. How to find L 
-Each segment can view as sliding window
-The difference of maximum and minimum value in window < k
-if the window go right and didn't meet bigger or smaller than the window grow > monotonic stack
+dp[i] =  total number of ways to partition from 0~i
+state transfer dp[i] = sumup(dp[j-1]:dp[i-1]);
 
-maximum deque  m
-minimum deque  n
-if m-n > k then popfront to find second max or second min;
-if m-n < k then i ~ min(index(m), index(n)) is the window;
+X X X X X X X i
+      ^
+      j
+      max min - i <=k
 
+      
+      
+* dp[i]
+* prefixsum for dp[i];
+* monotonic queue to solve segment max/min problem
 
-For speedup
-it's a accumulate of array -> prefix sum
-
-Technic in this 
-DP
-monotonic stack
-prefix sum
-sliding window
 */
