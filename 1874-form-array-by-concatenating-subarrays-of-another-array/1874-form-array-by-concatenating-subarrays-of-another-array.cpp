9bb95a278628874db03265c8527ec27bcd6c5c97
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-11 20:59:47 +0800

    Time: 0 ms (100%), Space: 18.4 MB (26.17%) - LeetHub

--- 1874-form-array-by-concatenating-subarrays-of-another-array/1874-form-array-by-concatenating-subarrays-of-another-array.cpp
+++ 1874-form-array-by-concatenating-subarrays-of-another-array/1874-form-array-by-concatenating-subarrays-of-another-array.cpp
@@ -1,27 +1,72 @@
 class Solution {
 public:
+    // bool canChoose(vector<vector<int>>& groups, vector<int>& nums) {
+    //   int cur = 0;
+    //   for (int i = 0; i < groups.size(); i++) {
+    //     if (cur >= nums.size()) return false;
+    //     cur = find(nums, cur, groups[i]);
+    //     if (cur == -1) return false;
+    //     cur += groups[i].size();
+    //   }
+    //   return true;
+    // }
+
+    // int find(vector<int>& nums, int idx, vector<int>& pattern) {
+    //   int j = 0;
+    //   for (int i = idx; i < nums.size(); i++) {
+    //     if (nums[i] == pattern[j]) {
+    //       j++;
+    //       if (j == pattern.size()) {
+    //         return i - j + 1;
+    //       }
+    //     } else {
+    //       i = i - j;
+    //       j = 0;
+    //     }
+    //   }
+    //   return -1;
+    // }
     bool canChoose(vector<vector<int>>& groups, vector<int>& nums) {
+      int n = groups.size();
       int cur = 0;
-      for (int i = 0; i < groups.size(); i++) {
-        if (cur >= nums.size()) return false;
-        cur = find(nums, cur, groups[i]);
+      for (int i = 0; i < n; i++) {
+        cur = kmp(groups[i],cur, nums);
         if (cur == -1) return false;
         cur += groups[i].size();
       }
       return true;
     }
 
-    int find(vector<int>& nums, int idx, vector<int>& pattern) {
-      int j = 0;
-      for (int i = idx; i < nums.size(); i++) {
+    int kmp(vector<int>& pattern, int idx, vector<int>& nums) {
+      int n = pattern.size();
+      vector<int> lsp(n,0);
+      int len = 0;
+      if (idx >= nums.size()) return -1;
+
+      for (int i = 1; i < n; ++i) {
+        while (len > 0 && pattern[i] != pattern[len]) {
+          len = lsp[len-1];
+        }
+        if (pattern[i] == pattern[len]) {
+          len++;
+        }
+        lsp[i] = len;
+      }
+
+      int i = idx, j = 0;
+      while (i < nums.size()) {
         if (nums[i] == pattern[j]) {
+          i++;
           j++;
-          if (j == pattern.size()) {
-            return i - j + 1;
+          if (j == n) {
+            return i - j;
           }
         } else {
-          i = i - j;
-          j = 0;
+          if (j > 0) {
+            j = lsp[j-1];
+          } else  {
+            i++;
+          }
         }
       }
       return -1;
