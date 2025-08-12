b05f0d08796ba5dae6c122897f01253f6037a788
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-13 01:29:47 +0800

    Time: 2 ms (98.22%), Space: 11.3 MB (60.56%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,35 +1,45 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int len = s.length();
-        if(len <= 1) return s;
-        int max_len = 1;
-        
-        std::string maxStr = s.substr(0,1);
-        s = std::regex_replace(s,std::regex(""), "#");
-        std::vector<int> dp(s.length(),0);
-        int center = 0;
-        int right = 0;
-        for (int i = 0; i<s.length(); i++) {
-            if (i<right) {
-                dp[i] = std::min(right-i, dp[2*center-i]);
-            }
-
-            while (i-dp[i]-1 >=0 && i+dp[i] +1<s.length() && s[i-dp[i]-1] == s[i+dp[i]+1]) {
-                dp[i]++;
-            }
-
-            if(i +dp[i] >right) {
-                center = i;
-                right = i+dp[i];
-            }
-
-            if (dp[i] > max_len) {
-                max_len = dp[i];
-                maxStr =s.substr(i-dp[i], 2*dp[i]+1);
-                maxStr.erase(std::remove(maxStr.begin(), maxStr.end(), '#'), maxStr.end());
-            }
+      int n = s.length();
+      string t = "#";
+      for (int i = 0; i < n; ++i) {
+        t += s[i];
+        t += '#';
+      }
+      int m = t.length();
+      int maxCenter = -1;
+      int maxRight = -1;
+      vector<int> P(m);
+      for (int i = 0; i < m; ++i) {
+        int r = 0;
+        if (i < maxRight) {
+          int j = maxCenter * 2 - i;
+          r = min(P[j], maxRight - i);
+          while (i - r >= 0 && i + r < m && t[i - r] == t[i + r]) {
+            r++;
+          }
+        } else {
+          r = 0;
+          while (i - r >= 0 && i + r < m && t[i - r] == t[i + r]) {
+            r++;
+          }
         }
-        return maxStr;
+        P[i] = r - 1;
+        if (i + P[i] > maxRight) {
+          maxCenter = i;
+          maxRight = i + P[i];
+        }
+      }
+      int center = -1;
+      int maxLen = -1;
+      for (int i = 0; i < m; ++i) {
+        if (P[i] > maxLen) {
+          center = i;
+          maxLen = P[i];
+        }
+      }
+
+      return s.substr(center/2 - maxLen/2 , maxLen);
     }
 };
