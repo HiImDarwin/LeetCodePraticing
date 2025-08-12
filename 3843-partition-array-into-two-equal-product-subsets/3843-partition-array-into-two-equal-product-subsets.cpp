348b20a14f909f0744083c540af0d7ac9e5d2927
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-12 15:25:06 +0800

    Time: 0 ms (100%), Space: 26.8 MB (14.04%) - LeetHub

--- 3843-partition-array-into-two-equal-product-subsets/3843-partition-array-into-two-equal-product-subsets.cpp
+++ 3843-partition-array-into-two-equal-product-subsets/3843-partition-array-into-two-equal-product-subsets.cpp
@@ -1,24 +1,47 @@
 class Solution {
     int n;
 public:
+    // bool checkEqualPartitions(vector<int>& nums, long long target) {
+    //   n = nums.size();
+    //   return dfs(nums, 0, 1, 1, target);
+    // }
+
+
+    // bool dfs(vector<int>& nums, int index, long long sum1, long long sum2, const long long& target) {
+      
+    //   if (target%sum1 != 0  || target%sum2 != 0) return false;
+    //   if (index == n && sum1 == target && sum2 == target) return true;
+    //   if (index == n) return false;
+      
+    //   if (dfs(nums, index + 1, sum1 * nums[index], sum2, target)) {
+    //     return true;
+    //   }
+    //   if (dfs(nums, index + 1, sum1, sum2 * nums[index], target)) {
+    //     return true;
+    //   }
+    //   return false;
+    // }
+    typedef __int128 ll;
     bool checkEqualPartitions(vector<int>& nums, long long target) {
-      n = nums.size();
-      return dfs(nums, 0, 1, 1, target);
-    }
-
-
-    bool dfs(vector<int>& nums, int index, long long sum1, long long sum2, const long long& target) {
-      
-      if (target%sum1 != 0  || target%sum2 != 0) return false;
-      if (index == n && sum1 == target && sum2 == target) return true;
-      if (index == n) return false;
-      
-      if (dfs(nums, index + 1, sum1 * nums[index], sum2, target)) {
-        return true;
+      int n = nums.size();
+      ll ans = 1;
+      for (auto it: nums) {
+        ans *= it;
+        if (ans > (ll)target * target) return false;
       }
-      if (dfs(nums, index + 1, sum1, sum2 * nums[index], target)) {
-        return true;
+
+      if (ans != (ll)target * target) return false;
+      
+      int maxi = (1 << n);
+      vector<long long> prod(maxi, -1);
+      prod[0] = 1;
+      for (int i = 1; i < maxi - 1; ++i) {
+        int highest = 31 - __builtin_clz(i);
+        int prevMask = i ^ (1 << highest);
+        prod[i] = prod[prevMask] * nums[highest];
+        if (prod[i] == target) return true;
       }
+      
       return false;
     }
 };
