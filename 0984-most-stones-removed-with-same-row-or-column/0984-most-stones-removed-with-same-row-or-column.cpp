533a5b5c7007a505a2251809428a7fb5857bdbb4
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-13 17:10:56 +0800

    Time: 29 ms (35.16%), Space: 26.6 MB (9.18%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
@@ -1,10 +1,18 @@
 class Solution {
+  unordered_map<int,vector<int>> mapX;
+  unordered_map<int,vector<int>> mapY;
   public:
     int removeStones(vector<vector<int>>& stones) {
       int n = stones.size();
       vector<int> group(n);
-      vector<int> weight(n,1);
       iota(group.begin(),group.end(),0);
+      for (int i = 0; i < n; i++) {
+        int x = stones[i][0];
+        int y = stones[i][1];
+        mapX[x].push_back(i);
+        mapY[y].push_back(i);
+      }
+
 
       auto find = [&](auto&& self, int x) -> int {
         if (group[x] != x) {
@@ -13,38 +21,40 @@ class Solution {
         return group[x];
       };
 
-      auto unite = [&](int x, int y) -> bool {
+      auto unite = [&](int x, int y) -> void {
         int u = find(find,x);
         int v = find(find,y);
-        if (u == v) return false;
-        if (weight[u] >= weight[v]) {
-          group[v] = u;
-          weight[u] += weight[v];
+        if (u == v) return;
+        if (u < v) {
+          group[v] = u; 
         } else {
-          group[u] = group[v];
-          weight[v] += weight[u];
+          group[u] = v;
         }
-        return true;
       };
-      int res = 0;
-      for (int i = 0 ; i < n; i++) {
-        for (int j = i+1; j < n; j++) {
-          if (stones[i][0] == stones[j][0] ||stones[i][1] == stones[j][1]) {
-            if (unite(i, j)) {
-              res++;
-            }
-          }
+
+      for (auto a : mapX) {
+        int id0 = a.second[0];
+        for (int i = 1; i < a.second.size(); i++) {
+          unite(id0, a.second[i]);
         }
       }
-      return res;
+      for (auto a : mapY) {
+        int id0 = a.second[0];
+        for (int i = 1; i< a.second.size(); i++) {
+          unite(id0, a.second[i]);
+        }
+      }
+      unordered_set<int> Set;
+      for(int i = 0; i < n; i++) {
+        Set.insert(find(find,i));
+      }
+      
+      return n - Set.size();
     }
 };
 
 
 /*
-  X  XXXXX
-
-      XX X
-      X
-      X
+  view the same row or colume stone as connected -> they are in same island
+  so the maximum stone can be remove = stones num - island num
 */
