f35962cd054660972ebd06b2483e99d9a283410b
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-14 00:41:28 +0800

    Time: 31 ms (93.96%), Space: 111.1 MB (96.3%) - LeetHub

--- 2101-last-day-where-you-can-still-cross/2101-last-day-where-you-can-still-cross.cpp
+++ 2101-last-day-where-you-can-still-cross/2101-last-day-where-you-can-still-cross.cpp
@@ -22,12 +22,20 @@ class Solution {
 
 public:
     int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
-      parent.resize(row*col);
+      parent.resize(row*col + 2);
       iota(parent.begin(),parent.end(),0);
       vector<vector<int>> grid(row,vector<int>(col,0));
       for(auto& cell : cells) {
         int x = cell[0], y = cell[1];
         grid[x-1][y-1] = 1;
+      }
+
+      for (int i = 0; i < col; ++i) {
+        Unite(row*col, i);
+      }
+
+      for (int i = 0; i < col; ++i) {
+        Unite(row*col+1, (row - 1)*col + i);
       }
 
       for (int i = 0; i < row; ++i) {
@@ -48,16 +56,11 @@ public:
 
       int days = cells.size();
       while (days >=0 ) {
-        for (int i = 0; i < col; ++i) {
-          if (grid[0][i] == 1) continue;
-          for (int j = 0; j < col; ++j) {
-            if (grid[row-1][j] == 1) continue;
-            int topParent = FindParent(i);
-            int botParent = FindParent((row- 1) * col + j);
-            if (topParent == botParent) return days;
-          }
-        }
 
+        int topParent = FindParent(row*col);
+        int botParent = FindParent(row*col+1);
+        if (topParent == botParent) return days;
+           
         int recoverX = cells[days-1][0]-1;
         int recoverY = cells[days-1][1]-1;
         grid[recoverX][recoverY] = 0;
@@ -72,7 +75,7 @@ public:
 
         days--;
       }
-      return -1;
+      return 0;
     }
 
     vector<pair<int,int>> directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
