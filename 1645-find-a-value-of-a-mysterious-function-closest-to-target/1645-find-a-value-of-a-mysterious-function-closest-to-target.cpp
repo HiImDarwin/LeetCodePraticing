936bc89047a08e4984653f972c1f3ba68bd0b991
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-15 03:43:55 +0800

    Time: 515 ms (46.28%), Space: 256.2 MB (29.26%) - LeetHub

---
+++ 1645-find-a-value-of-a-mysterious-function-closest-to-target/1645-find-a-value-of-a-mysterious-function-closest-to-target.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int closestToTarget(vector<int>& arr, int target) {
+      int n = arr.size();
+      set<int> Set, tmp;
+      int res = abs(-1e9-target);
+      for (int i = 0; i < n; i++) {
+        for (int y : Set) {
+          tmp.insert(y & arr[i]);
+        }
+        tmp.insert(arr[i]);
+
+        for (int y : tmp) {
+          res = min(res,abs(target - y));
+        }
+        Set = tmp;
+        tmp.clear();
+      }
+      return res;
+    }
+};
+/*
+
+ (a & b & c & d) ^ (a & b & c & d) = 0
+
+
+*/
