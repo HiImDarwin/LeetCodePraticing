08f4dc518b8ad90d5ade7974e0f738272a4910cc
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-15 01:27:26 +0800

    Time: 7 ms (64.77%), Space: 26.5 MB (39.39%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.cpp
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.cpp
@@ -2,17 +2,23 @@ class Solution {
 public:
     int maxProduct(vector<string>& words) {
       int n = words.size();
-      vector<int> mask(n,0);
+      unordered_map<int,int> mask;
       for (int i = 0; i < n; ++i) {
+        int tmp = 0;
         for (int j = 0; j < words[i].length(); j++) {
-          mask[i] |= (1 << (words[i][j] - 'a'));
+          tmp |= (1 << (words[i][j] - 'a'));
+        }
+        if (mask.find(tmp) != mask.end()) {
+          mask[tmp] = max(mask[tmp], (int)words[i].length());
+        } else {
+          mask[tmp] = words[i].length();
         }
       }
-      unsigned long res = 0;
-      for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-          if ((mask[i] & mask[j] )== 0) {
-            res = max(res, words[i].length() * words[j].length());
+      int res = 0;
+      for (auto [key1, len1] : mask) {
+        for (auto [key2, len2] : mask) {
+          if ((key1 & key2) == 0) {
+            res = max(res, len1 * len2);
           }
         }
       }
