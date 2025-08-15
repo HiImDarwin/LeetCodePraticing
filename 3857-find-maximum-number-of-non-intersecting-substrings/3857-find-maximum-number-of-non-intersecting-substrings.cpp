e5213de22f87f3b38a7f760a70c1df858a7dd5c4
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-15 16:17:18 +0800

    Time: 19 ms (76.04%), Space: 20.1 MB (93.87%) - LeetHub

--- 3857-find-maximum-number-of-non-intersecting-substrings/3857-find-maximum-number-of-non-intersecting-substrings.cpp
+++ 3857-find-maximum-number-of-non-intersecting-substrings/3857-find-maximum-number-of-non-intersecting-substrings.cpp
@@ -1,40 +1,57 @@
 class Solution {
 public:
-    int maxSubstrings(string word) {
+    // int maxSubstrings(string word) {
 
-      vector<pair<int,int>> interval;
-      vector<vector<int>> charPos(26);
+    //   vector<pair<int,int>> interval;
+    //   vector<vector<int>> charPos(26);
       
-      for(int i = 0; i < word.length(); ++i) {
-        charPos[word[i]-'a'].push_back(i);
-      }
+    //   for(int i = 0; i < word.length(); ++i) {
+    //     charPos[word[i]-'a'].push_back(i);
+    //   }
       
-      for (int i = 0;i < 26; i++) {
-        if (charPos[i].size() <= 1) continue;
-        int left = 0, right = 1;
-        while (right < charPos[i].size()) {
-          if (charPos[i][right] - charPos[i][left] < 3){
-            right++;
-          } else {
-            interval.push_back({charPos[i][left],charPos[i][right]});
-            left++;
+    //   for (int i = 0;i < 26; i++) {
+    //     if (charPos[i].size() <= 1) continue;
+    //     int left = 0, right = 1;
+    //     while (right < charPos[i].size()) {
+    //       if (charPos[i][right] - charPos[i][left] < 3){
+    //         right++;
+    //       } else {
+    //         interval.push_back({charPos[i][left],charPos[i][right]});
+    //         left++;
+    //       }
+    //     }
+    //   }
+
+    //   sort(interval.begin(), interval.end(), [](pair<int,int>& a,pair<int,int>& b){
+    //     return a.second  < b.second;
+    //   });
+    //   int count = 0;
+    //   int prev_end = -1;
+    //   for (auto [start , end] : interval) {
+    //     if (start <= prev_end) {
+    //       continue;
+    //     }
+    //     count++;
+    //     prev_end = end;
+    //   }
+    //   return count;
+    // }
+    int maxSubstrings(string word) {
+      int res = 0;
+      int prev = -1;
+      vector<int> charIdx(26,-1);
+      for (int i = 0; i < word.length(); ++i) {
+        int idx = word[i] - 'a';
+        if (charIdx[idx] > prev && i - charIdx[idx] >= 3) {
+          res++;
+          prev = i;
+        } else {
+          if (charIdx[idx] <= prev) {
+            charIdx[idx] = i;
           }
         }
       }
-
-      sort(interval.begin(), interval.end(), [](pair<int,int>& a,pair<int,int>& b){
-        return a.second  < b.second;
-      });
-      int count = 0;
-      int prev_end = -1;
-      for (auto [start , end] : interval) {
-        if (start <= prev_end) {
-          continue;
-        }
-        count++;
-        prev_end = end;
-      }
-      return count;
+      return res;
     }
 };
 
