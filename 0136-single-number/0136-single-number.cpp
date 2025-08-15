8118841d36fee1e616ea450d80bdcd192be84a24
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-15 11:35:45 +0800

    Time: 0 ms (100%), Space: 20.8 MB (26.06%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,27 +1,10 @@
 class Solution {
 public:
-    // use sorting
-    // int singleNumber(vector<int>& nums) {
-    //     if(nums.size()==1) return nums[0];
-    //     sort(nums.begin(),nums.end());
-    //     for(int i=0;i<nums.size();){
-    //         if(nums[i] == nums[i+1]){
-    //             i+=2;
-    //         }
-    //         else return nums[i];
-    //     }
-    //     return -1;
-    // }
-    //*********** BITWISE XOR
     int singleNumber(vector<int>& nums) {
-        int res=0;
-        for(int i=0;i<nums.size();i++){
-            res ^=nums[i];
-        }
-        return res;
+      int res = 0;
+      for (auto& num : nums) {
+        res ^= num;
+      }
+      return res;
     }
-    //
-    // int singleNumber(vector<int>& nums) {
-        
-    // }
 };
