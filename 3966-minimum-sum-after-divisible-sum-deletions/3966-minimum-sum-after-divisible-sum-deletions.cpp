5f4811d6f037edc5b3cceab9739400ff1560756d
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-17 19:56:00 +0800

    Time: 23 ms (93.67%), Space: 256.4 MB (97.57%) - LeetHub

--- 3966-minimum-sum-after-divisible-sum-deletions/3966-minimum-sum-after-divisible-sum-deletions.cpp
+++ 3966-minimum-sum-after-divisible-sum-deletions/3966-minimum-sum-after-divisible-sum-deletions.cpp
@@ -2,29 +2,36 @@ class Solution {
 public:
     long long minArraySum(vector<int>& nums, int k) {
       int n = nums.size();
-      vector<long long> reamind(k, LLONG_MIN);
-      reamind[0] = 0;
-      long long dp = 0, pref = 0, sum = 0;
-      for (int i = 0; i < n; i++) {
-        sum += nums[i];
-        pref += nums[i];
-        int r = pref % k;
-
-        if (reamind[r] != LLONG_MIN) {
-          dp = max(dp, reamind[r] + pref);
+      vector<long long> remain(k,LLONG_MIN);
+      long long preSum = 0, deleteSum = 0;
+      remain[0] = 0;
+      for (int i = 1; i <= nums.size(); ++i) {
+        preSum += nums[i-1];
+        int r = preSum % k;
+        if (remain[r] != LLONG_MIN) {
+          deleteSum = max(deleteSum, preSum + remain[r]);
         }
-        reamind[r] = max(reamind[r], dp - pref);
+        remain[r] = max(remain[r], deleteSum - preSum);
       }
-      return sum - dp;
+      return preSum - deleteSum;
     }
 };
+
+
 /*
 
 reminder必須存 %k 以前的
 ＊ prefixSum
 ＊ 已經做的區間
+// dp 目前可以刪除的最大區間
+// pref 目前的前綴和
+// reamind[r] = 已知的最佳 offset（狀態轉移需要的資訊）
+// 在此r之前的 
 
 
-XXXXXXXXXXXX
- |___| |____|
+X XXXXXX
+ |___| j   i
+
+ prefi(i) - prefix(j) + previousDP
+ prefix[j] = previousDP - prefix(i)
 */
