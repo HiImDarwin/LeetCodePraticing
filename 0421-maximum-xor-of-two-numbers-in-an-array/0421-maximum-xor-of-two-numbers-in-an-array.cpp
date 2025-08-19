fd0f8608e4048222659ec84ac753382a3f3efab3
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-20 00:56:25 +0800

    Time: 623 ms (48.97%), Space: 184.1 MB (75.34%) - LeetHub

---
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,48 @@
+class Solution {
+  class TrieNdoe {
+    public:
+      TrieNdoe* next[2];
+  };
+public:
+    int findMaximumXOR(vector<int>& nums) {
+      TrieNdoe* root = new TrieNdoe();
+      for (int& num : nums) {
+        TrieNdoe* curr = root;
+        for(int i = 31; i >= 0; --i) {
+          if (curr->next[((num>>i) & 1)] == nullptr) {
+            TrieNdoe* newNode = new TrieNdoe();
+            curr->next[((num>>i) & 1)] = newNode;
+          }
+          curr = curr->next[((num>>i) & 1)];
+        }
+      }
+
+      int res = 0;
+      for (auto& num : nums) {
+        TrieNdoe* curr = root;
+        int XORResult = 0;
+        for (int i = 31; i>=0; --i) {
+          if (curr->next[((num>>i )& 1)^1] != nullptr) {
+            curr = curr->next[((num>>i )& 1)^1];
+            XORResult ^= (1 << i);
+          } else {
+            curr = curr->next[(num>>i )& 1];
+          }
+        }
+        res = max(res,XORResult);
+      }
+      return res;
+    }
+};
+
+
+/*
+[3,10,5,25,2,8]
+
+ 000011
+ 001010
+ 000101
+ 011001
+ 000010
+ 001000
+ */
