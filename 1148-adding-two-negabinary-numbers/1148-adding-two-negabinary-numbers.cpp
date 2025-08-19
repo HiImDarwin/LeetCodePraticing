6096f66e469e9b35f680e265506994c747e073b3
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-20 04:20:46 +0800

    Time: 0 ms (100%), Space: 23.4 MB (62.62%) - LeetHub

---
+++ 1148-adding-two-negabinary-numbers/1148-adding-two-negabinary-numbers.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> addNegabinary(vector<int>& arr1, vector<int>& arr2) {
+      int m = arr1.size();
+      int n = arr2.size();
+      int idx1 = m-1, idx2 = n-1;
+      int carry = 0;
+      vector<int> res;
+      while (idx1 >= 0 || idx2 >= 0 || carry) {
+        int total = carry;
+        total += idx1 >= 0 ? arr1[idx1] : 0;
+        total += idx2 >= 0 ? arr2[idx2] : 0;
+        
+        int r = total%(-2);
+        carry = total/(-2);
+        
+        if (r<0) {
+          carry += 1;
+          r += abs(2);
+        }
+
+        res.push_back(r);
+        idx1--;
+        idx2--;
+      }
+      int i = res.size()-1;
+      while (res[i] == 0 && i > 0) {
+        res.pop_back();
+        i--;
+      }
+      reverse(res.begin(), res.end());
+      return res;
+    }
+};
+
+
+/*
+for the power of even num will be positive
+power of odd will be negative
+
+if postive i got two 1 the  
+
+
+  1 1 0 1
+    0 0 1
+       
+*/
