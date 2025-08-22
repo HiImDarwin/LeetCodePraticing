bf3b2dd428a55396db63f01dae1cebc47959b784
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-22 18:22:51 +0800

    Time: 0 ms (100%), Space: 7.8 MB (79.69%) - LeetHub

--- 0910-nth-magical-number/0910-nth-magical-number.cpp
+++ 0910-nth-magical-number/0910-nth-magical-number.cpp
@@ -1,19 +1,40 @@
+// class Solution {
+// public:
+//     int nthMagicalNumber(int n, int a, int b) {
+//       int MOD= 1e9+7;
+//       set<int> magicalSet;
+//       for (int i = 1; i <= b; i++) {
+//         magicalSet.insert(a*i);
+//       }
+//       for (int i = 1; i <= a; i++) {
+//         magicalSet.insert(b*i);
+//       }
+//       vector<int> magicalNum(magicalSet.begin(), magicalSet.end());
+//       int m = magicalNum.size();
+//       long long tmpNum = 1LL * a * b * (n/m) % MOD;
+//       int res = (tmpNum + ((n % m == 0) ? 0 : magicalNum[n%m-1])) % MOD;
+//       return res;
+//     }
+// };
+
 class Solution {
 public:
     int nthMagicalNumber(int n, int a, int b) {
-      int MOD= 1e9+7;
-      set<int> magicalSet;
-      for (int i = 1; i <= b; i++) {
-        magicalSet.insert(a*i);
-      }
-      for (int i = 1; i <= a; i++) {
-        magicalSet.insert(b*i);
-      }
-      vector<int> magicalNum(magicalSet.begin(), magicalSet.end());
-      int m = magicalNum.size();
-      long long tmpNum = 1LL * a * b * (n/m) % MOD;
-      int res = (tmpNum + ((n % m == 0) ? 0 : magicalNum[n%m-1])) % MOD;
-      return res;
+        long long MOD = 1e9 + 7;
+        long long left = 1, right = 1e18;
+        long long lcm = 1LL * a / gcd(a,b) * b;
+
+        while(left < right){
+            long long mid = left + (right-left)/2;
+            if(mid/a + mid/b - mid/lcm >= n) right = mid;
+            else left = mid + 1;
+        }
+
+        return left % MOD;
+    }
+
+    long long gcd(long long x, long long y){
+        return y == 0 ? x : gcd(y, x%y);
     }
 };
 
