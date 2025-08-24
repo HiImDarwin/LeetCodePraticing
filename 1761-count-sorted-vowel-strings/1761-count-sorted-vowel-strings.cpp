2e7a636b89cc481792a5243b1a01a69226ad5195
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-24 20:31:47 +0800

    Time: 1 ms (30.01%), Space: 9 MB (10.93%) - LeetHub

--- 1761-count-sorted-vowel-strings/1761-count-sorted-vowel-strings.cpp
+++ 1761-count-sorted-vowel-strings/1761-count-sorted-vowel-strings.cpp
@@ -1,22 +1,44 @@
 class Solution {
 public:
+//     int countVowelStrings(int n) {
+//       return comb(n+4,4);
+//     }
+// private: 
+//     long long comb(int m, int n) {
+//       if (n > m) return 0;
+//       long long res = 1;
+//       if (n > m - n) n = m - n;
+//       for (int i = 0; i < n; i++) {
+//         res *= (m-i);
+//         res /= (i+1);
+//       }
+//       return res;
+//     }
+
     int countVowelStrings(int n) {
-      return comb(n+4,4);
-    }
-private: 
-    long long comb(int m, int n) {
-      if (n > m) return 0;
-      long long res = 1;
-      if (n > m - n) n = m - n;
-      for (int i = 0; i < n; i++) {
-        res *= (m-i);
-        res /= (i+1);
+      vector<vector<int>> dp(n+1, vector<int>(5,0));
+      for (int i = 0; i < 5; i++) {
+        dp[1][i] = 1;
+      }
+      for (int i = 2; i <= n; i++) {
+        for (int vow = 0; vow < 5; vow++) {
+          for (int j = vow; j >= 0; j--) {
+            dp[i][vow] += dp[i-1][j];
+          }
+        }
+      }
+
+      int res = 0;
+      for (int i = 0; i < 5; i++) {
+        res += dp[n][i];
       }
       return res;
     }
 };
 
 /*
+
+dp[n][a] = dp[n-1][a]
 7
 XXXXX
 
