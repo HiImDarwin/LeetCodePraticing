7efdf03dca2e634180738800b0710743bfd80b48
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-24 16:11:37 +0800

    Time: 141 ms (56.5%), Space: 12.1 MB (65.82%) - LeetHub

--- 2114-minimum-number-of-work-sessions-to-finish-the-tasks/2114-minimum-number-of-work-sessions-to-finish-the-tasks.cpp
+++ 2114-minimum-number-of-work-sessions-to-finish-the-tasks/2114-minimum-number-of-work-sessions-to-finish-the-tasks.cpp
@@ -1,60 +1,32 @@
-// class Solution {
-// public:
-//     int minSessions(vector<int>& tasks, int sessionTime) {
-//       int n  = tasks.size();
-//       vector<int> dp(1 <<n, INT_MAX/2);
-
-//       for (int state = 0; state < (1 << n); state++) {
-//         int sum = 0;
-//         for (int i = 0; i < n; ++i) {
-//           if ((state >> i )& 1) {
-//             sum += tasks[i];
-//           }
-//         }
-//         if (sum <= sessionTime) {
-//           dp[state] = 1;
-//         }
-//       }
-
-//       for (int state = 0; state < (1 << n); state++) {
-//         for (int subset = state; subset > 0; subset = subset & (subset - 1)) {
-//           dp[state] = min(dp[state], dp[subset] + dp[state - subset]);
-//         }
-//       }
-
-//       return dp[(1 << n )- 1];
-        
-//     }
-// };
 class Solution {
 public:
-    int minSessions(vector<int>& tasks, int sessionTime) 
-    {        
-        int n = tasks.size();
-        vector<int>dp((1<<n), INT_MAX/2);
-        for (int state=0; state<(1<<n); state++)
-        {
-            int sum = 0;
-            for (int i=0; i<n; i++)
-            {
-                if ((state>>i)&1)
-                    sum+=tasks[i];
-            }
-            if (sum<=sessionTime)
-                dp[state]=1;
+    int minSessions(vector<int>& tasks, int sessionTime) {
+      int n  = tasks.size();
+      vector<int> dp(1 << n, INT_MAX/2);
+
+      for (int state = 0; state < (1 << n); state++) {
+        int sum = 0;
+        for (int i = 0; i < n; ++i) {
+          if ((state >> i )& 1) {
+            sum += tasks[i];
+          }
         }
-        
-        for (int state=1; state<(1<<n); state++)
-        {
-            for (int subset=state; subset>0; subset=(subset-1)&state)
-            {
-                dp[state] = min(dp[state], dp[subset]+dp[state-subset]);                                    
-            }
+        if (sum <= sessionTime) {
+          dp[state] = 1;
         }
-        return dp[(1<<n)-1];
+      }
+
+      for (int state = 0; state < (1 << n); state++) {
+        for (int subset = state; subset > 0; subset = state & (subset - 1)) {
+          dp[state] = min(dp[state], dp[subset] + dp[state - subset]);
+        }
+      }
+
+      return dp[(1 << n )- 1];
         
     }
 };
+
 /*
 徧歷子集
 這種問題是data dependent 沒有一個 greedy 的方法
