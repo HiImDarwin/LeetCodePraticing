6afba7152ccb05385493dd5d270a842e4bc6aa62
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-26 02:20:17 +0800

    Time: 0 ms (100%), Space: 9.8 MB (80.84%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,24 +1,19 @@
 class Solution {
-// every number in the nums have 2 choice [in set, out of set]
-// so the solution will contain 2^n subset
-
-
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> set;
-        dfs(res,nums,set,0);
-        return res;
-    }
-
-    void dfs (vector<vector<int>> &res, vector<int> &nums,vector<int> &set,  int index) {
-        if(index == nums.size()) {
-            res.push_back(set);
-            return;
+      int n = nums.size();
+      vector<vector<int>> res((1<<n));
+      res[0] = {};
+      for (int i = 0; i < (1 << n); i++) {
+        vector<int>& tmp = res[i];
+        for(int j = 0; j < n; j++) {
+          if ((i >> j) & 1) continue;
+          int mask = i | (1 << j);
+          vector<int>& tmp = res[mask];
+          tmp = res[i];
+          tmp.push_back(nums[j]);
         }
-        set.push_back(nums[index]);
-        dfs(res,nums,set,index+1);
-        set.pop_back();
-        dfs(res,nums,set,index+1);
+      }
+      return res;
     }
 };
