7cd4ca39ef0fb016372b47500bb88c35f8e100ff
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-25 17:06:46 +0800

    Time: 28 ms (39.34%), Space: 85.9 MB (28.74%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,61 +1,46 @@
 class Solution {
 public:
-    // int largestRectangleArea(vector<int>& heights) {
-    //     //monotonic increase
-    //     int n = heights.size();
-    //     int maximum = 0;
-    //     stack<int> st;
-    //     for(int i=0;i<n;i++) {
-    //         if(st.empty() || heights[i]>=st.top()) {
-    //             st.push(heights[i]);
-    //         } else {
-    //             maximum = max(maximum,cleanST(st,heights[i]));
-    //             st.push(heights[i]);
-    //         }
-    //     }
-    //     maximum = max(maximum,cleanST(st));
-    //     return maximum;
-    // }
-
-    // int cleanST(stack<int> &st,int val=-1) {
-    //     int maxi =0;
-    //     int count=0;
-    //     while(!st.empty() && st.top()>val) {
-    //         int x = st.top();
-    //         st.pop();
-    //         count++;
-    //         while(!st.empty() && st.top() == x) {
-    //             st.pop();
-    //             count++;
-    //         }
-    //         maxi = max(maxi,x*count);
-    //     }
-    //     if(val == -1) return maxi;
-    //     while(count>0){
-    //         st.push(val);
-    //         count--;
-    //     }
-    //     return maxi;
-    // }
-
-    //這題精妙的地方在於 他塞進stack的是index，因此很好查找寬度
     int largestRectangleArea(vector<int>& heights) {
-        //monotonic increase
-        // when counter heights[i] < heights[st.top()] means can start cal the area
-        heights.push_back(0);
-        int n = heights.size();
-        stack<int> st;
-        int maxArea = 0;
+      int n = heights.size();
+      vector<int> prevSmaller(n), nextSmaller(n);
+      stack<int> st1, st2;
 
-        for(int i=0; i<n; ++i) {
-            while(!st.empty()&&heights[i]<heights[st.top()]) {
-                int h = heights[st.top()];
-                st.pop();
-                int width = st.empty() ? i: (i-1)-st.top();
-                maxArea = max(maxArea, h*width);
-            }
-            st.push(i);
+      for(int i = 0; i < n; i++) {
+        while(!st1.empty() && heights[st1.top()] >= heights[i]) {
+          st1.pop();
         }
-        return maxArea;
+        if (st1.empty()) {
+          prevSmaller[i] = -1;
+        } else {
+          prevSmaller[i] = st1.top();
+        }
+        st1.push(i);
+      }
+
+      for(int i = n-1; i >= 0; --i) {
+        while(!st2.empty() && heights[st2.top()] >= heights[i]) {
+          st2.pop();
+        }
+        if (st2.empty()) {
+          nextSmaller[i] = n;
+        } else {
+          nextSmaller[i] = st2.top();
+        }
+        st2.push(i);
+      }
+
+      int res = 0;
+      for (int i = 0; i < n; i++) {
+        res = max(res, heights[i] * (nextSmaller[i] - prevSmaller[i] - 1));
+      }
+
+      return res;
     }
 };
+
+/*
+高受限於最小的bar
+左右邊界取決於prevSmaller nextSmaller
+
+
+*/
