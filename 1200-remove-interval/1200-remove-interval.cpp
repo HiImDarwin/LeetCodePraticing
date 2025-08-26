63c9a88964fb1185363a09a17e1f97b801004867
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-26 16:25:06 +0800

    Time: 6 ms (79.37%), Space: 42.6 MB (87.3%) - LeetHub

---
+++ 1200-remove-interval/1200-remove-interval.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<vector<int>> removeInterval(vector<vector<int>>& intervals, vector<int>& toBeRemoved) {
+      int removeFront = toBeRemoved[0];
+      int removeEnd = toBeRemoved[1];
+      vector<vector<int>> res;
+      for(auto& interval: intervals) {
+        int front = interval[0];
+        int end = interval[1];
+
+        if (removeEnd <= front) {
+          res.push_back({front,end});
+        } else if (removeFront <= front && front < removeEnd && removeEnd < end) {
+          res.push_back({removeEnd, end});
+        } else if (removeFront <= front && end <= removeEnd) {
+          continue;
+        } else if (front < removeFront && removeEnd < end) {
+          res.push_back({front, removeFront});
+          res.push_back({removeEnd, end});
+        } else if (front < removeFront &&  removeFront < end && end <= removeEnd) {
+          res.push_back({front, removeFront});
+        } else if (end <= removeFront) {
+          res.push_back({front,end});
+        }
+      }
+      return res;
+    }
+};
+
+
+/*
+XXX XXX XXX XXXXXX
+  --------
+
+
+not overlap front
+  rmoved end <= front
+
+overlap in front
+ front > romove front && front < rmoved end
+
+overlap whole segment
+
+
+overlap in end
+ front < remove front && end <remove end
+not overlap end
+  end <= rmoveFront
+*/
