56b5d1b6fa3561732df7f8dff2e7cee145b23328
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-26 21:41:45 +0800

    Time: 114 ms (15.83%), Space: 137.1 MB (62.4%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+      priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,less<>> pq;
+      int m = nums1.size();
+      int n = nums2.size();
       vector<vector<int>> res;
-      priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
-
-      for(int x : nums1) {
-        pq.push({x+nums2[0],0});
-      }
-      while(k-- && !pq.empty()) {
-        int sum = pq.top().first;
-        int pos = pq.top().second;
-        res.push_back({sum-nums2[pos], nums2[pos]});
-        pq.pop();
-
-        if( pos+1 < nums2.size()) {
-          pq.push({sum - nums2[pos] + nums2[pos + 1], pos + 1});
+      
+      for (int i = 0;  i < m; i++) {
+        for (int j = 0; j < n; j++) {
+          if (pq.size() < k) {
+            pq.push({nums1[i] + nums2[j], i, j});
+          } else if (pq.size() == k && nums1[i] + nums2[j] >= get<0>(pq.top())) {
+            break;
+          } else if (pq.size() == k && nums1[i] + nums2[j] < get<0>(pq.top())) {
+            pq.pop();
+            pq.push({nums1[i] + nums2[j], i, j});
+          }  
         }
+      }
+      while (!pq.empty()) {
+        auto [num, i, j] = pq.top();
+        pq.pop();
+        res.push_back({nums1[i],nums2[j]});
       }
       return res;
     }
@@ -23,9 +29,9 @@ public:
 
 
 
+
+
 /*
-
-
-
-
+在連續區間找第K個
+這就是 binary search 的起手式
 */
