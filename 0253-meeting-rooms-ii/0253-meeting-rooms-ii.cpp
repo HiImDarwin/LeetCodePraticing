6c22ccb05b2c40ec77e9b4f236b30a16136ae22c
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-26 16:50:38 +0800

    Time: 3 ms (55.01%), Space: 17.1 MB (24.74%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
-    // int minMeetingRooms(vector<vector<int>>& intervals) {
-    //   int maxRoom = 0;
-    //   int room = 0;
-    //   int n = intervals.size();
-    //   vector<pair<int,int>> timeLine;
-    //   for(auto interval :intervals) {
-    //     timeLine.push_back({interval[0],1});
-    //     timeLine.push_back({interval[1],-1});
-    //   }
-    //   sort(timeLine.begin(),timeLine.end());
-    //   for(auto [time, delta] : timeLine){
-    //     room += delta;
-    //     maxRoom = max(maxRoom,room);
-    //   }
-    //   return maxRoom;
-    // }  
     int minMeetingRooms(vector<vector<int>>& intervals) {
-      vector<int> time(1e6+1,0);
-      for (auto& interval : intervals) {
-        time[interval[0]]++;
-        time[interval[1]]--;
+      int maxRoom = 0;
+      int room = 0;
+      int n = intervals.size();
+      vector<pair<int,int>> timeLine;
+      for(auto interval :intervals) {
+        timeLine.push_back({interval[0],1});
+        timeLine.push_back({interval[1],-1});
       }
-      int res = 0;
-      int current = 0;
-      for (int i = 0; i < time.size(); i++) {
-        current += time[i];
-        res = max(res,current);
+      sort(timeLine.begin(),timeLine.end());
+      for(auto [time, delta] : timeLine){
+        room += delta;
+        maxRoom = max(maxRoom,room);
       }
-      return res;
-    }
+      return maxRoom;
+    }  
+
+
+
+
+
+    // int minMeetingRooms(vector<vector<int>>& intervals) {
+    //   vector<int> time(1e6+1,0);
+    //   for (auto& interval : intervals) {
+    //     time[interval[0]]++;
+    //     time[interval[1]]--;
+    //   }
+    //   int res = 0;
+    //   int current = 0;
+    //   for (int i = 0; i < time.size(); i++) {
+    //     current += time[i];
+    //     res = max(res,current);
+    //   }
+    //   return res;
+    // }
 };
 
 
@@ -42,7 +47,13 @@ XXX
 
 at the moment how many meeting overlap
 
+difference array
 
+
+XXXXXX
+ X 
+    XXX
+  X
 
 
 
