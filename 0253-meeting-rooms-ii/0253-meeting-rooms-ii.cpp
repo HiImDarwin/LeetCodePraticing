28fc8697dd9fe6c5432db1d6824d7474a9c5418e
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-26 16:42:00 +0800

    Time: 163 ms (5.13%), Space: 302.2 MB (5.29%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,20 +1,50 @@
 class Solution {
 public:
+    // int minMeetingRooms(vector<vector<int>>& intervals) {
+    //   int maxRoom = 0;
+    //   int room = 0;
+    //   int n = intervals.size();
+    //   vector<pair<int,int>> timeLine;
+    //   for(auto interval :intervals) {
+    //     timeLine.push_back({interval[0],1});
+    //     timeLine.push_back({interval[1],-1});
+    //   }
+    //   sort(timeLine.begin(),timeLine.end());
+    //   for(auto [time, delta] : timeLine){
+    //     room += delta;
+    //     maxRoom = max(maxRoom,room);
+    //   }
+    //   return maxRoom;
+    // }  
     int minMeetingRooms(vector<vector<int>>& intervals) {
-      int maxRoom = 0;
-      int room = 0;
-      int n = intervals.size();
-      vector<pair<int,int>> timeLine;
-      for(auto interval :intervals) {
-        timeLine.push_back({interval[0],1});
-        timeLine.push_back({interval[1],-1});
+      vector<int> time(1e6+1,0);
+      for (auto& interval : intervals) {
+        time[interval[0]]++;
+        time[interval[1]]--;
       }
-      sort(timeLine.begin(),timeLine.end());
-      for(auto [time, delta] : timeLine){
-        room += delta;
-        maxRoom = max(maxRoom,room);
+      int res = 0;
+      int current = 0;
+      for (int i = 0; i < time.size(); i++) {
+        current += time[i];
+        res = max(res,current);
       }
-      return maxRoom;
+      return res;
     }
-    
 };
+
+
+
+/*
+
+    XXX
+  XXXX
+XXX
+
+at the moment how many meeting overlap
+
+
+
+
+
+
+*/
