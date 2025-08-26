27b42f4e3cb0cc646f8921620d25a301fc65d7cf
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-26 21:46:38 +0800

    Time: 85 ms (39.98%), Space: 141.7 MB (47.3%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,29 +1,62 @@
 class Solution {
 public:
+    // vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+    //   priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,less<>> pq;
+    //   int m = nums1.size();
+    //   int n = nums2.size();
+    //   vector<vector<int>> res;
+      
+    //   for (int i = 0;  i < m; i++) {
+    //     for (int j = 0; j < n; j++) {
+    //       if (pq.size() < k) {
+    //         pq.push({nums1[i] + nums2[j], i, j});
+    //       } else if (pq.size() == k && nums1[i] + nums2[j] >= get<0>(pq.top())) {
+    //         break;
+    //       } else if (pq.size() == k && nums1[i] + nums2[j] < get<0>(pq.top())) {
+    //         pq.pop();
+    //         pq.push({nums1[i] + nums2[j], i, j});
+    //       }  
+    //     }
+    //   }
+    //   while (!pq.empty()) {
+    //     auto [num, i, j] = pq.top();
+    //     pq.pop();
+    //     res.push_back({nums1[i],nums2[j]});
+    //   }
+    //   return res;
+    // }
+
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-      priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,less<>> pq;
       int m = nums1.size();
       int n = nums2.size();
-      vector<vector<int>> res;
-      
-      for (int i = 0;  i < m; i++) {
-        for (int j = 0; j < n; j++) {
-          if (pq.size() < k) {
-            pq.push({nums1[i] + nums2[j], i, j});
-          } else if (pq.size() == k && nums1[i] + nums2[j] >= get<0>(pq.top())) {
-            break;
-          } else if (pq.size() == k && nums1[i] + nums2[j] < get<0>(pq.top())) {
-            pq.pop();
-            pq.push({nums1[i] + nums2[j], i, j});
-          }  
-        }
+
+      vector<vector<int>> ans;
+      set<pair<int, int>> visited;
+
+      priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
+                      greater<pair<int, pair<int, int>>>> minHeap;
+      minHeap.push({nums1[0] + nums2[0], {0, 0}});
+      visited.insert({0, 0});
+
+      while (k-- && !minHeap.empty()) {
+          auto top = minHeap.top();
+          minHeap.pop();
+          int i = top.second.first;
+          int j = top.second.second;
+
+          ans.push_back({nums1[i], nums2[j]});
+
+          if (i + 1 < m && visited.find({i + 1, j}) == visited.end()) {
+              minHeap.push({nums1[i + 1] + nums2[j], {i + 1, j}});
+              visited.insert({i + 1, j});
+          }
+
+          if (j + 1 < n && visited.find({i, j + 1}) == visited.end()) {
+              minHeap.push({nums1[i] + nums2[j + 1], {i, j + 1}});
+              visited.insert({i, j + 1});
+          }
       }
-      while (!pq.empty()) {
-        auto [num, i, j] = pq.top();
-        pq.pop();
-        res.push_back({nums1[i],nums2[j]});
-      }
-      return res;
+      return ans;
     }
 };
 
