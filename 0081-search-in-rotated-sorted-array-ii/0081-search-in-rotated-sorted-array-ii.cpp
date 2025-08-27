43f46a02aa5782e71a95d452e2176024d039c6c2
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-27 16:57:06 +0800

    Time: 0 ms (100%), Space: 18.1 MB (20.06%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int left =0;
-        int right = nums.size()-1;
-        while(left<=right) {
-            int mid = left + (right-left)/2;
-            if(nums[mid] == target) {
-                return true;
-            }
-            if( nums[mid] == nums[left] && nums[mid] == nums[right]) {
-                left++;
-                right--;
-            } else if(nums[mid] >= nums[left]) { //left side accending
-                if(target < nums[mid] && nums[left] <= target) {
-                    right = mid-1;
-                } else {
-                    left = mid+1;
-                }
-            } else { //right side accending
-                if(target > nums[mid] && target <=nums[right]) {
-                    left = mid+1;
-                } else {
-                    right = mid-1;
-                }
-            }
+      int n = nums.size();
+      int left = 0, right = n - 1;
+      while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+          return true;
         }
-        return false;
+        while (nums[left] == nums[right] && left < mid && right > mid) {
+          if (nums[left] == target) return true;
+          left++;
+          right--;
+        }
+        if (nums[left] <= nums[mid]) {
+          if (nums[left] <= target && target < nums[mid]) {
+            right = mid - 1;
+          } else {
+            left = mid + 1;
+          }
+        } else {
+          if (nums[mid] < target && target <= nums[right]) {
+            left = mid + 1;
+          } else {
+            right = mid - 1;
+          }
+        }
+        
+      }
+      return false;
     }
 };
