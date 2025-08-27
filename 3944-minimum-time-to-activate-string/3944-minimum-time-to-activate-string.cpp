b7f8d7fb78afaa5141fa743718b472d86420dda3
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-28 01:44:52 +0800

    Time: 51 ms (89.35%), Space: 165.7 MB (91.44%) - LeetHub

--- 3944-minimum-time-to-activate-string/3944-minimum-time-to-activate-string.cpp
+++ 3944-minimum-time-to-activate-string/3944-minimum-time-to-activate-string.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
     int minTime(string s, vector<int>& order, int k) {
-      if(k == 0) return 0;
       int n = order.size();
-      set<pair<int,int>> segments;
-      segments.insert({0,n-1});
-      int res = 0;
-      long long count = 0;
-      for(int i = 0; i < n; ++i) {
-        s[order[i]] = '*';
-        auto it = segments.upper_bound({order[i],INT_MAX});
-        if(it != segments.begin()) {
-          it--;
+      int left = 0, right = n;
+      while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (countSub(order, mid) >= k) {
+          right = mid;
+        } else{
+          left = mid + 1;
         }
-        auto [l, r] = *it;
-        segments.erase(it);
-        segments.insert({l,order[i]-1});
-        segments.insert({order[i]+1, r});
-
-        count += 1LL * (order[i]- l + 1) * (r - order[i] + 1);
-
-        if(count >= k) return res;
-        res++;
       }
-      return -1;
+
+      return left == n ? -1 : left;
     }
+    
+    long long countSub(vector<int>& order, int days) {
+      int n = order.size();
+      vector<bool> isStar(n,false);
+      for (int i = 0; i <= days; ++i) {
+        isStar[order[i]] = true;
+      }
+
+      int prev = -1;
+      long long total = 0;
+      for (int i = 0; i < n; i++) {
+        if (isStar[i]) {
+          int preVlen = i - prev;
+          total += 1LL * (n - i) * preVlen;
+          prev = i;
+        }
+      }
+      return total;
+    }
+
 };
