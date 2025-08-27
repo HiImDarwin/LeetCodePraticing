6bfe24af02e95f7dcd1156de3330f6ca896969f1
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-28 02:10:19 +0800

    Time: 0 ms (100%), Space: 71.8 MB (16.69%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
-        // F(n) = max( F(n), F(n-1)+F(n)) 
+    //DP[i] == max(DP[i-1]+num[i],num[i])
+    // int maxSubArray(vector<int>& nums) {
+    //     int DP[nums.size()];
+    //     int res =nums[0];
+    //     DP[0]=nums[0];
+    //     for(int i=1;i<nums.size();i++) {
+    //         DP[i] = max(DP[i-1]+nums[i],nums[i]);
+    //         res = max(DP[i],res);
+    //     }
+    //     return res;
+    // }
     int maxSubArray(vector<int>& nums) {
-        int perior_sum = nums[0];
-        int res=nums[0];
-        for(int i=1; i<nums.size(); i++) {
-            perior_sum = max(nums[i],nums[i]+perior_sum);
-            res = max (perior_sum, res);
-        }
-        return res;
-
+      long res = INT_MIN;
+      long total = 0;
+      for (const auto& num : nums) {
+        total = max((long)num , total + num);
+        res = max(res, total); 
+      }
+      return res;
     }
 };
