47927ce93eabafdd41a2b68afd21c6cf75f4ee51
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-27 22:47:31 +0800

    Time: 23 ms (91.67%), Space: 36.9 MB (8.33%) - LeetHub

--- 2069-kth-smallest-subarray-sum/2069-kth-smallest-subarray-sum.cpp
+++ 2069-kth-smallest-subarray-sum/2069-kth-smallest-subarray-sum.cpp
@@ -10,7 +10,7 @@ public:
       long long left = 1, right = preSum.back();
       while (left < right) {
         long long mid = left + (right - left) / 2;
-        if (countSub(preSum, mid) >= k) {
+        if (countSub(nums, mid) >= k) {
           right = mid;
         } else {
           left = mid + 1;
@@ -20,24 +20,39 @@ public:
     }
 
 
-    int countSub(vector<long long>& preSum, int middle) {
-      int n = preSum.size();
-      int count = 0;
+    // int countSub(vector<long long>& preSum, int middle) {
+    //   int n = preSum.size();
+    //   int count = 0;
       
-      // for (int i = 1; i < n; i++) {
-      //   for (int j = i-1; j >= 0; j--) {
-      //     if (preSum[i] - preSum[j] <= middle) {
-      //       count++;
-      //     } else {
-      //       break;
-      //     }
-      //   }
-      // }
-      for (int i = 0, j = 1; j < n; j++) {
-        while(i < j && (preSum[j] - preSum[i]) > middle) {
-          i++;
-        }
-        count += j - i;
+    //   // for (int i = 1; i < n; i++) {
+    //   //   for (int j = i-1; j >= 0; j--) {
+    //   //     if (preSum[i] - preSum[j] <= middle) {
+    //   //       count++;
+    //   //     } else {
+    //   //       break;
+    //   //     }
+    //   //   }
+    //   // }
+    //   for (int i = 0, j = 1; j < n; j++) {
+    //     while(i < j && (preSum[j] - preSum[i]) > middle) {
+    //       i++;
+    //     }
+    //     count += j - i;
+    //   }
+    //   return count;
+    // }
+
+    int countSub(vector<int>& nums, int target) {
+      int n = nums.size();
+      int count = 0;
+      int total = 0;
+      for (int left = 0, right = 0; right < n; right++) {
+	      total += nums[right];
+	      while (left <= right && total > target) {
+		      total -= nums[left];
+		      left++;
+	      }
+	      count += right - left + 1;
       }
       return count;
     }
