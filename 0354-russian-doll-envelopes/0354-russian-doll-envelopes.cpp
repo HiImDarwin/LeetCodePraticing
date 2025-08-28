75e080ebc54b3dbc8756f7ff4723f024b0a90d18
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-28 20:37:52 +0800

    Time: 40 ms (66.43%), Space: 81.6 MB (78.19%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -18,26 +18,48 @@ public:
     //   }
     //   return res;
     // }
-    struct Compare {
-      bool operator()(vector<int> &a, vector<int> &b) {
-        if(a[0]== b[0]) {
-          return a[1] > b[1];
-        } else {
-          return a[0] < b[0];
-        }
-      }
-    };
+    // struct Compare {
+    //   bool operator()(vector<int> &a, vector<int> &b) {
+    //     if(a[0]== b[0]) {
+    //       return a[1] > b[1];
+    //     } else {
+    //       return a[0] < b[0];
+    //     }
+    //   }
+    // };
+    // int maxEnvelopes(vector<vector<int>>& envelopes) {
+    //   sort(envelopes.begin(), envelopes.end(), Compare());
+    //   vector<int> q;
+    //   for(auto x :envelopes ) {
+    //     if (q.empty() || x[1] > q.back()) {
+    //       q.push_back(x[1]);
+    //     } else {
+    //       auto iter = lower_bound(q.begin(), q.end(),x[1]);
+    //       *iter = x[1];
+    //     }
+    //   }
+    //   return q.size();
+    // }
     int maxEnvelopes(vector<vector<int>>& envelopes) {
-      sort(envelopes.begin(), envelopes.end(), Compare());
+      
+      sort(envelopes.begin(), envelopes.end(),[](const vector<int>& a, const vector<int>& b){
+        if (a[0] == b[0]) {
+          return a[1] > b[1];
+        } 
+        return a[0] < b[0];
+      });
+
       vector<int> q;
-      for(auto x :envelopes ) {
-        if (q.empty() || x[1] > q.back()) {
+
+      for (auto& x : envelopes) {
+        int idx = lower_bound(q.begin(), q.end(), x[1]) - q.begin();
+        if (idx == q.size()) {
           q.push_back(x[1]);
         } else {
-          auto iter = lower_bound(q.begin(), q.end(),x[1]);
-          *iter = x[1];
+          q[idx] = x[1];
         }
       }
+
       return q.size();
     }
 };
@@ -46,7 +68,7 @@ public:
 /*
 sort the array with length
 the legal answer must be a sebsequence of the array > Longest increasing subsequence
-but inverse may not be right be right because need to consider the width
+but inverse may not be right because need to consider the width
 
 
 dp[i]: the maximum length of Russian doll sequence if i-th envelope is the largest one
@@ -59,6 +81,12 @@ return max{dp[i]}
 
 
 
-so the second method is using monotonic stack
+so the second method is using patient sort 
+
+sort the length in increasing order
+when the legnth is the same
+sort the wid in decreasing order
+by this way we can apply patient sort
+
 
 */
