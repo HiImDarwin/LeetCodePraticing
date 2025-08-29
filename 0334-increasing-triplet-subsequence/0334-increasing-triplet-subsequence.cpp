cf29c35e5a7b14928abade2ee62e7c7f76ef47f0
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-29 18:55:25 +0800

    Time: 0 ms (100%), Space: 115.7 MB (56.45%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,36 +1,49 @@
 class Solution {
 public:
-    bool increasingTriplet(vector<int>& nums) {
-      int n = nums.size();
+    // bool increasingTriplet(vector<int>& nums) {
+    //   int n = nums.size();
       
-      vector<bool> leftSmaller(n,false);
-      stack<int> st;
+    //   vector<bool> leftSmaller(n,false);
+    //   stack<int> st;
 
-      for (int i = 0; i < n; i++) {
-        while (!st.empty() && nums[i] <= st.top()) {
-          st.pop();
-        }
-        if (st.empty()) {
-          leftSmaller[i] = false;
-        } else {
-          leftSmaller[i] = true;
-        }
-        st.push(nums[i]);
-      }
+    //   for (int i = 0; i < n; i++) {
+    //     while (!st.empty() && nums[i] <= st.top()) {
+    //       st.pop();
+    //     }
+    //     if (st.empty()) {
+    //       leftSmaller[i] = false;
+    //     } else {
+    //       leftSmaller[i] = true;
+    //     }
+    //     st.push(nums[i]);
+    //   }
       
-      stack<int> st2;
-      for (int i = n - 1; i >= 0; --i) {
-        while (!st2.empty() && nums[i] >= st2.top()) {
-         st2.pop();
-        }
-        if (st2.empty() || !leftSmaller[i]) {
-          st2.push(nums[i]);
-          continue;
-        } else {
+    //   stack<int> st2;
+    //   for (int i = n - 1; i >= 0; --i) {
+    //     while (!st2.empty() && nums[i] >= st2.top()) {
+    //      st2.pop();
+    //     }
+    //     if (st2.empty() || !leftSmaller[i]) {
+    //       st2.push(nums[i]);
+    //       continue;
+    //     } else {
+    //       return true;
+    //     }
+    //   }
+    //   return false; 
+    // }
+    bool increasingTriplet(vector<int>& nums) {
+      int first = INT_MAX, second = INT_MAX;
+      for (auto& n : nums) {
+        if (n < first) {
+          first = n;
+        } else if ( n > first && n < second) {
+          second = n;
+        } else if (n > first && n > second) {
           return true;
         }
       }
-      return false; 
+      return false;
     }
 };
 
