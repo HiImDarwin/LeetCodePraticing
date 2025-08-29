d5635c453a6504c0d030141b4c42ee77775fdd8e
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-30 00:07:21 +0800

    Time: 1 ms (9.9%), Space: 8.3 MB (80.96%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        if(s.empty()) return 0;
-        int count=0;
-        int leftPar =0;
-        for(char &x : s) {
-            if(x == '('){
-                leftPar++;
-            } else {
-                if(leftPar <= 0) count++;
-                else leftPar--;
-            }
+      int count = 0;
+      int res = 0;
+      for(auto& c : s) {
+        if (c == '(') {
+          count++;
+        } else if (c == ')') {
+          if (count == 0) {
+            res++;
+          } else {
+            count--;
+          }
         }
-        return count+leftPar;
+      }
+      return res + count;
+
     }
 };
