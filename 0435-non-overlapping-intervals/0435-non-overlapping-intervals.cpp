125e6cf77d217f2f18c482ee04d0b9f6e6d67024
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-29 14:43:48 +0800

    Time: 65 ms (26.62%), Space: 93.9 MB (67.03%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,18 +1,45 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end(),[](vector<int> &a, vector<int> &b) {
-          return a[1]<b[1];
-        });
-        int tmp_val = intervals[0][1];
-        int removeNum=0;
-        for(int i = 1; i < intervals.size(); ++i) {
-          if(intervals[i][0]<tmp_val){
-            removeNum++;
-          } else {
-            tmp_val = intervals[i][1];
-          }
+      int n = intervals.size();
+      sort(intervals.begin(), intervals.end());
+      int prevEnd = intervals[0][1];
+      int res = 0;
+      for (int i = 1; i < n; ++i) {
+        int start = intervals[i][0];
+        int end = intervals[i][1];
+        if (start < prevEnd) {
+          prevEnd = min(end, prevEnd);   
+          res++;
+        } else {
+          prevEnd = end;
         }
-        return removeNum;
+        
+      }
+
+      return res;
     }
 };
+
+
+/*
+
+ XXX
+  XXXX
+       XX
+          xx
+
+XXX
+  XXXX
+
+XXXXXX
+  XX
+
+
+minimum number of intervals you need to remove
+always remove the one with larger length
+
+XX XX XX XX XX XXXX
+ XXXXXXXXXXXXXXXX
+
+*/
