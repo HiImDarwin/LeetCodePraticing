2b2ed8714c4b5c550ee191b9c4b5b09322133700
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-30 13:01:13 +0800

    Time: 107 ms (44%), Space: 16 MB (88%) - LeetHub

--- 2322-number-of-ways-to-build-sturdy-brick-wall/2322-number-of-ways-to-build-sturdy-brick-wall.cpp
+++ 2322-number-of-ways-to-build-sturdy-brick-wall/2322-number-of-ways-to-build-sturdy-brick-wall.cpp
@@ -1,17 +1,39 @@
 class Solution {
 public:
     int buildWall(int height, int width, vector<int>& bricks) {
-      int n = bricks.size();
-      int m = width - 1;
-      int MOD = 1e9 +7;
-      
-      
-      unordered_set<int> widthSet;
-      vector<int> arrangeWays;
-      for (auto& wid : bricks) {
-        widthSet.insert(wid);
+      unordered_set<int> widthSet(bricks.begin(), bricks.end());
+      vector<int> patterns = BuildPattern(width, widthSet);
+      return CountWays(height, patterns);  
+    }
+
+    int CountWays(const int height, vector<int>& pattern) {
+      int MOD = 1e9 + 7;
+
+      vector<vector<int>> dp(height,vector<int>(pattern.size(),0));
+      for (int i = 0; i < pattern.size(); i++) {       
+          dp[0][i] = 1;
       }
 
+      for (int i = 1 ; i < height; i++) {
+        for (int state = 0; state < pattern.size(); state++) {
+          for (int prevState = 0; prevState < pattern.size(); prevState++) {
+            if (!(pattern[state] & pattern[prevState])) {
+              dp[i][state] = (dp[i][state] + dp[i - 1][prevState]) % MOD;
+            }
+          }
+        }
+      }
+      int sum = 0;
+      for (int i = 0 ; i < pattern.size(); ++i) {
+        sum = (sum + dp[height - 1][i]) % MOD;
+      }
+
+      return sum;    
+    }
+
+    vector<int> BuildPattern(const int width, unordered_set<int>& widthSet) {
+      int m = width - 1;
+      vector<int> pattern;
       for (int i = 0; i < (1 << m); ++i) {
         vector<int> tmp;
         tmp.push_back(-1);
@@ -31,30 +53,10 @@ public:
           }
         }
         if (valid) {
-          arrangeWays.push_back(i);
+          pattern.push_back(i);
         }
       }
-
-      vector<vector<int>> dp(height,vector<int>(arrangeWays.size(),0));
-      for (int i = 0; i < arrangeWays.size(); i++) {       
-          dp[0][i] = 1;
-      }
-
-      for (int i = 1 ; i < height; i++) {
-        for (int state = 0; state < arrangeWays.size(); state++) {
-          for (int prevState = 0; prevState < arrangeWays.size(); prevState++) {
-            if (!(arrangeWays[state] & arrangeWays[prevState])) {
-              dp[i][state] = (dp[i][state] + dp[i - 1][prevState]) % MOD;
-            }
-          }
-        }
-      }
-      int sum = 0;
-      for (int i = 0 ; i < arrangeWays.size(); ++i) {
-        sum = (sum + dp[height - 1][i]) % MOD;
-      }
-
-      return sum;     
+      return pattern;
     }
 };
 
