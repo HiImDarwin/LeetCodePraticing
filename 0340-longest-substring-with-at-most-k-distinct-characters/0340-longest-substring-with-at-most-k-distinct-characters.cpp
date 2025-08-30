fc331dc7b94f2a4f57c7ab54cf9ed15f4eb33530
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-30 22:03:16 +0800

    Time: 11 ms (21.96%), Space: 10.6 MB (49.45%) - LeetHub

--- 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.cpp
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.cpp
@@ -1,24 +1,48 @@
 class Solution {
 public:
+    // int lengthOfLongestSubstringKDistinct(string s, int k) {
+    //     vector<int> table(256,0);
+    //     int count = 0;
+    //     int res = 0;
+    //     for(int r = 0, l = 0; r < s.length(); ++r) {
+    //       if(table[s[r]] == 0) {
+    //         table[s[r]]++;
+    //         count++;
+    //       } else {
+    //         table[s[r]]++;
+    //       }
+    //       while(count > k) {
+    //         table[s[l]]--;
+    //         if(table[s[l]] == 0) 
+    //           count--;
+    //         l++;
+    //       }
+    //       res = max (res, r - l + 1);
+    //     }
+    //     return res;
+    // }
+
     int lengthOfLongestSubstringKDistinct(string s, int k) {
-        vector<int> table(256,0);
-        int count = 0;
-        int res = 0;
-        for(int r = 0, l = 0; r < s.length(); ++r) {
-          if(table[s[r]] == 0) {
-            table[s[r]]++;
-            count++;
-          } else {
-            table[s[r]]++;
+      if (k == 0) return 0;
+
+      unordered_map<char,int> charMap;
+      int left = 0, right = 0;
+      int maxLen = 0;
+
+      while (right < s.length()) {
+        charMap[s[right]]++;
+        while(charMap.size() > k) {
+          maxLen = max(maxLen, right - left);
+          charMap[s[left]]--;
+          if (charMap[s[left]] == 0) {
+            charMap.erase(s[left]);
           }
-          while(count > k) {
-            table[s[l]]--;
-            if(table[s[l]] == 0) 
-              count--;
-            l++;
-          }
-          res = max (res, r - l + 1);
-        }
-        return res;
+          left++;
+        } 
+        right++;
+      }
+      maxLen = max(maxLen, right - left);
+
+      return maxLen;
     }
 };
