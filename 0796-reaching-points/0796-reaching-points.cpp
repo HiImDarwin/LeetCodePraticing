d293ece8202ec02181c13e13164c56bea52c6439
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-08-31 22:09:43 +0800

    Time: 0 ms (100%), Space: 7.6 MB (97.95%) - LeetHub

---
+++ 0796-reaching-points/0796-reaching-points.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool reachingPoints(int sx, int sy, int tx, int ty) {
+      while (tx > sx && ty > sy) {
+        if (tx > ty) {
+          tx %= ty;
+        } else {
+          ty %= tx;
+        }
+      }
+      return sx == tx && sy <= ty && (ty - sy) % sx == 0 ||
+             sy == ty && sx <= tx && (tx - sx) % sy == 0;
+    }
+};
+
+/*
+corner case tx < sx || ty < sy
+recursive
+
+recursive (x y tx ty) {
+  if
+
+
+}
+
+
+
+
+memo[x][y]
+DP
+
+*/
