b7694d626f97aa1b4c1d9741b4822702ef50cee5
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-09-01 18:19:39 +0800

    Time: 0 ms (100%), Space: 9.1 MB (88.55%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
+    // int combinationSum4(vector<int>& nums, int target) {
+    //     unordered_map<int, int> memo;
+    //     return dfs(nums,target,memo);
+    // }
+    // int dfs(vector<int>& nums, int target, unordered_map<int, int> &memo){
+    //     if(target ==0) return 1;
+    //     if(target<0) return 0;
+    //     if(memo.count(target)) return memo[target];
+    //     int res =0;
+    //     for(int num:nums) {
+    //         res += dfs(nums, target -num,memo);
+    //     }
+    //     memo[target] = res;
+    //     return res;
+    // }
+
     int combinationSum4(vector<int>& nums, int target) {
-        unordered_map<int, int> memo;
-        return dfs(nums,target,memo);
-    }
-    int dfs(vector<int>& nums, int target, unordered_map<int, int> &memo){
-        if(target ==0) return 1;
-        if(target<0) return 0;
-        if(memo.count(target)) return memo[target];
-        int res =0;
-        for(int num:nums) {
-            res += dfs(nums, target -num,memo);
+      int n = nums.size();
+
+      sort(nums.begin(),nums.end());
+      vector<unsigned int> dp(target+1,0);
+
+      dp[0] = 1;
+      for (int sum = 1; sum <= target; ++sum) {
+        for (auto& num : nums) {
+          dp[sum] += (sum - num >= 0) ? dp[sum - num] : 0;
         }
-        memo[target] = res;
-        return res;
+      }
+
+      return dp[target];
     }
     
 };
 
+/*
+ dp[num][i] += dp[num- nums[i] * times][i-1]
+
+*/
