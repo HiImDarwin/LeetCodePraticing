4a90d467c0d9079f42c544d2542bdf84988f6c6c
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-09-01 16:13:38 +0800

    Time: 609 ms (62.63%), Space: 184.3 MB (34.55%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -4,33 +4,63 @@ class Solution {
       TrieNdoe* next[2];
   };
 public:
+    // int findMaximumXOR(vector<int>& nums) {
+    //   TrieNdoe* root = new TrieNdoe();
+    //   for (int& num : nums) {
+    //     TrieNdoe* curr = root;
+    //     for(int i = 31; i >= 0; --i) {
+    //       if (curr->next[((num>>i) & 1)] == nullptr) {
+    //         TrieNdoe* newNode = new TrieNdoe();
+    //         curr->next[((num>>i) & 1)] = newNode;
+    //       }
+    //       curr = curr->next[((num>>i) & 1)];
+    //     }
+    //   }
+
+    //   int res = 0;
+    //   for (auto& num : nums) {
+    //     TrieNdoe* curr = root;
+    //     int XORResult = 0;
+    //     for (int i = 31; i>=0; --i) {
+    //       if (curr->next[((num>>i )& 1)^1] != nullptr) {
+    //         curr = curr->next[((num>>i )& 1)^1];
+    //         XORResult ^= (1 << i);
+    //       } else {
+    //         curr = curr->next[(num>>i )& 1];
+    //       }
+    //     }
+    //     res = max(res,XORResult);
+    //   }
+    //   return res;
+    // }
+
     int findMaximumXOR(vector<int>& nums) {
       TrieNdoe* root = new TrieNdoe();
-      for (int& num : nums) {
-        TrieNdoe* curr = root;
-        for(int i = 31; i >= 0; --i) {
-          if (curr->next[((num>>i) & 1)] == nullptr) {
-            TrieNdoe* newNode = new TrieNdoe();
-            curr->next[((num>>i) & 1)] = newNode;
+
+      for (const auto& num : nums) {
+        TrieNdoe* node = root;
+        for (int i = 31; i >= 0; --i) {
+          if (node->next[(num >> i) & 1] == nullptr) {
+            node->next[(num >> i) & 1] = new TrieNdoe();
           }
-          curr = curr->next[((num>>i) & 1)];
+          node = node->next[(num >> i) & 1];
         }
       }
-
       int res = 0;
-      for (auto& num : nums) {
+      for (const auto& num : nums) {
         TrieNdoe* curr = root;
         int XORResult = 0;
-        for (int i = 31; i>=0; --i) {
-          if (curr->next[((num>>i )& 1)^1] != nullptr) {
-            curr = curr->next[((num>>i )& 1)^1];
+        for (int i = 31; i >= 0; --i) {
+          if (curr->next[((num >> i) & 1) ^ 1] != nullptr) {
+            curr = curr->next[((num >> i) & 1) ^ 1];
             XORResult ^= (1 << i);
           } else {
-            curr = curr->next[(num>>i )& 1];
+             curr = curr->next[((num >> i) & 1)];
           }
         }
-        res = max(res,XORResult);
+        res = max(res, XORResult);
       }
+    
       return res;
     }
 };
