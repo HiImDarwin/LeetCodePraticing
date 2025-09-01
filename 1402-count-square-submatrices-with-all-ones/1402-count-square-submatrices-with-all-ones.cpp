a2d21f2b1ccb5250a8457ba779d9643ff13dfd1d
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-09-01 17:40:33 +0800

    Time: 7 ms (57.97%), Space: 27.5 MB (86.73%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -1,28 +1,46 @@
 class Solution 
 {
   public:
-    int countSquares(vector<vector<int>>& matrix) 
-    {
+    // int countSquares(vector<vector<int>>& matrix) 
+    // {
+    //   int m = matrix.size();
+    //   int n = matrix[0].size();
+    //   vector<vector<int>> dp(m+1, vector<int>(n+1,0));
+    //   for (int i = 1; i <= m; ++i) {
+    //     for (int j = 1; j <=n; j++) {
+    //       dp[i][j] = matrix[i-1][j-1];
+    //     }
+    //   }
+    //   int res = 0;
+
+    //   for (int i = 1; i <= m; ++i) {
+    //     for (int j = 1; j <= n; ++j) {
+    //       if(dp[i][j] == 0) {
+    //         continue;
+    //       }
+
+    //       dp[i][j] = min({dp[i-1][j],dp[i-1][j-1],dp[i][j-1]})+1;
+    //       res += dp[i][j];
+    //     }
+    //   }
+    //   return res;
+    // }
+    int countSquares(vector<vector<int>>& matrix) {
+      int res = 0;
       int m = matrix.size();
       int n = matrix[0].size();
-      vector<vector<int>> dp(m+1, vector<int>(n+1,0));
-      for (int i = 1; i <= m; ++i) {
-        for (int j = 1; j <=n; j++) {
-          dp[i][j] = matrix[i-1][j-1];
-        }
-      }
-      int res = 0;
 
-      for (int i = 1; i <= m; ++i) {
-        for (int j = 1; j <= n; ++j) {
-          if(dp[i][j] == 0) {
-            continue;
+      for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+          if (i == 0 || j == 0) {
+            res += matrix[i][j];
+          } else if (matrix[i][j] == 1) {
+            matrix[i][j] = min({matrix[i - 1][j], matrix[i][j - 1], matrix[i - 1][j - 1]}) + 1;
+            res += matrix[i][j];
           }
-
-          dp[i][j] = min({dp[i-1][j],dp[i-1][j-1],dp[i][j-1]})+1;
-          res += dp[i][j];
         }
       }
+
       return res;
     }
 };
