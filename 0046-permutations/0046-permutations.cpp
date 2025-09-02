b770693addefff4c20543c755050e5864fae62d0
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-09-03 00:50:41 +0800

    Time: 0 ms (100%), Space: 10.7 MB (46.76%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,22 +1,26 @@
 class Solution {
-  public:
-    vector<vector<int>> permute(vector<int> &nums) {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
       vector<vector<int>> res;
-      vector<int> sol;
-      vector<bool> visited(nums.size(), false);
-      backtracking(nums ,res, sol, visited, 0);
+      recursive(nums, 0, res);
       return res;
     }
-    void backtracking(vector<int> &nums,vector<vector<int>> &res, vector<int> &sol,vector<bool> &visited, int count) {
-      if(count == nums.size()) res.push_back(sol);
 
-      for(int i = 0; i< nums.size(); ++i) {
-        if(visited[i]) continue;
-        visited[i] = true;
-        sol.push_back(nums[i]);
-        backtracking(nums, res, sol, visited, count+1);
-        visited[i] = false;
-        sol.pop_back();
+    void recursive(vector<int>& nums, int index, vector<vector<int>>& res) {
+      if (index == nums.size()) {
+        res.push_back(nums);
+      }
+      for (int i = index; i < nums.size(); i++) {
+        swap(nums[i], nums[index]);
+        recursive(nums, index + 1, res);
+        swap(nums[i], nums[index]);
       }
     }
 };
+
+
+/*
+位置交換
+
+
+*/
