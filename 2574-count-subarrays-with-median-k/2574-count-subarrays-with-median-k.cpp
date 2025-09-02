04918f31257adec21a8861ec89d40e2c7ebb311b
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-09-02 18:20:47 +0800

    Time: 31 ms (38.19%), Space: 56.7 MB (40.82%) - LeetHub

--- 2574-count-subarrays-with-median-k/2574-count-subarrays-with-median-k.cpp
+++ 2574-count-subarrays-with-median-k/2574-count-subarrays-with-median-k.cpp
@@ -1,82 +1,46 @@
-// class Solution {
-// public:
-//     int countSubarrays(vector<int>& nums, int k) {
-//       unordered_map<int,int> evenMp, oddMp;
-//       int n = nums.size();
-//       int idx = 0;
-//       for (int i = 0; i < n; i++)  {
-//         if(nums[i] == k) {
-//           idx = i;
-//           break;
-//         }
-//       }
-      
-//       evenMp[0] = 1;
-
-//       int preSum = 0;
-//       int res = 0;
-//       for (int i = 0; i < n; i++) {
-//         if (nums[i] > k) preSum++;
-//         if (nums[i] < k) preSum--;
-//         if (i  >= idx)  {
-//           if (i%2 == 0) {
-//             res += evenMp[preSum - 1];
-//             res += oddMp[preSum];
-//           } else {
-//             res += oddMp[preSum - 1];
-//             res += evenMp[preSum];
-//           }
-          
-          
-//         }
-//         if (i%2 == 0) {
-//           evenMp[preSum]++;
-//         } else {
-//           oddMp[preSum]++;
-//         }
-//       }
-//       return res;
-//     }
-// };
 class Solution {
 public:
-    int countSubarrays(vector<int>& nums, int k) 
-    {
-        for (auto& x: nums)
-        {
-            if (x>k) x=1;
-            else if (x==k) x=0;
-            else x=-1;
+    int countSubarrays(vector<int>& nums, int k) {
+      unordered_map<int,int> evenMp, oddMp;
+      int n = nums.size();
+      int idx = 0;
+      for (int i = 0; i < n; i++)  {
+        if(nums[i] == k) {
+          idx = i;
+          break;
         }
-        
-        unordered_map<int, int>oddSum;
-        unordered_map<int, int>evenSum;
-        
-        evenSum[0] = 1;
-        
-        int sum = 0;
-        int ret = 0;
-        for (int i=0; i<nums.size(); i++)
-        {
-            sum += nums[i];
-            if (i%2==0)
-            {
-                ret += evenSum[sum-0];
-                ret += oddSum[sum-1];
-                oddSum[sum] += 1;
-            }
-            else
-            {
-                ret += oddSum[sum-0];
-                ret += evenSum[sum-1];
-                evenSum[sum] += 1;
-            }
+      }
+      
+      oddMp[0] = 1;
+
+      int preSum = 0;
+      int res = 0;
+      for (int i = 0; i < n; i++) {
+        if (nums[i] > k) preSum++;
+        if (nums[i] < k) preSum--;
+
+        if (i >= idx)  {
+          if (i%2 == 0) {
+            res += oddMp[preSum];
+            res += evenMp[preSum - 1];
+          } else {
+            res += evenMp[preSum];
+            res += oddMp[preSum - 1];
+          }
+        } else {
+           if (i%2 == 0) {
+            evenMp[preSum]++;
+          } else {
+            oddMp[preSum]++;
+          }
         }
-        
-        return ret;
-        
+
+      }
+
+      return res;
     }
 };
+
 /*
 
  [3,2,1,4,5]
