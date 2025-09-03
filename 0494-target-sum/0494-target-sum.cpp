a6c4b01c010218a322700dc78c55a712b949de4c
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-09-03 23:56:23 +0800

    Time: 141 ms (36.63%), Space: 53.9 MB (13.84%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -21,16 +21,16 @@ public:
 
 
     int findTargetSumWays(vector<int>& nums, int target) {
-      unordered_map<int,int> counter;
-      counter[0] = 1;
-      for(int num: nums){
+      unordered_map<int,int> mp;
+      mp[0] = 1;
+      for (int i = 0; i < nums.size(); i++) {
         unordered_map<int,int> tmp;
-        for(auto [value, count]: counter) {
-          tmp[value+num] += count;
-          tmp[value-num] += count;
+        for (auto& [sum, count] : mp) {
+          tmp[sum + nums[i]] += count;
+          tmp[sum - nums[i]] += count;
         }
-        counter = tmp;
+        mp = tmp;
       }
-      return counter[target];
+      return mp[target];
     }
 };
