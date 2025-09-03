8715143877f0643bed0f45c0710720d0eb3e6373
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-09-03 20:43:05 +0800

    Time: 32 ms (5.07%), Space: 63.5 MB (5.17%) - LeetHub

--- 0811-number-of-subarrays-with-bounded-maximum/0811-number-of-subarrays-with-bounded-maximum.cpp
+++ 0811-number-of-subarrays-with-bounded-maximum/0811-number-of-subarrays-with-bounded-maximum.cpp
@@ -1,14 +1,47 @@
 class Solution {
 public:
-    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
-      return count(nums, right) - count(nums, left - 1);
-    }
+    // int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+    //   return count(nums, right) - count(nums, left - 1);
+    // }
 
-    int count(vector<int>& nums , int bound) {
-      int res = 0, cur = 0;
-      for (auto& num : nums) {
-        cur = num <= bound ? cur + 1 : 0;
-        res += cur;
+    // int count(vector<int>& nums , int bound) {
+    //   int res = 0, cur = 0;
+    //   for (auto& num : nums) {
+    //     cur = num <= bound ? cur + 1 : 0;
+    //     res += cur;
+    //   }
+    //   return res;
+    // }
+    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+      int n = nums.size();
+      vector<int> prevGreaterOrEqual(n,-1);
+      vector<int> nextGreater(n, -1);
+      stack<int> st;
+      st.push(-1);
+      for (int i = 0; i < n; i++) {
+        while (st.top() != -1 && nums[st.top()] < nums[i]) {
+          st.pop();
+        }
+        prevGreaterOrEqual[i] = st.top();
+        st.push(i);
+      }
+      while(!st.empty()) {
+        st.pop();
+      }
+      st.push(n);
+      for (int i = n - 1; i >= 0; --i) {
+        while (st.top() != n && nums[st.top()] <= nums[i]) {
+          st.pop();
+        }
+        nextGreater[i] = st.top();
+        st.push(i);
+      }
+
+      int res = 0;
+      for (int i = 0; i < n; ++i) {
+        if (left <= nums[i] && nums[i] <= right) {
+          res +=  (nextGreater[i] - i) * (i - prevGreaterOrEqual[i]);
+        }
       }
       return res;
     }
