2580b0f26945227cc08a6f8f5c9ebda2c64ce285
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-09-04 03:12:44 +0800

    Time: 283 ms (71.8%), Space: 97.7 MB (72.56%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
@@ -5,39 +5,74 @@ class Solution {
     }
   };
 public:
-    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
-      priority_queue<pair<double,int>,vector<pair<double,int>>,smaller> pq;
+    // double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+    //   priority_queue<pair<double,int>,vector<pair<double,int>>,smaller> pq;
 
-      for(int i = 0 ;i < classes.size(); i++) {
-        double pass = classes[i][0];
-        double total = classes[i][1];
-        double delta = (pass+1)/(total+1) - pass/total;
+    //   for(int i = 0 ;i < classes.size(); i++) {
+    //     double pass = classes[i][0];
+    //     double total = classes[i][1];
+    //     double delta = (pass+1)/(total+1) - pass/total;
+    //     pq.push({delta, i});
+    //   }
+
+    //   while(extraStudents) {
+    //     auto [delta, index] = pq.top();
+    //     pq.pop();
+    //     classes[index][0]++;
+    //     classes[index][1]++;
+    //     double pass = classes[index][0];
+    //     double total = classes[index][1];
+    //     double del = (pass+1)/(total+1) - pass/total;
+    //     pq.push({del, index});
+    //     extraStudents--;
+    //   }
+    //   double res = 0;
+    //   for(auto cla : classes) {
+    //     res += 1.0*cla[0]/cla[1];
+    //   }
+    //   res /= (double)classes.size();
+
+    //   return res;
+    // }
+
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+      priority_queue<pair<double, int>> pq;
+      
+      for (int i = 0; i < classes.size(); i++) {
+        int pass = classes[i][0];
+        int total = classes[i][1];
+        double delta =  1.0 * (pass + 1) / (total + 1) - 1.0 * (pass) / (total);
         pq.push({delta, i});
       }
 
-      while(extraStudents) {
-        auto [delta, index] = pq.top();
+      while (extraStudents > 0) {
+        auto [delta, cla] = pq.top();
         pq.pop();
-        classes[index][0]++;
-        classes[index][1]++;
-        double pass = classes[index][0];
-        double total = classes[index][1];
-        double del = (pass+1)/(total+1) - pass/total;
-        pq.push({del, index});
         extraStudents--;
+        int pass = ++classes[cla][0];
+        int total = ++classes[cla][1];
+        double newDelta =  1.0 * (pass + 1) / (total + 1) - 1.0 * (pass) / (total);
+        pq.push({newDelta, cla});
       }
-      double res = 0;
-      for(auto cla : classes) {
-        res += 1.0*cla[0]/cla[1];
-      }
-      res /= (double)classes.size();
 
-      return res;
+      double res = 0;
+      for (int i = 0; i < classes.size(); i++) {
+          int pass = classes[i][0];
+          int total = classes[i][1];
+          res += 1.0 * (pass) / (total);
+      }
+      return res/classes.size();
     }
 };
 
 /*
 
+each class pass rate passi/total
+
+priority queue<pair<double, i>> pq
+
+passi + 1 /total + 1 - passi/total
+average pass ratio = sum(class pass)/classNum
 
 
 */
