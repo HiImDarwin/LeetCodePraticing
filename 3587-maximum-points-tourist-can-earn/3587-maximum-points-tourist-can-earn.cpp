35dd06272a86ff57723054c111dff4ba2995d3b2
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-09-04 21:22:24 +0800

    Time: 330 ms (79.56%), Space: 57.2 MB (92.45%) - LeetHub

--- 3587-maximum-points-tourist-can-earn/3587-maximum-points-tourist-can-earn.cpp
+++ 3587-maximum-points-tourist-can-earn/3587-maximum-points-tourist-can-earn.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     int maxScore(int n, int k, vector<vector<int>>& stayScore, vector<vector<int>>& travelScore) {
-      vector<vector<vector<int>>> dp(k+1,vector<vector<int>>(n,vector<int>(2,INT_MIN)));
+      vector<vector<int>> dp(2,vector<int>(n,INT_MIN));
 
       for (int city = 0 ; city < n; city++) {
-        dp[0][city][0] = 0;
+        dp[0][city] = 0;
       }
 
       for (int day = 1; day <= k ; ++day) {
-        for (int city = 0 ; city < n; city++) {
+        fill(dp[1].begin(), dp[1].end(), INT_MIN);
+        for (int city = 0; city < n; city++) {
           for (int i = 0; i < n; i++) {
-            if (i == city) continue;
-            dp[day][city][0] = max(dp[day][city][0], 
-                                max(dp[day - 1][i][0], dp[day - 1][i][1]) + travelScore[i][city]);
-          }
-          for (int d = 1; d <= day; d++) {
-            dp[day][city][1] = max(dp[day][city][1],
-                                   dp[day-d][city][0] + stayScore[d-1][city]);
-          }
+            if (i == city) {
+               dp[1][city] = max(dp[1][city], dp[0][city] + stayScore[day - 1][city]);
+            }else  {
+              dp[1][city] = max(dp[1][city], dp[0][i] + travelScore[i][city]);
+            }
+          }  
         }
+        swap(dp[0],dp[1]);
       }
 
-      int maxScore = 0;
+      int maxScore = INT_MIN;
       for (int i = 0; i < n ; i++) {
-        maxScore = max({maxScore, dp[k][i][0], dp[k][i][1]});
+        maxScore = max(maxScore, dp[0][i]);
       }
 
       return maxScore;
