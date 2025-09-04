be508486e45ed9643e4d10e3bcceac8fa0ae3bd8
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-09-05 00:37:41 +0800

    Time: 33 ms (95.94%), Space: 102.5 MB (96.47%) - LeetHub

--- 2256-count-words-obtained-after-adding-a-letter/2256-count-words-obtained-after-adding-a-letter.cpp
+++ 2256-count-words-obtained-after-adding-a-letter/2256-count-words-obtained-after-adding-a-letter.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
     int wordCount(vector<string>& startWords, vector<string>& targetWords) {
-      int count = 0;
-      unordered_set<string> startSet;
-      for (auto& s : startWords) {
-        sort(s.begin(), s.end());
-        startSet.insert(s);
+      unordered_set<int> startSet;
+      for (const auto& s : startWords) {
+        int charMask = 0;
+        for (auto& c : s) {
+          int idx = c - 'a';
+          charMask |= (1 << idx);
+        }
+        startSet.insert(charMask);
       }
+
       int res = 0;
-      for (auto& s : targetWords) {
-        sort(s.begin(),s.end());
-        for (int i = 0 ; i < s.length(); i++) {
-          string tmp = s.substr(0,i) + s.substr(i+1);
-          if (startSet.find(tmp) != startSet.end()) {
+      for (const auto& s : targetWords) {
+        int targetMask = 0;
+        for (auto& c : s) {
+          int idx = c - 'a';
+          targetMask |= (1 << idx);
+        }
+        for (auto& c : s) {
+          int idx = c - 'a';
+          targetMask ^= (1 << idx);
+          if (startSet.find(targetMask) != startSet.end()) {
             res++;
             break;
           }
+          targetMask ^= (1 << idx);
         }
       }
       return res;
     }
 };
-
-/*
-trie
-O(n * m)
-
-only on operation ! so check
-
-DFS in trie O(n * m)
-  check letter in target didin't duplicate in path
-  the word in startWords can't be same as targetwords
-
-
-*/
