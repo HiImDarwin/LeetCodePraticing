5a21876e143e0d3a32225000296b76285de74768
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-09-05 23:42:37 +0800

    Time: 561 ms (28.72%), Space: 561.1 MB (17.33%) - LeetHub

--- 1244-distinct-echo-substrings/1244-distinct-echo-substrings.cpp
+++ 1244-distinct-echo-substrings/1244-distinct-echo-substrings.cpp
@@ -1,35 +1,49 @@
 class Solution {
 public:
-    int distinctEchoSubstrings(string text) {
-      int n = text.length();
-      text = "#" + text;
-      auto dp = vector<vector<int>>(n+1,vector<int>(n+1,0));
-      dp[0][0] = 0;
-      unordered_set<string> Set;
+    // int distinctEchoSubstrings(string text) {
+    //   int n = text.length();
+    //   text = "#" + text;
+    //   auto dp = vector<vector<int>>(n+1,vector<int>(n+1,0));
+    //   dp[0][0] = 0;
+    //   unordered_set<string> Set;
 
-      for (int i = 0; i <=n; ++i) {
-        for (int j = i + 1; j <= n; ++j) {
-          if (text[i] == text[j]) {
-            dp[i][j] = dp[i-1][j-1] + 1;
+    //   for (int i = 0; i <=n; ++i) {
+    //     for (int j = i + 1; j <= n; ++j) {
+    //       if (text[i] == text[j]) {
+    //         dp[i][j] = dp[i-1][j-1] + 1;
+    //       }
+    //       if (dp[i][j] >= j-i) { //this means two substring in concatenation 
+    //         string str = text.substr(i + 1, j - i);
+    //         Set.insert(str);
+    //       }
+    //     }
+    //   }
+    //   return Set.size();
+    // }
+
+    int distinctEchoSubstrings(string text) {
+      unordered_set<string> res;
+      for(int i = 1; i <= text.length() / 2; i++) {
+        int len = 0;
+        for(int left = 0, right = i; right < text.length(); left++,right++) {
+          if (text[left] == text[right]) {
+            len++;
+          } else {
+            len = 0;
           }
-          if (dp[i][j] >= j-i) { //this means two substring in concatenation 
-            string str = text.substr(i + 1, j - i);
-            Set.insert(str);
+          if (len == i) {
+            res.insert(text.substr(left,i));
+            len--;
           }
         }
       }
-      return Set.size();
+
+      return res.size();
     }
 };
 
         
 /*
-dp[i][j]
-
-i means start index 
-j means substrin half length
-dp[i] = dp[]
-"abcabcabc"
 
 
 
