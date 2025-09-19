36a15b4183389e30ba13e1eafebc15966d5c8d8d
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-09-19 17:08:26 +0800

    Time: 0 ms (100%), Space: 9.6 MB (38.16%) - LeetHub

--- 2130-maximum-product-of-the-length-of-two-palindromic-subsequences/2130-maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
+++ 2130-maximum-product-of-the-length-of-two-palindromic-subsequences/2130-maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
@@ -1,59 +1,76 @@
 class Solution {
 public:
+    // int maxProduct(string s) {
+    //   int n = s.size();
+    //   int all = (1 << n) - 1;
+    //   int res = 0;
+    //   for (int subset = 1; subset < all; subset++) {
+    //     res = max(res, longestPalindrome(s, all - subset) * longestPalindrome(s,subset));
+    //   }
+    //   return res;
+    // }
+    // int longestPalindrome(string& s, int state) {
+    //   string t;
+    //   for (int i = 0; i < s.size(); i++) {
+    //     if ((state >> i) & 1) {
+    //       t += s[s.size() - 1 - i];
+    //     }
+    //   }
+
+    //   int n = t.size();
+    //   vector<vector<int>> dp(n, vector<int>(n));
+    //   for (int i = 0; i < n; i++) {
+    //     dp[i][i] = 1;
+    //   }
+    //   for (int len = 2; len <= n; len++) {
+    //     for (int i = 0; i + len - 1 < n; i++) {
+    //       int j = i + len - 1;
+    //       if (t[i] == t[j]) {
+    //         dp[i][j] = dp[i+1][j-1] + 2;
+    //       } else {
+    //         dp[i][j] = max(dp[i][j - 1], dp[i + 1][j]);
+    //       }
+    //     }
+    //   }
+
+    //   return dp[0][n-1];
+    // }
     int maxProduct(string s) {
-      int n = s.size();
+      int n = s.length();
       int all = (1 << n) - 1;
-      int res = 0;
-      for (int subset = 1; subset < all; subset++) {
-        res = max(res, longestPalindrome(s, all - subset) * longestPalindrome(s,subset));
+      vector<int> lp(1 << n, 0);
+      // initialize
+      for (int i = 0; i < n; i++) {
+        lp[1 << i] = 1;
+      }
+      int res = 1;
+      for (int state = 1; state <= all; state++) {
+        if(__builtin_popcount(state) <= 1) {
+          continue;
+        } 
+        int leftIdx = 31 - __builtin_clz(state);
+        int rightIdx = __builtin_ctz(state);
+        if (s[leftIdx] == s[rightIdx]) {
+          int submask = state;
+          submask ^= (1 << leftIdx);
+          submask ^= (1 << rightIdx);
+          lp[state] = lp[submask] + 2;
+        } else {
+          lp[state] = max(lp[state ^ (1 << leftIdx)], lp[state ^ (1 << rightIdx)]);
+        }
+
+        res = max(res, lp[state] * lp[all & (~state)]);
       }
       return res;
-    }
-    int longestPalindrome(string& s, int state) {
-      string t;
-      for (int i = 0; i < s.size(); i++) {
-        if ((state >> i) & 1) {
-          t += s[s.size() - 1 - i];
-        }
-      }
-
-      int n = t.size();
-      vector<vector<int>> dp(n, vector<int>(n));
-      for (int i = 0; i < n; i++) {
-        dp[i][i] = 1;
-      }
-      for (int len = 2; len <= n; len++) {
-        for (int i = 0; i + len - 1 < n; i++) {
-          int j = i + len - 1;
-          if (t[i] == t[j]) {
-            dp[i][j] = dp[i+1][j-1] + 2;
-          } else {
-            dp[i][j] = max(dp[i][j - 1], dp[i + 1][j]);
-          }
-        }
-      }
-
-      return dp[0][n-1];
     }
 };
 
 /*
+暴力枚舉
+1. 切成兩個subsequence
+2. 讓所有 subsequence 計算 LPS
 
-LIS longest Incresing Subsequence (X)
-LIS longest palindrone Subsequence
-manacher palindrone substring
-prefix Map
 
-1. how to find all palindrone Subsequence ?
-  prefix Map (n^2)
-  for each index give vector<int> bitmask for it
-  list all possiable 
-  dp[i][j]
 
-2. how to solve disjoint ?
-  only length 12
-  bitmask for picking the index
 
-  ---XX--X-XX-
-  -Y----YY----
 */
