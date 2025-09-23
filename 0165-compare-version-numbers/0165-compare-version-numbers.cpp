9d5d4f27b3bb49a84886e7c2c9faa99b41ad0fcf
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-09-24 03:10:42 +0800

    Time: 0 ms (100%), Space: 8.2 MB (87.43%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+      int ver1, ver2;
+      int l = 0, r = 0;
+      while (l < version1.length() && r < version2.length()) {
+        ver1 = 0;
+        ver2 = 0;
+        
+        while (l < version1.length() && version1[l] != '.' ) {
+          ver1 *= 10;
+          ver1 += version1[l] - '0';
+          l++;
+        }
+
+        while (r < version2.length() && version2[r] != '.' ) {
+          ver2 *= 10;
+          ver2 += version2[r] - '0';
+          r++;
+        }
+
+        if (ver1 < ver2) {
+          return -1;
+        } else if (ver1 > ver2) {
+          return 1;
+        }
+
+        l++;
+        r++;
+      }
+      if (l < version1.length() && checkRemaining(l, version1)) {
+        return 1;
+      } else if (r < version2.length() && checkRemaining(r, version2)) {
+        return -1;
+      }
+
+      return 0;
+    }
+
+
+    bool checkRemaining(int idx, string& version) {
+      while (idx < version.length()) {
+        if (version[idx] != '.' && version[idx] != '0') {
+          return true; // 有非 0 的數字
+        }
+        idx++;
+      }
+      return false;
+    }
+};
+
+/*
+compare each part of revisions
+
+
+
+*/
