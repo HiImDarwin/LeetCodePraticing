eee3aa3bd0c0e04ed15202f4ab18ababcb04c3db
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-12 17:18:27 +0800

    Time: 0 ms (100%), Space: 19.9 MB (92.21%) - LeetHub

--- 1224-minimum-falling-path-sum-ii/1224-minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/1224-minimum-falling-path-sum-ii.cpp
@@ -1,32 +1,64 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& grid) {
-      int n = grid.size();
-      vector<int> dp(n,INT_MAX);
+    // int minFallingPathSum(vector<vector<int>>& grid) {
+    //   int n = grid.size();
+    //   vector<int> dp(n,INT_MAX);
 
-      for (int i = 0; i < n; i ++) {
-        dp[i] = grid[0][i];
-      } 
+    //   for (int i = 0; i < n; i ++) {
+    //     dp[i] = grid[0][i];
+    //   } 
 
-      for (int i = 0; i < n - 1; i++) {
-        pair<int,int> smallest = {INT_MAX, -1}, secondSmall = {INT_MAX, -1};
-        for(int j = 0; j < n; j++) {
-          if (dp[j] < smallest.first) {
-            secondSmall = smallest;
-            smallest = {dp[j],j};
-          } else if (dp[j] < secondSmall.first) {
-            secondSmall = {dp[j],j};
-          }
-        }
-        for(int j = 0; j < n; j++) {
-          if(j == smallest.second) {
-            dp[j] = grid[i+1][j] + secondSmall.first;
-          } else {
-            dp[j] = grid[i+1][j] + smallest.first;
-          }
+    //   for (int i = 0; i < n - 1; i++) {
+    //     pair<int,int> smallest = {INT_MAX, -1}, secondSmall = {INT_MAX, -1};
+    //     for(int j = 0; j < n; j++) {
+    //       if (dp[j] < smallest.first) {
+    //         secondSmall = smallest;
+    //         smallest = {dp[j],j};
+    //       } else if (dp[j] < secondSmall.first) {
+    //         secondSmall = {dp[j],j};
+    //       }
+    //     }
+    //     for(int j = 0; j < n; j++) {
+    //       if(j == smallest.second) {
+    //         dp[j] = grid[i+1][j] + secondSmall.first;
+    //       } else {
+    //         dp[j] = grid[i+1][j] + smallest.first;
+    //       }
+    //     }
+    //   }
+    //   return *min_element(dp.begin(),dp.end());
+    // }
+
+  int minFallingPathSum(vector<vector<int>>& grid) {
+    int n = grid.size();
+    vector<int> dp(n,0);
+    for (int i = 0; i < n; i++) {
+      dp[i] = grid[0][i];
+    }
+    for (int row = 1; row < n; row++) {
+      pair<int,int> smallest = {INT_MAX, -1}, secondSmall = {INT_MAX, -1};
+      for (int col = 0; col < n; col++) {
+        if (dp[col] < smallest.first) {
+          secondSmall = smallest;
+          smallest.first = dp[col];
+          smallest.second = col;
+        } else if (dp[col] < secondSmall.first) {
+          secondSmall.first = dp[col];
+          secondSmall.second = col;
         }
       }
-      return *min_element(dp.begin(),dp.end());
+
+      for (int col = 0; col < n; col++) {
+        int val = smallest.second != col ? smallest.first : secondSmall.first;
+        dp[col] = grid[row][col] + val;
+      }
     }
 
+    return *min_element(dp.begin(), dp.end());
+  }
 };
+
+/*
+  if i choose i as end
+
+*/
