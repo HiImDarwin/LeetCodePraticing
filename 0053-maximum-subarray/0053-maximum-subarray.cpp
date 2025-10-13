a41123703ca4dca5b762f7752fc73947ab146370
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-14 00:10:53 +0800

    Time: 0 ms (100%), Space: 71.7 MB (80.34%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    //DP[i] == max(DP[i-1]+num[i],num[i])
-    // int maxSubArray(vector<int>& nums) {
-    //     int DP[nums.size()];
-    //     int res =nums[0];
-    //     DP[0]=nums[0];
-    //     for(int i=1;i<nums.size();i++) {
-    //         DP[i] = max(DP[i-1]+nums[i],nums[i]);
-    //         res = max(DP[i],res);
-    //     }
-    //     return res;
-    // }
     int maxSubArray(vector<int>& nums) {
-      long res = INT_MIN;
-      long total = 0;
-      for (const auto& num : nums) {
-        total = max((long)num , total + num);
-        res = max(res, total); 
+      int res = INT_MIN;
+      int sum = 0;
+      for (const int& num : nums) {
+        sum += num;
+        res = max(res, sum);
+        sum = sum < 0 ? 0 : sum;
       }
       return res;
     }
 };
+
+/*
+
+subarray
+
+
+
+*/ 
