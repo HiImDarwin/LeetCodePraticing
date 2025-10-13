fb7c7a568cad6fa998e0dde217f722f8199a1702
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-13 17:19:49 +0800

    Time: 0 ms (100%), Space: 18.6 MB (50.03%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,33 +10,63 @@
  */
 class Solution {
 public:
+    // ListNode* mergeKLists(vector<ListNode*>& lists) {
+    //   if (lists.size() == 0) {
+    //     return nullptr;
+    //   }
+
+    //   priority_queue<pair<int,ListNode*>, vector<pair<int,ListNode*>>, greater<>> pq;
+
+    //   for (ListNode* list : lists) {
+    //     if (!list) {
+    //       continue;
+    //     }
+    //     pq.push({list->val, list});
+    //   }
+
+    //   ListNode dummy = ListNode();
+    //   ListNode* curr = &dummy;
+
+    //   while (!pq.empty()) {
+    //     auto [val, list] = pq.top();
+    //     pq.pop();
+    //     curr->next = list;
+    //     curr = curr->next;
+    //     if (list->next) {
+    //       pq.push({list->next->val, list->next});
+    //     }
+    //   }
+
+    //   return dummy.next;
+    // }
+
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-      if (lists.size() == 0) {
+      int n = lists.size();
+      if (n == 0) {
         return nullptr;
       }
-
-      priority_queue<pair<int,ListNode*>, vector<pair<int,ListNode*>>, greater<>> pq;
-
-      for (ListNode* list : lists) {
-        if (!list) {
-          continue;
+      //divide
+      while (n > 1) {
+        int k = (n + 1) / 2;
+        for (int i = 0; i < n / 2; i++) {
+          lists[i] = mergeTwoLists(lists[i],lists[i + k]);
         }
-        pq.push({list->val, list});
+        n = k;
       }
+      return lists[0];
 
-      ListNode dummy = ListNode();
-      ListNode* curr = &dummy;
+    }
 
-      while (!pq.empty()) {
-        auto [val, list] = pq.top();
-        pq.pop();
-        curr->next = list;
-        curr = curr->next;
-        if (list->next) {
-          pq.push({list->next->val, list->next});
-        }
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+      if (list1 == nullptr || list2 == nullptr) {
+        return list1 == nullptr ? list2 : list1;
       }
-
-      return dummy.next;
+      if (list1 -> val <= list2 -> val) {
+        list1 -> next = mergeTwoLists(list1->next, list2);
+        return list1;
+      } else {
+        list2->next = mergeTwoLists(list1, list2->next);
+        return list2;
+      }
     }
 };
