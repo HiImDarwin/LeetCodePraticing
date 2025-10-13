14c81c4a4fdc9d0b9213608ebfb8d9cab390e426
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-14 00:33:48 +0800

    Time: 0 ms (100%), Space: 11.8 MB (11.85%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
+  // void sortColors(vector<int>& nums) {
+  //   vector<int> color(3,0);
+  //   for (auto& num : nums) {
+  //     color[num]++;
+  //   }
+  //   int idx = 0;
+  //   for (int i = 0; i < nums.size(); i++) {
+  //     while (color[idx] == 0) {
+  //       idx++;
+  //     }
+  //     nums[i] = idx;
+  //     color[idx]--;
+  //   }
+  // 
+
   void sortColors(vector<int>& nums) {
-    vector<int> color(3,0);
-    for (auto& num : nums) {
-      color[num]++;
-    }
-    int idx = 0;
-    for (int i = 0; i < nums.size(); i++) {
-      while (color[idx] == 0) {
-        idx++;
+    int whiteStart = 0;
+    int blueStart = nums.size();
+    int curr = 0;
+    while (curr < nums.size() && curr < blueStart) {
+      if (nums[curr] == 0) {
+        swap(nums[curr], nums[whiteStart++]);
+        curr++;
+      } else if (nums[curr] == 2) {
+        swap(nums[curr], nums[--blueStart]);
+      } else {
+        curr++;
       }
-      nums[i] = idx;
-      color[idx]--;
     }
   }
 };
