fb05b5b2cde75aaef087baa49402109eb5e394cb
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-13 14:35:35 +0800

    Time: 296 ms (33.51%), Space: 132.5 MB (79.42%) - LeetHub

--- 2671-shortest-cycle-in-a-graph/2671-shortest-cycle-in-a-graph.cpp
+++ 2671-shortest-cycle-in-a-graph/2671-shortest-cycle-in-a-graph.cpp
@@ -1,53 +1,105 @@
 class Solution {
-   int n;
-   vector<unordered_set<int>> adj;
+
 public:
-    int findShortestCycle(int n, vector<vector<int>>& edges) 
-    {
+    // int findShortestCycle(int n, vector<vector<int>>& edges) 
+    // {
+    //   this->n = n;
+    //   adj.resize(n);
+    //   for (auto& edge: edges) {
+    //     int a = edge[0], b = edge[1];
+    //     adj[a].insert(b);
+    //     adj[b].insert(a);
+    //   }
+    //   int res = INT_MAX;
+    //   for (auto& edge: edges) {
+    //     int a = edge[0], b = edge[1];
+    //     adj[a].erase(b);
+    //     adj[b].erase(a);
+    //     res = min(res, bfs(a,b));
+    //     adj[a].insert(b);
+    //     adj[b].insert(a);
+    //   }
+
+    //   return res == INT_MAX ? -1 : res+1;
+    // }
+
+    // int bfs (int start, int end) {
+    //   vector<int> visited(n);
+    //   queue<int> q;
+    //   q.push(start);
+    //   visited[start] = 1;
+
+    //   int step = 0;
+    //   while (!q.empty()) {
+    //     int len = q.size();
+    //     for (int i = 0; i < len; ++i) {
+    //       int cur = q.front();
+    //       q.pop();
+    //       if (cur == end) return step;
+    //       for (int nxt: adj[cur]) {
+    //         if (visited[nxt]) continue;
+    //         q.push(nxt);
+    //         visited[nxt] = 1;
+    //       }
+    //     }
+    //     step++;
+    //   }
+    //   return INT_MAX;
+    // }
+
+    int findShortestCycle(int n, vector<vector<int>>& edges) {
       this->n = n;
       adj.resize(n);
-      for (auto& edge: edges) {
-        int a = edge[0], b = edge[1];
-        adj[a].insert(b);
-        adj[b].insert(a);
-      }
-      int res = INT_MAX;
-      for (auto& edge: edges) {
-        int a = edge[0], b = edge[1];
-        adj[a].erase(b);
-        adj[b].erase(a);
-        res = min(res, bfs(a,b));
-        adj[a].insert(b);
-        adj[b].insert(a);
+      for (auto edge : edges) {
+        int u = edge[0];
+        int v = edge[1];
+        adj[u].insert(v);
+        adj[v].insert(u);
       }
 
-      return res == INT_MAX ? -1 : res+1;
+      int res = INT_MAX;
+      for (int i = 0; i < n; i++) {
+        if (adj[i].size() >= 2) {
+          
+          bfs(i, res);
+        }
+      }
+
+      return res == INT_MAX ? -1 : res;
     }
 
-    int bfs (int start, int end) {
-      vector<int> visited(n);
-      queue<int> q;
-      q.push(start);
-      visited[start] = 1;
+    void bfs(int start , int &res) {
+      vector<int> dist(n, -1);
+      queue<pair<int, int>> qu; // node parent
+      qu.push({start,-1});
+      dist[start] = 0;
 
-      int step = 0;
-      while (!q.empty()) {
-        int len = q.size();
-        for (int i = 0; i < len; ++i) {
-          int cur = q.front();
-          q.pop();
-          if (cur == end) return step;
-          for (int nxt: adj[cur]) {
-            if (visited[nxt]) continue;
-            q.push(nxt);
-            visited[nxt] = 1;
+      while (!qu.empty()) {
+        auto [node, parent] = qu.front();
+        qu.pop();
+
+        for (const int &v : adj[node]) {
+          if (dist[v] == -1) {
+            qu.push({v, node});
+            dist[v] = dist[node] + 1;
+          } else if (v != parent) {
+            res = min(res, dist[node] + dist[v] + 1);
           }
         }
-        step++;
       }
-      return INT_MAX;
     }
+  private:
+    int n;
+    vector<unordered_set<int>> adj;
 };
+
+/*
+
+BFS
+  find out the adj map for each node
+  start with any node
+  memo for step in each node
+*/
 
 
  
