765fb92af37a2b169d617f027e51d6c27bd900a3
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-15 02:27:34 +0800

    Time: 847 ms (5%), Space: 398.8 MB (5.42%) - LeetHub

--- 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.cpp
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
-      vector<int> preSumLeft(k + 1), preSumRight(k + 1), OriginLeft(k + 1), Originright(k + 1);
+      vector<int> preSumLeft(k + 1), preSumRight(k + 1);
       unordered_map<int,int> mp;
       for (auto& fruit : fruits) {
         mp[fruit[0]] = fruit[1];
@@ -10,26 +10,20 @@ public:
       for (int i = 0; i <= k; i++) {
         sum += mp[startPos - i];
         preSumLeft[i] = sum;
-        if (i * 2 <= k) {
-          OriginLeft[i*2] = sum;
-        }
       }
 
       sum = 0;
       for (int i = 0; i <= k; i++) {
         sum += mp[startPos + i];
         preSumRight[i] = sum;
-        if (i * 2 <= k) {
-          Originright[i*2] = sum;
-        }
       }
 
       int res = max(preSumLeft[k], preSumRight[k]);
-      for (int i = 1; i <= k / 2; i++) {
-        res = max(res, OriginLeft[i * 2] + preSumRight[k - (i * 2)] - mp[startPos]);
+      for (int i = 0; i <= k ; i++) {
+        res = max(res, preSumLeft[i] + preSumRight[(k - i) / 2] - mp[startPos]);
       }
-      for (int i = 1; i <= k / 2; i++) {
-        res = max(res, Originright[i * 2] + preSumLeft[k - (i * 2)] -  mp[startPos]);
+      for (int i = 0; i <= k; i++) {
+        res = max(res, preSumRight[i] + preSumLeft[(k - i) / 2] - mp[startPos]);
       }
 
       return res;
@@ -46,5 +40,6 @@ preSumLeft[k]
 preSumRight[k]
 Originleft[k]
 Originright[k]
+2x + 
 
 */
