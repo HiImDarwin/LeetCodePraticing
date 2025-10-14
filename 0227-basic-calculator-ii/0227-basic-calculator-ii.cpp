7747d9f43b5535aa9e8a90c4a4f211fc9cab9625
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-14 16:00:35 +0800

    Time: 11 ms (33.79%), Space: 16.6 MB (16.73%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -2,48 +2,34 @@ class Solution {
 public:
     int calculate(string s) {
       stack<long long> st;
-      stack<char> oprators;
-      int sign = 1;
+      char op = '+';
       int num = 0;
       for (int i = 0; i < s.length(); i++) {
-        if (s[i] == ' ') {
-          continue;
-        } else if (isdigit(s[i])) {
+        if (isdigit(s[i])) {
           num *= 10;
           num += s[i] - '0';
-        } else {
-          if (oprators.empty()) {
-            st.push(sign * num);
-          } else {
-            long long prevNum = st.top();
+        } 
+        
+        if (!isdigit(s[i]) && !isspace(s[i]) || i == s.length() - 1) {
+          if (op == '+') {
+            st.push(num);
+          } else if (op == '-') {
+            st.push(-num);
+          } else if (op == '*') {
+            long long prev = st.top();
             st.pop();
-            if (oprators.top() == '*') {
-              st.push(prevNum * num);
-            } else if (oprators.top() == '/') {
-              st.push(prevNum / num);
-            }
-            oprators.pop();
+            st.push(prev * num);
+          } else if (op == '/') {
+            long long prev = st.top();
+            st.pop();
+            st.push(prev / num);
+          }
+
+          if (!isdigit(s[i])) {
+            op = s[i];
           }
           num = 0;
-          sign = 1;
-          if (s[i] == '-') {
-            sign = -1;
-          } else if (s[i] == '*' || s[i] == '/') {
-            oprators.push(s[i]);
-          }
-        }
-      }
-      if (oprators.empty()) {
-        st.push(sign * num);
-      } else {
-        long long prevNum = st.top();
-        st.pop();
-        if (oprators.top() == '*') {
-          st.push(prevNum * num);
-        } else if (oprators.top() == '/') {
-          st.push(prevNum / num);
-        }
-        oprators.pop();
+        }     
       }
       
       long long sum = 0;
