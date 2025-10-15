13da3b26e0b276fddee4f78bca78d7b946a78ae0
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-16 03:48:32 +0800

    Time: 187 ms (5%), Space: 12.5 MB (5.77%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,33 +1,53 @@
 class Solution {
 public:
+    // int splitArray(vector<int>& nums, int k) {
+    //   int left = *max_element(nums.begin(), nums.end());
+    //   int right = accumulate(nums.begin(), nums.end(), 0);
+    //   while (left < right) {
+    //     int mid = left + (right - left) / 2;
+    //     if (check(nums, mid, k)) {
+    //       right = mid;
+    //     } else {
+    //       left = mid + 1;
+    //     }
+    //   }
+    //   return left;
+    // }
+
+    // bool check(const vector<int>& nums,const int val, const int k) {
+    //   int n = nums.size();
+    //   int sum = 0;
+    //   int partitionConut = 1;
+
+    //   for (int i = 0; i < n; i++) {        
+    //     if (sum + nums[i] > val) {
+    //       sum = nums[i];
+    //       partitionConut++;
+    //     } else {
+    //       sum += nums[i];
+    //     }
+    //   }
+    //   return partitionConut <= k;
+    // }
+
     int splitArray(vector<int>& nums, int k) {
-      int left = *max_element(nums.begin(), nums.end());
-      int right = accumulate(nums.begin(), nums.end(), 0);
-      while (left < right) {
-        int mid = left + (right - left) / 2;
-        if (check(nums, mid, k)) {
-          right = mid;
-        } else {
-          left = mid + 1;
-        }
-      }
-      return left;
-    }
 
-    bool check(const vector<int>& nums,const int val, const int k) {
       int n = nums.size();
-      int sum = 0;
-      int partitionConut = 1;
+      vector<vector<long>> dp(n + 1, vector<long>(k + 1, INT_MAX));
+      nums.insert(nums.begin(), 0);
+      dp[0][0] = 0;
 
-      for (int i = 0; i < n; i++) {        
-        if (sum + nums[i] > val) {
-          sum = nums[i];
-          partitionConut++;
-        } else {
-          sum += nums[i];
+      for (int i = 1; i <= n; i++) {
+        for (int s = 1; s <= min(k, i); s++) {
+          long sum = 0;
+          for (int j = i; j >= s; j--) {
+            sum += nums[j];
+            dp[i][s] = min(dp[i][s],max(dp[j - 1][s - 1], sum));
+          }
         }
       }
-      return partitionConut <= k;
+
+      return dp[n][k];
     }
 };
 
@@ -44,6 +64,10 @@ brute force
   C(n - 1,k - 1) split way
   O(n^k)
   find all split way with recursive and use prefix Sum to find the min max
+
+
+dp
+dp[i][k] split 1~i into k part minMax Val
 
 
 binary search
