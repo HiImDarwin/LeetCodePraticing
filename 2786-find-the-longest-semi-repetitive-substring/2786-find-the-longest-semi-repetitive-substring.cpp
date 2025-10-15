cb9c4d75356d6fdd61a2f19e44a20b55ae2e93bc
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-15 17:18:30 +0800

    Time: 0 ms (100%), Space: 10.3 MB (63.97%) - LeetHub

--- 2786-find-the-longest-semi-repetitive-substring/2786-find-the-longest-semi-repetitive-substring.cpp
+++ 2786-find-the-longest-semi-repetitive-substring/2786-find-the-longest-semi-repetitive-substring.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     int longestSemiRepetitiveSubstring(string s) {
-      if (s.length() == 1) {
-        return 1;
-      }
       int left = 0, right = 1;
       int last_pos = 0;
-      bool found_one = false;
-      int res = 0;
+      int res = 1;
       while (right < s.length()) {
         if (s[right] == s[right - 1]) {
           left = last_pos;
