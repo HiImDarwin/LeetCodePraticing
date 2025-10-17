f2bc0884fdba637f29111fe84757684461140ae0
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-17 17:12:46 +0800

    Time: 12 ms (74.46%), Space: 16.6 MB (75.54%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,42 +1,56 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-      vector<vector<pair<int,int>>> adj(n);
-      for(auto road : edges) {
-        int u = road[0], v = road[1], w= road[2];
-        adj[u].push_back({v,w});
-        adj[v].push_back({u,w});
+      vector<vector<int>> dist(n, vector<int>(n, INT_MAX/2));
+      for (int i = 0; i < n; i++) {
+        dist[i][i] = 0;
       }
-      vector<int> city(n,0);
-      for(int i = 0; i < n; ++i) {
-        vector<int> dist(n,INT_MAX);
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
-        pq.push({0,i});
-        dist[i] = 0;
-        while (!pq.empty()) {
-          auto [curDist, curCity] = pq.top();
-          pq.pop();
-          if( curDist > dist[curCity]) continue;
-          city[i]++;
-          for (auto [nei, travel] : adj[curCity]) {
-            if(curDist + travel <= distanceThreshold && curDist + travel < dist[nei]) {
-              dist[nei] = curDist + travel;
-              pq.push({dist[nei], nei});
-            }
+      for (auto& edge : edges) {
+        int u = edge[0];
+        int v = edge[1];
+        int distance = edge[2];
+        dist[u][v] = distance;
+        dist[v][u] = distance;
+      }
+      floydWarshell(dist,edges);
+      int minCount = INT_MAX;
+      int city = -1;
+      for (int i = 0; i < n; i++) {
+        int count = 0;
+        for (int j = 0; j < n; j++) {
+          if (i == j) continue;
+          if (dist[i][j] <= distanceThreshold) {
+            count++;
+          }
+        }
+        if (count <= minCount) {
+          minCount = count;
+          city = i;
+        }
+      }
+      return city;
+    }
+
+
+    void floydWarshell(vector<vector<int>>& dist, vector<vector<int>>& edges) {
+      int n = dist.size();
+      for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+          for (int j = 0; j < n; j++) {
+            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
           }
         }
       }
-      int res = 0;
-      int count = n;
-      for (int i = 0; i < n; ++i) {
-        cout <<  city[i] << endl;
-        if(city[i] <= count) {
-          res = i;
-          count = city[i];
-          
-        }
-      }
-
-      return res;
     }
 };
+
+/*
+bidirectional
+is there self edge?
+is the grapch connected?
+is there parallel edge?
+
+
+floyd warshell (v^3)
+dijkstra(ElogE)
+*/
