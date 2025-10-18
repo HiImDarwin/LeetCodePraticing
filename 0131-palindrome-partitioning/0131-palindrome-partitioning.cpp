30bbd2cdfdf444a72c32111b4ea603148b33c620
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-18 13:39:31 +0800

    Time: 31 ms (87.58%), Space: 53.5 MB (85.09%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,27 +1,33 @@
 class Solution {
-public:
+  vector<vector<bool>> memo;
+  public:
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> res;
-        vector<string> sol;
-        backtracking(s,0,res,sol);
-        return res;
+      int n = s.length();
+      vector<string> sol;
+      vector<vector<string>> res;
+      memo.resize(n, vector<bool>(n,false));
+      recursive(s, 0, sol, res);
+      return res;
     }
 
-    void backtracking(string s,int start,vector<vector<string>> &res, vector<string> &sol) {
-        if(start==s.length()) res.push_back(sol);
-        for(int i=start;i<s.length();++i){
-            if(isPalindrome(s,start,i)) {
-                sol.push_back(s.substr(start,i-start+1));
-                backtracking(s,i+1,res,sol);
-                sol.pop_back();
-            }
+    void recursive(const string& s, int idx, vector<string> &sol, vector<vector<string>>& res) {
+      if (idx == s.length()) {
+        res.push_back(sol);
+        return;
+      }
+      for (int i = idx; i < s.length(); i++) {
+        if (s[i] == s[idx] && (i - 1 < idx + 1 || memo[idx + 1][i - 1])) {
+          memo[idx][i] = true;
+          sol.push_back(s.substr(idx, i - idx + 1));
+          recursive(s, i + 1, sol, res);
+          sol.pop_back();
         }
-    }
-
-    bool isPalindrome(string &s,int left, int right){
-        while(left<=right){
-            if(s[left++]!=s[right--]) return false;
-        }
-        return true;
+      }
     }
 };
+
+/*
+recursive way
+
+
+*/
