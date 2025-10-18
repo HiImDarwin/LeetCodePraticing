66cf4c6205daa998d95933175fa4b6bd08bee33a
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-19 03:07:44 +0800

    Time: 239 ms (18.09%), Space: 85.3 MB (10.84%) - LeetHub

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,44 +1,50 @@
 class Solution {
 public:
     int shortestPath(vector<vector<int>>& grid, int k) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<vector<int>>> dist(m,vector<vector<int>>(n,vector<int>(k+1,INT_MAX)));
-        queue<tuple<int,int,int>> qu;
-        dist[0][0][k] = 0;
-        qu.push({0,0,k});
-        while (!qu.empty()) {
-          auto [x, y, hammer] = qu.front();
-          qu.pop();
-          int step = dist[x][y][hammer];
-          if (x == m-1 && y == n-1) return step;
-          for (const auto& [dx, dy] : directions) {
-            int nX = x + dx;
-            int nY = y + dy;
-            int newHammer = hammer;
-            int newStep = step + 1;
-            if (nX < 0 || nX >= m || nY < 0 || nY >= n) {
+      int m = grid.size();
+      int n = grid[0].size();
+      vector<vector<vector<int>>> states(k + 1, vector<vector<int>>(m, vector<int>(n,INT_MAX)));
+      for (int i = 0; i <=k; i++) {
+        states[i][0][0] = 0;
+      }
+      queue<vector<int>> qu;
+      qu.push({0, 0, k, 0});
+      while (!qu.empty()) {
+        vector<int> state = qu.front();
+        qu.pop();
+        int x = state[0];
+        int y = state[1];
+        int k_left = state[2];
+        int step = state[3];
+        if (x == m - 1 && y == n - 1) {
+          return step;
+        }
+        for (auto [dx, dy] : directions) {
+          int nei_x = x + dx;
+          int nei_y = y + dy;
+          int newSetp = step + 1;
+          if (nei_x < 0 || nei_x >= m || nei_y < 0 || nei_y >= n) {
+            continue;
+          }
+          if (grid[nei_x][nei_y] == 1) {
+            int new_k_left = k_left - 1;
+            if (new_k_left < 0 || newSetp >= states[new_k_left][nei_x][nei_y]) {
               continue;
             }
-            if (grid[nX][nY] == 1) {
-              if (newHammer <= 0) {
-                continue;
-              }
-              newHammer--;
-            } 
-            if (newStep < dist[nX][nY][newHammer]) {
-              dist[nX][nY][newHammer] = newStep;
-              qu.push({nX, nY, newHammer});
+            states[new_k_left][nei_x][nei_y] = newSetp;
+            qu.push({nei_x, nei_y, new_k_left, newSetp});
+          } else {
+            if (newSetp >= states[k_left][nei_x][nei_y]) {
+              continue;
             }
+            states[k_left][nei_x][nei_y] = newSetp;
+            qu.push({nei_x, nei_y, k_left, newSetp});
           }
         }
-        return -1;
+      } 
+      return -1;
     }
-    vector<pair<int,int>> directions = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+
+    vector<pair<int,int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 };
-/*
-  無法greedy 的說先用掉穿牆或是不用穿牆哪一個好。
-  you need to record both satuation
-
-
-*/
