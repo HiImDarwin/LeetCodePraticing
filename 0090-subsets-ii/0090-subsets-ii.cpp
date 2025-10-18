bd95944c8fae66660c7ee966fde005a3e5ccc26a
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-18 15:58:37 +0800

    Time: 0 ms (100%), Space: 10.6 MB (48.79%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<int> tmp;
-        vector<vector<int>> res;
-        backtracking(nums, 0, tmp, res);
-        return res;
-        
+      vector<vector<int>> res;
+      vector<int> sol;
+
+      sort(nums.begin(), nums.end());
+      recursive(nums, 0, sol, res);
+      return res;
     }
 
-    // void backtracking(vector<int>& nums ,int start,vector<int> &tmp,vector<vector<int>> &res) {
-    //     if(start >= nums.size()) {
-    //         res.push_back(tmp);
-    //         return;
-    //     } 
-    //     tmp.push_back(nums[start]);
-    //     backtracking(nums, start+1, tmp, res);
-    //     tmp.pop_back();
-    //     start++;
-    //     while(start < nums.size() && nums[start-1] == nums[start]) {
-    //         start++;
-    //     }
-    //     backtracking(nums, start, tmp, res);
-    // }
-
-    void backtracking(vector<int>& nums ,int start,vector<int> &tmp,vector<vector<int>> &res) {
-        res.push_back(tmp);
-
-        for(int i=start;i<nums.size();++i) {
-            if(i>start && nums[i]==nums[i-1]) continue;
-            tmp.push_back(nums[i]);
-            backtracking(nums, i+1,tmp,res);
-            tmp.pop_back();
+    void recursive(vector<int>& nums, int idx, vector<int>& sol, vector<vector<int>>& res) {
+      res.push_back(sol);
+      if (idx == nums.size()) {
+        return;
+      }
+      for (int i = idx; i < nums.size(); i++) {
+        if (i != idx && nums[i] == nums[i - 1]) {
+          continue;
         }
+        sol.push_back(nums[i]);
+        recursive(nums, i + 1, sol, res);
+        sol.pop_back();
+      }
     }
 };
