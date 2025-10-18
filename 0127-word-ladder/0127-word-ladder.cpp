342a408bf3ec7015105ce4b44c92e8759e8121d8
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-19 01:08:02 +0800

    Time: 95 ms (41.6%), Space: 48.5 MB (7.08%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,35 +1,51 @@
 class Solution {
 public:
-  int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-    unordered_map<string,vector<string>> patternMap;
-    bool foundEnd = false;
-    for (auto word : wordList) {
-      foundEnd |= (word == endWord);
-      for(int i = 0; i<word.length(); ++i) {
-        string pattern = word;
-        pattern[i] = '*';
-        patternMap[pattern].push_back(word);
-      }
-    }
-    if(!foundEnd) return 0;
-
-    queue<string> qu;
-    qu.push(beginWord);
-    unordered_map<string,int> dist;
-    dist[beginWord] = 1;
-    while(!qu.empty()) {
-      string node = qu.front(); qu.pop();
-      for(int i = 0; i < node.length(); ++i) {
-        string pattern = node;
-        pattern[i] = '*';
-        for( auto sib : patternMap[pattern]) {
-          if(sib == endWord) return  dist[node] + 1;
-          if(dist.count(sib)) continue;
-          dist[sib] = dist[node] + 1;
-          qu.push(sib);
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+      unordered_map<string,vector<string>> routeMap;
+      int m = beginWord.length();
+      for (int i = 0; i < m; i++) {
+        for (const string& word : wordList) {
+          string tmp = word;
+          tmp[i] = '*';
+          routeMap[tmp].push_back(word);
         }
       }
+      unordered_set<string> visited;
+      queue<pair<string,int>> qu;
+      qu.push({beginWord,1});
+      visited.insert(beginWord);
+      while (!qu.empty()) {
+        auto [node, numOfWord] = qu.front();
+        qu.pop();
+        if (node == endWord) {
+          return numOfWord;
+        }
+        for (int i = 0; i < m; i++) {
+          int c = node[i];
+          node[i] = '*';
+          for (auto& nei : routeMap[node]) {
+            if (visited.count(nei)) {
+              continue;
+            }
+            qu.push({nei, numOfWord + 1});
+            visited.insert(nei);
+          }
+          node[i] = c;
+        }
+      }
+
+      return 0;
     }
-    return  0;
-  }
 };
+
+
+/*
+  dfs
+  hot dot dog lot log cog
+  *ot: hot dot log
+  *og: dog log cog
+  h*t: hot
+  d*t: dot
+
+
+*/
