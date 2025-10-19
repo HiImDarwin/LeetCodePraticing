0db127dae342855e0b86fa60dbd9adfdb544e62d
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-19 13:46:01 +0800

    Time: 8 ms (22.85%), Space: 20.8 MB (5.13%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,67 +1,42 @@
 class Solution {
 public:
-    //************************BFS+indrgree************************
-    // vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-    //     vector<int> indegree(numCourses,0);
-    //     vector<vector<int>> adj(numCourses);
-    //     for(auto &pre: prerequisites) {
-    //         indegree[pre[0]]++;
-    //         adj[pre[1]].push_back(pre[0]);
-    //     }
-    //     vector<int> res;
-    //     queue<int> qu;
-        
-    //     for(int i=0; i<numCourses; i++) {
-    //         if(indegree[i]==0) {
-    //             qu.push(i);
-    //         }
-    //     }
-    //     //BFS
-    //     while(!qu.empty()) {
-    //         int node = qu.front();
-    //         res.push_back(node);
-    //         qu.pop();
-    //         //check adj
-    //         for(auto &adj_node: adj[node]) {
-    //             if(--indegree[adj_node]==0){
-    //                 qu.push(adj_node);
-    //             }
-    //         }
-    //     }
-    //     return (res.size() == numCourses) ? res : vector<int>{};
-    // }
-    //*************************DFS+postorder******************************
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        vector<bool> visited(numCourses,false);
-        vector<bool> onPath(numCourses,false);
-        vector<int> res;
+      vector<int> indegree(numCourses);
+      vector<vector<int>> adj(numCourses);
 
-        for(auto& pre: prerequisites) {
-            adj[pre[1]].push_back(pre[0]);
-        }
-        for(int i=0; i<numCourses;i++ ) {
-            if(!visited[i]) {
-                if(!DFS(adj,i,visited,res,onPath)) {
-                    return {};
-                }
-            }
-        }
-        reverse(res.begin(),res.end());
-        return res.size() == numCourses  ? res : vector<int>{} ; 
-    }
+      for(auto pre : prerequisites) {
+        int a = pre[0];
+        int b = pre[1];
+        indegree[a]++;
+        adj[b].push_back(a);
+      }
 
-    bool DFS(vector<vector<int>> &adj,int node,vector<bool> &visited,vector<int> &res,vector<bool> &onPath) {
-        visited[node] = true;
-        onPath[node] = true;
-        for(auto &next_node: adj[node]) {
-            if(onPath[next_node]) return false;
-            if(!visited[next_node]) {
-                 DFS(adj,next_node,visited,res,onPath);
-            }
+      queue<int> qu;
+      for (int i = 0; i < numCourses; i++) {
+        if (indegree[i] == 0) {
+          qu.push(i);
         }
-        onPath[node] = false;
-        res.push_back(node);
-        return true;
+      }
+      
+
+      vector<int> courseOrder;
+
+      while (!qu.empty()) {
+        queue<int> new_qu;
+
+        while (!qu.empty()) {
+          int course = qu.front();
+          qu.pop();
+          courseOrder.push_back(course);
+          for (auto& nei : adj[course]) {
+            if (--indegree[nei] == 0) {
+              new_qu.push(nei);
+            }
+          }
+        } 
+        qu.swap(new_qu);
+      }
+
+      return (courseOrder.size() == numCourses) ? courseOrder : vector<int>{};
     }
 };
