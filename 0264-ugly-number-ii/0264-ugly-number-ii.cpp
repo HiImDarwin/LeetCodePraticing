16f757ff474ba1099ba076fb79abef69517d551b
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-19 15:23:03 +0800

    Time: 5 ms (53.73%), Space: 12.4 MB (39.67%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,50 +1,24 @@
 class Solution {
 public:
-    // int nthUglyNumber(int n) {
-    //   set<long long> numSet;
-      
-    //   numSet.insert(1);
-    //   int count = 0;
-    //   for (auto& num : numSet) {
-    //     count++;
-    //     if (count == n) return num;
-    //     numSet.insert(num * 2);
-    //     numSet.insert(num * 3);
-    //     numSet.insert(num * 5);
-    //   }
-    
-    //   return 0; 
-    // }
-
     int nthUglyNumber(int n) {
-      int i = 0, j = 0, k = 0;
-      vector<int> res;
-      res.push_back(1);
+      vector<long long> ugly(n);
+      int p2 = 0, p3 = 0, p5 = 0; // this is index
+      ugly[0] = 1;
 
-      for (int t = 0; t < n - 1; t++) {
-        int cur = min({res[i] * 2, res[j] * 3, res[k] * 5});
-        res.push_back(cur);
-        if (cur == res[i] * 2) i++;
-        if (cur == res[j] * 3) j++;
-        if (cur == res[k] * 5) k++;
+      for (int i = 1; i < n; i++) {
+        long long next = min({ugly[p2] * 2, ugly[p3] * 3, ugly[p5] * 5});
+        ugly[i] = next;
+        if (next == ugly[p2] * 2) p2++;
+        if (next == ugly[p3] * 3) p3++;
+        if (next == ugly[p5] * 5) p5++;
       }
 
-      return res[n - 1];
+      return ugly.back();
     }
-
 };
 
-
 /*
-i *2 *3 *4 *5 *9
 
-
-new ugly num = old[i] * 2
-               old[j] * 3
-               old[k] * 5
-
-dp[t] = min(dp[i]*2, dp[j]*3, dp[k]*5);
-update(i,j,k);
 
 
 */
