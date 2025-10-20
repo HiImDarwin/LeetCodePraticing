4da795544e2a20d5d54d68c134f2d274ce041997
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-20 13:40:09 +0800

    Time: 25 ms (94.87%), Space: 112.6 MB (88.72%) - LeetHub

--- 2101-last-day-where-you-can-still-cross/2101-last-day-where-you-can-still-cross.cpp
+++ 2101-last-day-where-you-can-still-cross/2101-last-day-where-you-can-still-cross.cpp
@@ -1,82 +1,84 @@
-class Solution {
-  vector<int> parent;
-  int FindParent(int x) {
-    if (parent[x] != x) {
-      parent[x] = FindParent(parent[x]);
-    }
-    return parent[x];
-  }
-
-  bool Unite(int x, int y) {
-    int xParent = FindParent(x);
-    int yParent = FindParent(y);
-    if (xParent == yParent) {
-      return false;
-    }
-    if (xParent > yParent) {
-      swap(xParent,yParent);
-    }
-    parent[yParent] = xParent;
-    return true;
-  }
-
-public:
-    int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
-      parent.resize(row*col + 2);
-      iota(parent.begin(),parent.end(),0);
-      vector<vector<int>> grid(row,vector<int>(col,0));
-      for(auto& cell : cells) {
-        int x = cell[0], y = cell[1];
-        grid[x-1][y-1] = 1;
-      }
-
-      for (int i = 0; i < col; ++i) {
-        Unite(row*col, i);
-      }
-
-      for (int i = 0; i < col; ++i) {
-        Unite(row*col+1, (row - 1)*col + i);
-      }
-
-      for (int i = 0; i < row; ++i) {
-        for (int j = 0; j < col; ++j) {
-          if (grid[i][j] == 1) {
-            continue;
-          }
-          for (auto& [dx, dy] : directions) {
-            int neiX = i + dx;
-            int neiY = j + dy;
-            if (neiX < 0 || neiX >= row || neiY < 0 || neiY >= col || grid[neiX][neiY] == 1) {
-              continue;
-            } 
-            Unite(i * col + j, neiX * col + neiY);
-          }
-        }
-      }
-
-      int days = cells.size();
-      while (days >=0 ) {
-
-        int topParent = FindParent(row*col);
-        int botParent = FindParent(row*col+1);
-        if (topParent == botParent) return days;
-           
-        int recoverX = cells[days-1][0]-1;
-        int recoverY = cells[days-1][1]-1;
-        grid[recoverX][recoverY] = 0;
-        for (auto& [dx, dy] : directions) {
-          int neiX = recoverX + dx;
-          int neiY = recoverY + dy;
-          if (neiX < 0 || neiX >= row || neiY < 0 || neiY >= col || grid[neiX][neiY] == 1) {
-            continue;
-          } 
-          Unite(recoverX * col + recoverY, neiX * col + neiY);
-        }
-
-        days--;
-      }
-      return 0;
+class DSU {
+  public:
+  
+    DSU(int n) {
+      root_.resize(n);
+      iota(root_.begin(), root_.end(), 0);
+      size_.resize(n,1);
     }
 
-    vector<pair<int,int>> directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    int find(int x) {
+      if (root_[x] != x) {
+        root_[x] = find(root_[x]);
+      }
+      return root_[x];
+    }
+
+    bool unite(int x, int y) {
+      int rootX = find(x);
+      int rootY = find(y);
+      if (rootX == rootY) {
+          return false;;
+      }
+      
+      if (size_[rootX] > size_[rootY]) {
+        swap(rootX, rootY);
+      }
+      root_[rootX] = rootY;
+      size_[rootY] += size_[rootX];
+
+      return true;
+    }
+  
+  private:
+    vector<int> root_;
+    vector<int> size_;
 };
+
+class Solution {
+  public:
+    int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
+      DSU dsu = DSU(row * col + 2);// last 2 is top and bot
+      vector<vector<int>> grid(row,vector<int>(col,1));
+
+      for (int i = cells.size() - 1; i >= 0; i--) {
+        int x = cells[i][0] - 1, y = cells[i][1] - 1;
+        int index = x * col + y;
+        grid[x][y] = 0;
+
+        for (auto& [dx, dy] : directions) {
+          int nei_x = x + dx;
+          int nei_y = y + dy;
+          if (nei_x < 0 || nei_x >= row || nei_y < 0 || nei_y >= col || grid[nei_x][nei_y] == 1) {
+            continue;
+          }
+          int nei_index = nei_x * col + nei_y;
+          dsu.unite(index, nei_index);
+        }
+        if (x == 0) {
+          dsu.unite(index, row * col);
+        }
+        if (x == row - 1) {
+          dsu.unite(index, row * col + 1);
+        }
+        if (dsu.find(row * col) == dsu.find(row * col + 1)) {
+          return i;
+        }
+      }
+      return -1;
+    }
+  private:
+    vector<pair<int,int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+};
+
+
+
+/*
+Return the last day where it is possible to walk from the top to the bottom by only walking on land cells
+binary Search + bfs
+
+
+backwarding union find
+
+*/
+
