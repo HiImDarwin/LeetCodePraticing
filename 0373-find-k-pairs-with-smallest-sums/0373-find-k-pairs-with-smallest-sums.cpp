58f3628949e8cb2671fbcbf0ee9f2814897af48e
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-20 20:08:50 +0800

    Time: 48 ms (72.08%), Space: 144.9 MB (27.49%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,70 +1,32 @@
 class Solution {
 public:
-    // vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-    //   priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,less<>> pq;
-    //   int m = nums1.size();
-    //   int n = nums2.size();
-    //   vector<vector<int>> res;
-      
-    //   for (int i = 0;  i < m; i++) {
-    //     for (int j = 0; j < n; j++) {
-    //       if (pq.size() < k) {
-    //         pq.push({nums1[i] + nums2[j], i, j});
-    //       } else if (pq.size() == k && nums1[i] + nums2[j] >= get<0>(pq.top())) {
-    //         break;
-    //       } else if (pq.size() == k && nums1[i] + nums2[j] < get<0>(pq.top())) {
-    //         pq.pop();
-    //         pq.push({nums1[i] + nums2[j], i, j});
-    //       }  
-    //     }
-    //   }
-    //   while (!pq.empty()) {
-    //     auto [num, i, j] = pq.top();
-    //     pq.pop();
-    //     res.push_back({nums1[i],nums2[j]});
-    //   }
-    //   return res;
-    // }
-
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-      int m = nums1.size();
-      int n = nums2.size();
+      int n = nums1.size();
+      int m = nums2.size();
 
-      vector<vector<int>> ans;
-      set<pair<int, int>> visited;
-
-      priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
-                      greater<pair<int, pair<int, int>>>> minHeap;
-      minHeap.push({nums1[0] + nums2[0], {0, 0}});
-      visited.insert({0, 0});
-
-      while (k-- && !minHeap.empty()) {
-          auto top = minHeap.top();
-          minHeap.pop();
-          int i = top.second.first;
-          int j = top.second.second;
-
-          ans.push_back({nums1[i], nums2[j]});
-
-          if (i + 1 < m && visited.find({i + 1, j}) == visited.end()) {
-              minHeap.push({nums1[i + 1] + nums2[j], {i + 1, j}});
-              visited.insert({i + 1, j});
-          }
-
-          if (j + 1 < n && visited.find({i, j + 1}) == visited.end()) {
-              minHeap.push({nums1[i] + nums2[j + 1], {i, j + 1}});
-              visited.insert({i, j + 1});
-          }
+      vector<int> idx(n, 0); 
+      priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+      for (int i = 0; i < n; i++) {
+        pq.push({nums1[i] + nums2[idx[i]], i});
       }
-      return ans;
+
+      vector<vector<int>> res;
+      while (!pq.empty() && k-- > 0) {
+        auto [sum, idx_1] = pq.top();
+        pq.pop();
+        res.push_back({nums1[idx_1], nums2[idx[idx_1]]});
+        if (++idx[idx_1] < m) {
+          pq.push({nums1[idx_1] + nums2[idx[idx_1]], idx_1});
+        }
+      }
+
+      return res;
     }
 };
 
-
-
-
-
 /*
-在連續區間找第K個
-這就是 binary search 的起手式
+{1,1} {1,2} {1,3} {1, 4} 
+{2,1} {2,2} {2,3} {2, 4}
+{3,1} {3,2} {3,3} {3, 4} 
+
 */
