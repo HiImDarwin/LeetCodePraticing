c3c2fa0d336bf9ed62c4a661a6d1d712637d5227
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-20 18:08:53 +0800

    Time: 49 ms (36.67%), Space: 49.9 MB (38.09%) - LeetHub

---
+++ 1144-optimize-water-distribution-in-a-village/1144-optimize-water-distribution-in-a-village.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int minCostToSupplyWater(int n, vector<int>& wells, vector<vector<int>>& pipes) {
+      vector<vector<pair<int,int>>> adj(n + 1);
+      for (auto& pipe : pipes) {
+        int house1 = pipe[0];
+        int house2 = pipe[1];
+        int cost = pipe[2];
+        adj[house1].push_back({cost, house2});
+        adj[house2].push_back({cost, house1});
+      }
+
+      priority_queue<pair<int,int>,vector<pair<int,int>>, greater<>> pq;
+      for (int i = 0; i < n; i++) {
+        pq.push({wells[i],i + 1});
+      }
+
+      unordered_set<int> visited;
+      int totalCost = 0;
+
+      while (!pq.empty()) {
+        auto [cost, house] = pq.top();
+        pq.pop();
+        if (visited.count(house)) {
+          continue;
+        }
+        visited.insert(house);
+        totalCost += cost;
+        if (visited.size() == n) {
+          break;
+        }
+        for (pair<int, int>& nei : adj[house]) {
+          pq.push(nei);
+        }
+      }
+      return totalCost;
+    }
+};
+
+/*
+build first well with minimun cost wells
+put all wells in queue
+
+
+*/
