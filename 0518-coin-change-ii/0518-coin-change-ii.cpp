b008a86d9a52ee6d247fd2d02c0f5e272f3cfbeb
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-21 17:43:18 +0800

    Time: 3 ms (96.25%), Space: 10.4 MB (84.29%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-      vector<unsigned long long> dp(amount+1,0); 
+      vector<unsigned long long> dp(amount + 1, 0);
       dp[0] = 1;
-      for (int t = 0; t < coins.size(); t++) {
-        for (int i = coins[t]; i <= amount; ++i) {
-            dp[i] += dp[i - coins[t]];
+
+
+      for (int& coinAmount : coins) {
+        for (int i = 1; i <= amount; i++) { 
+          if (i - coinAmount < 0) {
+            continue;
+          }
+          dp[i] += dp[i - coinAmount];
         }
       }
-      return (int)dp[amount];
+      return dp.back();
     }
-
 };
 
-
-
 /*
-  number of combinations that make up that amount
+number of combination
+  for (amount)
 
 
+  1 2 
+  2 1
 
-  1 3 5
-
-  1 2 3 4 5 6 7 8 
-  1 1 2 3
-
-
-  1111
-  為了避免 131 113 這種重覆情況
-  我們使用1 3必須按照順序 就可以避免這問題
-  那怎麼做到 讓外圈for 以coin value 為優先
 */
