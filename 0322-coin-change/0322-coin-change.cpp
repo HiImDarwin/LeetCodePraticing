93ddb825aafc30f8251a9c7ebda3081b73385749
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-21 17:24:48 +0800

    Time: 23 ms (69.9%), Space: 17.7 MB (93.17%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-      vector<int> dp(amount+1,INT_MAX); 
+      vector<int> dp(amount + 1, INT_MAX / 2);
       dp[0] = 0;
       for (int i = 1; i <= amount; i++) {
-        for (int& money : coins) {
-          if(i - money < 0 || dp[i - money] == INT_MAX) {
+        for (int& coinAmount : coins) {
+          if (i - coinAmount < 0) {
             continue;
-          }
-          dp[i] =min(dp[i], dp[i-money] + 1);
+          } 
+          dp[i] = min(dp[i], dp[i - coinAmount] + 1);
         }
       }
-      return dp[amount] == INT_MAX ? -1 : dp[amount];
+      return dp[amount] == INT_MAX / 2 ? -1 : dp[amount];
     }
 };
 
-
 /*
-dp[n] = fewest number of coins to make up n
+每個 coin 我都能用多次
 
+for (i = 1 ~ amount)
+  for (coins)
+    dp[i] =min(dp[i],dp[i - coinAmount] + 1);
+  
 */
