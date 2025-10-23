54b0a863a369fbaa46e350992fbe331448810b99
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-10-23 17:08:17 +0800

    Time: 38 ms (63.91%), Space: 23.8 MB (62.13%) - LeetHub

--- 1389-minimum-moves-to-move-a-box-to-their-target-location/1389-minimum-moves-to-move-a-box-to-their-target-location.cpp
+++ 1389-minimum-moves-to-move-a-box-to-their-target-location/1389-minimum-moves-to-move-a-box-to-their-target-location.cpp
@@ -1,84 +1,97 @@
 class Solution {
+
 public:
     int minPushBox(vector<vector<char>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
+      m = grid.size();
+      n = grid[0].size();
 
-        int p_x, p_y, b_x, b_y;
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 'S') {
-                    p_x = i; p_y = j;
-                    grid[i][j] = '.';
-                } else if (grid[i][j] == 'B') {
-                    b_x = i; b_y = j;
-                    grid[i][j] = '.';
-                }
-            }
+
+      int p_x, p_y, b_x, b_y;
+      for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++) {
+          if (grid[i][j] == 'S') {
+            p_x = i;
+            p_y = j;
+            grid[i][j] = '.';
+          } else if (grid[i][j] == 'B') {
+            b_x = i;
+            b_y = j;
+            grid[i][j] = '.';
+          }
         }
-
-        using State = array<int,4>;
-        queue<State> qu;
-        set<State> visited;
-        qu.push({b_x, b_y, p_x, p_y});
-        visited.insert({b_x, b_y, p_x, p_y});
-
-        int push = 0;
-        while (!qu.empty()) {
-            int len = qu.size();
-            while (len--) {
-                auto [bx, by, px, py] = qu.front();
-                qu.pop();
-
-                if (grid[bx][by] == 'T') return push;
-
-                for (auto [dx, dy] : directions) {
-                    int nbx = bx + dx, nby = by + dy;   // next box position
-                    int npx = bx - dx, npy = by - dy;   // player must stand here before push
-
-                    if (nbx < 0 || nbx >= m || nby < 0 || nby >= n || grid[nbx][nby] == '#')
-                        continue;
-
-                    State next = {nbx, nby, npx, npy};
-                    if (visited.count(next)) continue;
-
-                    if (!moveBehindBox(grid, bx, by, px, py, npx, npy))
-                        continue;
-
-                    visited.insert(next);
-                    qu.push(next);
-                }
+      }
+      using State = array<int,4>;
+      queue<State> qu;
+      qu.push({b_x, b_y, p_x, p_y});
+      set<State> visited;
+      visited.insert({b_x, b_y, p_x, p_y});
+      
+      int push = 0;
+      while (!qu.empty()) {
+        int len = qu.size();
+        while (len--) {
+          auto [box_x, box_y, per_x, per_y] = qu.front();
+          qu.pop();
+          if (grid[box_x][box_y] == 'T') {
+            return push;
+          }
+          
+          for (int i = 0; i < 4; i++) {
+            int nei_x = box_x + directions[i].first;
+            int nei_y = box_y + directions[i].second;
+            int per_next_x = box_x + directions[(i + 2) % 4].first;
+            int per_next_y = box_y + directions[(i + 2) % 4].second;
+            State next = {nei_x, nei_y, per_next_x, per_next_y};
+            if (nei_x < 0 || nei_x >= m || nei_y < 0 || nei_y >= n || 
+                grid[nei_x][nei_y] == '#' || visited.count(next)) {
+              continue;
             }
-            push++;
+            if (!moveBehindBox(grid, box_x, box_y, per_x, per_y, per_next_x, per_next_y)) {
+              continue;
+            }
+            visited.insert(next);
+            qu.push(next);
+
+          }
         }
-        return -1;
+        push++;
+      }
+      
+      return -1;
     }
 
-private:
+  private:
     bool moveBehindBox(vector<vector<char>>& grid,int bx, int by, int px, int py, int tx, int ty) {
-        queue<pair<int,int>> qu;
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        qu.push({px, py});
-        visited[px][py] = true;
-        while (!qu.empty()) {
-            auto [x, y] = qu.front();
-            qu.pop();
-            if (x == tx && y == ty) return true;
-            for (auto [dx, dy] : directions) {
-                int nx = x + dx, ny = y + dy;
-                if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-                if (grid[nx][ny] == '#' || (nx == bx && ny == by) || visited[nx][ny]) continue;
-                visited[nx][ny] = true;
-                qu.push({nx, ny});
-            }
+      queue<pair<int,int>> qu;
+      vector<vector<bool>> visited(m, vector<bool>(n, false));
+      visited[px][py] = true;
+      qu.push({px, py});
+      while (!qu.empty()) {
+        auto [x, y] = qu.front();
+        qu.pop();
+        if (x == tx && y == ty) {
+          return true;
         }
-        return false;
-    }
+        for (auto [dx, dy] : directions) {
+          int nei_x = x + dx;
+          int nei_y = y + dy;
+          if (nei_x < 0 || nei_x >= m || nei_y < 0 || nei_y >= n) {
+            continue;
+          }
+          if (grid[nei_x][nei_y] == '#' || (nei_x == bx && nei_y== by) || visited[nei_x][nei_y]) {
+            continue;
+          }
+          visited[nei_x][nei_y] = true;
+          qu.push({nei_x, nei_y});
+        }
+      }
 
-    vector<pair<int,int>> directions = {{1,0},{-1,0},{0,1},{0,-1}};
+      return false;
+    }
+    vector<pair<int,int>> directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    
     int m, n;
 };
-
 
 
 /*
