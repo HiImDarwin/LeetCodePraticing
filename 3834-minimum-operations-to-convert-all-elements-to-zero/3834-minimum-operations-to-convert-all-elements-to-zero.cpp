e51cc30f7da9a33809bc681d6350b258961b6684
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2025-11-10 16:24:20 +0800

    Time: 35 ms (69.66%), Space: 222 MB (87.36%) - LeetHub

--- 3834-minimum-operations-to-convert-all-elements-to-zero/3834-minimum-operations-to-convert-all-elements-to-zero.cpp
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/3834-minimum-operations-to-convert-all-elements-to-zero.cpp
@@ -1,45 +1,65 @@
 class Solution {
 public:
+    // int minOperations(vector<int>& nums) {
+    //   nums.insert(nums.begin(), -1);
+    //   nums.push_back(-1);
+    //   int n = nums.size();
+    //   vector<int> pre(n), next(n);
+    //   // find prev smaller
+    //   vector<int> st;
+    //   st.push_back(0);
+
+    //   for (int i = 1; i < n - 1; i++) {
+    //     while (nums[st.back()] >= nums[i]) {
+    //       st.pop_back();
+    //     }
+    //     pre[i] = st.back();
+    //     st.push_back(i);
+    //   }
+
+    //   // find next smaller
+    //   st.clear();
+    //   st.push_back(n - 1);
+    //   for (int i = n - 2; i > 0; i--) {
+    //     while (nums[st.back()] >= nums[i]) {
+    //       st.pop_back();
+    //     }
+    //     next[i] = st.back();
+    //     st.push_back(i);
+    //   }
+
+    //   unordered_set<long long> intervels_set;
+    //   int res = 0;
+    //   for (int i = 1; i < n - 1; i++) {
+    //     if (nums[i] == 0) {
+    //       continue;
+    //     }
+    //     long long val = ((long long)pre[i] << 32) + next[i];
+    //     if (intervels_set.find(val) == intervels_set.end()) {
+    //       intervels_set.insert(val);
+    //       res++;
+    //     }
+    //   }
+    //   return res;
+    // }
     int minOperations(vector<int>& nums) {
-      nums.insert(nums.begin(), -1);
-      nums.push_back(-1);
-      int n = nums.size();
-      vector<int> pre(n), next(n);
-      // find prev smaller
-      vector<int> st;
-      st.push_back(0);
-
-      for (int i = 1; i < n - 1; i++) {
-        while (nums[st.back()] >= nums[i]) {
-          st.pop_back();
-        }
-        pre[i] = st.back();
-        st.push_back(i);
-      }
-
-      // find next smaller
-      st.clear();
-      st.push_back(n - 1);
-      for (int i = n - 2; i > 0; i--) {
-        while (nums[st.back()] >= nums[i]) {
-          st.pop_back();
-        }
-        next[i] = st.back();
-        st.push_back(i);
-      }
-
-      unordered_set<long long> intervels_set;
       int res = 0;
-      for (int i = 1; i < n - 1; i++) {
-        if (nums[i] == 0) {
-          continue;
-        }
-        long long val = ((long long)pre[i] << 32) + next[i];
-        if (intervels_set.find(val) == intervels_set.end()) {
-          intervels_set.insert(val);
+      stack<int> st;
+      for (int& x : nums) {
+        while (!st.empty() && st.top() > x) {
+          st.pop();
           res++;
         }
+        if (x > 0 && (st.empty() || st.top() < x)) {
+          st.push(x);
+        }
       }
+
+      while(!st.empty()) {
+        st.pop();
+        res++;
+      }
+
       return res;
     }
 };
