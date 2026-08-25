483811d2cb7d250e33786513162988fe5c3085bc
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2026-08-26 00:19:42 +0800

    Time: 0 ms (100%), Space: 21.6 MB (76.61%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,50 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-      int N = intervals.size();
-      int newStart =  newInterval[0];
-      int newEnd = newInterval[1];
       vector<vector<int>> res;
-
-      for (int i = 0; i < N; i++) {
-        int start = intervals[i][0];
-        int end = intervals[i][1];
-        if (end < newStart) {
-          res.push_back({start, end});
-        } else if ((start < newEnd && end >= newStart ) || 
-                    (newStart <= start && newEnd >= start)) {
-          newStart = min(start,newStart);
-          newEnd = max(end, newEnd);
-        } else if (newEnd < start) {
-          res.push_back({newStart, newEnd});
-          newStart = start;
-          newEnd = end;
+      vector<int> seg = newInterval;
+      bool notPush = true;
+      for(int i = 0; i < intervals.size(); i++) {
+        if (seg[0] > intervals[i][1]) {
+          res.push_back(intervals[i]);
+        } else if (seg[1] < intervals[i][0] && notPush) {
+          res.push_back(seg);
+          res.push_back(intervals[i]);
+          notPush = false;
+        } else if (seg[1] < intervals[i][0]) {
+          res.push_back(intervals[i]);
+        } else {
+          seg[0] = min(seg[0], intervals[i][0]);
+          seg[1] = max(seg[1], intervals[i][1]);
         }
+      } 
+      if (notPush) {
+        res.push_back(seg);
       }
-      res.push_back({newStart, newEnd});
+
       return res;
     }
 };
 
-
+//.   [.   ]   [.   ]
 /*
-
-XXX XXX XXX XXX
-  XXXX
-
-XXX   XXX XXX XXX
-  XXXX
-
-
-XXX     XXX XXX XXX
-  XXXX
-
-XXX        XXX XXX XXX
-     XXXX
-
-XXXXXX XXX XXX XXX
-  XX
+    [.  ]
+           [.   ]
+                  [.   ]
+     [                 ]
+             []
 
 
+     we got 4 satuation
 
 */
