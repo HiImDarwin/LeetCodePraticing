c346f89b03bea392f6747d7d1cb274e14db30b46
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2026-08-27 01:01:14 +0800

    Time: 84 ms (19.03%), Space: 113.4 MB (15.64%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
+  // bool containsDuplicate(vector<int>& nums) {
+  //   unordered_set<int> s;
+  //   for (const int& num : nums) {
+  //     if(s.find(num) != s.end()) {
+  //       return true;
+  //     }
+  //     s.insert(num);
+  //   }    
+  //   return false;
+  // }
   bool containsDuplicate(vector<int>& nums) {
-    unordered_set<int> s;
-    for (const int& num : nums) {
-      if(s.find(num) != s.end()) {
-        return true;
-      }
-      s.insert(num);
-    }    
-    return false;
+    unordered_set<int> numSet(nums.begin(), nums.end());
+    return numSet.size() < nums.size();
   }
 };
