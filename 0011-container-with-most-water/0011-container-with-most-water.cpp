d1e9826df8726ad0564a083f6a3b8851106cdf89
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2026-08-27 02:23:54 +0800

    Time: 4 ms (20.66%), Space: 62.8 MB (79.28%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,39 +1,57 @@
 class Solution {
 public:
+    // int maxArea(vector<int>& height) {
+    //   int n = height.size();
+    //   vector<int> postHigher(n);
+    //   vector<int> preHigher(n);
+    //   stack<int> st;
+    //   for (int i = n - 1; i >= 0; i--) {
+    //     while (!st.empty() && height[i] >= height[st.top()]) {
+    //       st.pop();
+    //     }
+    //     postHigher[i] = st.empty() ? -1 : st.top();
+    //     st.push(i);
+    //   }
+    //   st = stack<int>();
+    //   for (int i = 0; i < n; i++) {
+    //     while (!st.empty() && height[i] >= height[st.top()]) {
+    //       st.pop();
+    //     }
+    //     preHigher[i] = st.empty() ? -1 : st.top();
+    //     st.push(i);
+    //   }
+
+    //   int maxArea = 0;
+    //   int front = 0;
+    //   int end = n - 1;
+    //   int lowerHeight = INT_MAX;
+    //   int area;
+    //   while (front != -1 && end != -1) {
+    //     lowerHeight = min(height[front], height[end]);
+    //     area = lowerHeight * (end - front);
+    //     maxArea = max(maxArea, area);
+    //     if (lowerHeight == height[front]) {
+    //       front = postHigher[front];
+    //     } else {
+    //       end = preHigher[end];
+    //     }
+    //   }
+
+    //   return maxArea;
+    // }
     int maxArea(vector<int>& height) {
       int n = height.size();
-      vector<int> postHigher(n);
-      vector<int> preHigher(n);
-      stack<int> st;
-      for (int i = n - 1; i >= 0; i--) {
-        while (!st.empty() && height[i] >= height[st.top()]) {
-          st.pop();
-        }
-        postHigher[i] = st.empty() ? -1 : st.top();
-        st.push(i);
-      }
-      st = stack<int>();
-      for (int i = 0; i < n; i++) {
-        while (!st.empty() && height[i] >= height[st.top()]) {
-          st.pop();
-        }
-        preHigher[i] = st.empty() ? -1 : st.top();
-        st.push(i);
-      }
-
-      int maxArea = 0;
       int front = 0;
       int end = n - 1;
-      int lowerHeight = INT_MAX;
-      int area;
-      while (front != -1 && end != -1) {
-        lowerHeight = min(height[front], height[end]);
-        area = lowerHeight * (end - front);
-        maxArea = max(maxArea, area);
-        if (lowerHeight == height[front]) {
-          front = postHigher[front];
+      int maxArea = 0;
+
+      while (front < end) {
+        int minHeight = min(height[front], height[end]);
+        maxArea = max(maxArea, minHeight * (end - front));
+        if (minHeight == height[front]) {
+          front++;
         } else {
-          end = preHigher[end];
+          end--;
         }
       }
 
