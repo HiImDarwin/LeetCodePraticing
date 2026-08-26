55c3cdcda587800c722432604c5a179f35b8df62
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2026-08-27 02:15:55 +0800

    Time: 19 ms (6.49%), Space: 68.7 MB (11.08%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+      int n = height.size();
+      vector<int> postHigher(n);
+      vector<int> preHigher(n);
+      stack<int> st;
+      for (int i = n - 1; i >= 0; i--) {
+        while (!st.empty() && height[i] >= height[st.top()]) {
+          st.pop();
+        }
+        postHigher[i] = st.empty() ? -1 : st.top();
+        st.push(i);
+      }
+      st = stack<int>();
+      for (int i = 0; i < n; i++) {
+        while (!st.empty() && height[i] >= height[st.top()]) {
+          st.pop();
+        }
+        preHigher[i] = st.empty() ? -1 : st.top();
+        st.push(i);
+      }
+
+      int maxArea = 0;
+      int front = 0;
+      int end = n - 1;
+      int lowerHeight = INT_MAX;
+      int area;
+      while (front != -1 && end != -1) {
+        lowerHeight = min(height[front], height[end]);
+        area = lowerHeight * (end - front);
+        maxArea = max(maxArea, area);
+        if (lowerHeight == height[front]) {
+          front = postHigher[front];
+        } else {
+          end = preHigher[end];
+        }
+      }
+
+      return maxArea;
+    }
+};
+
+
+/*
+  two pointer n^2
+  right to left ascending arrry
+  left to right ascending array
+  
+
+*/
