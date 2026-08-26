bbde81dfccde1c8182138d7b3eb27c0559110f9a
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2026-08-26 22:56:08 +0800

    Time: 10 ms (20.07%), Space: 16.9 MB (27.19%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,39 +1,27 @@
 class Solution {
-  public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-      int n = candidates.size();
-      //vector<int> memo(target+1, -1);
-      vector<int> sol;
-      
-      //memo[0] = 1;
-      recursive(candidates, 0, target, sol);
-
-      return res;
-    }
-
-    void recursive(const vector<int>& candidates,int index , int target, vector<int>& sol){
-      if (target == 0) {
-        res.push_back(sol);
-      }
-      if (target < 0) return;
-      if (index == candidates.size()) return;
-
-      for (int i = index; i < candidates.size(); i++) {
-        sol.push_back(candidates[i]);
-        recursive(candidates, i, target - candidates[i], sol);
-        sol.pop_back();
+public:
+  vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    int n = candidates.size();
+    unordered_map<int, vector<vector<int>>> dy(target + 1);
+    dy[0]= {{}};
+    
+    for(int idx = 0; idx < n; idx++) {
+      for(int val = 0; val <= target; val++) {
+        if (dy.find(val - candidates[idx]) != dy.end()) {
+          for(vector<int>& vec :  dy[val - candidates[idx]]) {
+            dy[val].push_back(vec);
+            dy[val].back().push_back(candidates[idx]);
+          }
+        }
       }
     }
-  private:
-    vector<vector<int>> res;
+    
+    return dy[target];
+  }
 };
 
-
-/*
-element can be chosen unlimited tims
-{2, 3, 2} is equal to  {2, 2, 3}
-
-
-
-
-*/
+// sum to target
+// unlimited number of times
+// recursive way
+// dynamic way 
+//   error > 重覆計算 [2,3] [3,2] 我都會算到
