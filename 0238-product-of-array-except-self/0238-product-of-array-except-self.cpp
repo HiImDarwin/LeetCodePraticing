a50aa800a72063ad6fd32edc2f6827b90698bd7e
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2026-08-26 22:08:18 +0800

    Time: 0 ms (100%), Space: 40.4 MB (44.79%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
+  // vector<int> productExceptSelf(vector<int>& nums) {
+  //   int n = nums.size();
+
+  //   vector<int> pre(n, 1),post(n, 1);
+  //   int total = 1;
+  //   for(int i = 0; i < n - 1; i++) {
+  //     pre[i + 1] = pre[i] * nums[i];
+  //   } 
+  //   for(int i = n - 2; i >= 0; i--) {
+  //     post[i] = post[i + 1] * nums[i + 1]; 
+  //   }
+
+  //   vector<int> res(n);
+  //   for (int i = 0; i < n; i++) {
+  //     res[i] = pre[i] * post[i];
+  //   }
+
+  //   return res;
+  // }
+
   vector<int> productExceptSelf(vector<int>& nums) {
     int n = nums.size();
-    vector<int> pre(n, 1),post(n, 1);
-    int total = 1;
-    for(int i = 0; i < n - 1; i++) {
-      pre[i + 1] = pre[i] * nums[i];
-    } 
-    for(int i = n - 2; i >= 0; i--) {
-      post[i] = post[i + 1] * nums[i + 1]; 
+    vector<int> res(n,1);
+    int tmp = 1;
+    for (int i = 1; i < n; i++) {
+      res[i] = res[i - 1] * nums[i - 1]; 
     }
-
-    vector<int> res(n);
-    for (int i = 0; i < n; i++) {
-      res[i] = pre[i] * post[i];
+    for (int i = n - 1; i >= 0; i--) {
+      res[i] = res[i] * tmp;
+      tmp *= nums[i];
     }
 
     return res;
