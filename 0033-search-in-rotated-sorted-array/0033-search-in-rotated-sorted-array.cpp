4efae6435d400c547594e61219f27a066dd2bc94
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2026-08-30 17:15:20 +0800

    Time: 0 ms (100%), Space: 15.1 MB (69.08%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,58 +1,34 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-      int n = nums.size();
-      int left = 0, right = n-1;
-      while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (nums[mid] == target) {
-          return mid;
-        }
-        if (nums[left] <= nums[mid]) { // <= 很重要 因為你是left+ (right -left) 靠左
-          if (nums[mid] > target && target >= nums[left]) {
-            right = mid - 1;
-          } else {
-            left = mid + 1;
-          }
+  int search(vector<int>& nums, int target) {
+    int left = 0, right = nums.size() - 1;
+
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      }
+      if (nums[left] <= nums[mid]) {
+        if (target < nums[mid] && nums[left] <= target) {
+          right = mid - 1;
         } else {
-          if (nums[mid] < target && target <= nums[right]) {
-            left = mid + 1;
-          } else {
-            right = mid - 1;
-          }
+          left = mid + 1;
+        }
+      } else {
+        if (nums[mid] < target  && target <= nums[right]) {
+          left = mid + 1;
+        } else {
+          right = mid - 1;
         }
       }
-      return -1;
     }
+    return -1;
+  }
 };
-
-
-
-
 
 /*
 
-1 2 3 4 5 6 7
+[4,5,6,7,0,1,2]
 
-5 6 7 1 2 3 4 
-
-
-5 6 7 1 2
-
-6 7 1 2 3
-
-XXXXXXXXXOOO
-        ^
-XXOOOOOOO
-       ^
-XXOOOOOOO
- ^      
-
-1. check if we are in rotate part
-  *  mid < right && mid > left  : normal
-  *  mid > right > abnormal
-  *  mid < left > abnormal
-
-2. shift according to 1
 
 */
