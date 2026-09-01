54b186d0bc586d60b110bbc4c496b5f2ea2e0025
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2026-09-02 02:31:51 +0800

    Time: 0 ms (100%), Space: 18.2 MB (56.39%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,46 +1,30 @@
 class Solution {
 public:
-
-    // ***************DFS******************
-    // vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-    //     if(image[sr][sc] == color) return image;
-    //     DFS(image, sr,sc,image.size(),image[0].size(),color,image[sr][sc]);
-    //     return image;
-
-    // }
-    // void DFS(vector<vector<int>>& image, int sr, int sc, int x,int y, int color, int originCol){
-    //     if(sr <0 || sc <0 || sr>=x || sc>=y) return;
-    //     if(image[sr][sc] == originCol) image[sr][sc] = color;
-    //     else return;
-    //     DFS(image, sr-1,sc,x,y,color,originCol);
-    //     DFS(image, sr,sc+1,x,y,color,originCol);
-    //     DFS(image, sr,sc-1,x,y,color,originCol);
-    //     DFS(image, sr+1,sc,x,y,color,originCol);
-    //     return;
-    // }
-
-    // ***************BFS******************
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        if(image[sr][sc] == color) return image;
-        int originalCol = image[sr][sc];
-        int x = image.size();
-        int y = image[0].size();
-        BFS(image,sr,sc,x,y,color,originalCol);
-        return image;
+  vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+    int ori = image[sr][sc];
+    if (ori == color) {
+      return image;
     }
-
-    void BFS (vector<vector<int>> &image,int sr,int sc,int x,int y,int color,int originalCol) {
-        queue<pair<int,int>> qu;
-        qu.push({sr,sc});
-        while(!qu.empty()) {
-            //pair<int,int> location = qu.front();
-            auto [r,c] = qu.front();
-            qu.pop();
-            image[r][c] = color;
-            if(r+1 < x && image[r+1][c] == originalCol) qu.push({r+1,c});
-            if(r-1 >= 0 && image[r-1][c] == originalCol) qu.push({r-1,c});
-            if(c+1 < y && image[r][c+1] == originalCol) qu.push({r,c+1});
-            if(c-1 >= 0 && image[r][c-1] == originalCol) qu.push({r,c-1});
-        }        
+    int height = image.size();
+    int width = image[0].size();
+    queue<pair<int,int>> qu;
+    qu.push({sr,sc});
+    image[sr][sc] = color;
+    while(!qu.empty()) {
+      auto [x, y] = qu.front();
+      qu.pop();
+      for (auto& [dx, dy] : dir) {
+        if (x + dx < 0 || x + dx >= height || y + dy < 0 || y + dy >= width) {
+          continue;
+        }
+        if (image[x + dx][y + dy] == ori) {
+          image[x + dx][y + dy] = color;
+          qu.push({x + dx, y + dy});
+        }
+      }
     }
+    return image;
+  }
+
+  vector<pair<int,int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 };
