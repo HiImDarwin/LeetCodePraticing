6de18cc504efa41f62cd8df501e4d69feffc7b10
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2026-09-02 02:38:52 +0800

    Time: 0 ms (100%), Space: 18 MB (90.02%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,30 +1,53 @@
 class Solution {
 public:
+  // vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+  //   int ori = image[sr][sc];
+  //   if (ori == color) {
+  //     return image;
+  //   }
+  //   int height = image.size();
+  //   int width = image[0].size();
+  //   queue<pair<int,int>> qu;
+  //   qu.push({sr,sc});
+  //   image[sr][sc] = color;
+  //   while(!qu.empty()) {
+  //     auto [x, y] = qu.front();
+  //     qu.pop();
+  //     for (auto& [dx, dy] : dir) {
+  //       if (x + dx < 0 || x + dx >= height || y + dy < 0 || y + dy >= width) {
+  //         continue;
+  //       }
+  //       if (image[x + dx][y + dy] == ori) {
+  //         image[x + dx][y + dy] = color;
+  //         qu.push({x + dx, y + dy});
+  //       }
+  //     }
+  //   }
+  //   return image;
+  // }
+
+  vector<pair<int,int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
   vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-    int ori = image[sr][sc];
-    if (ori == color) {
+    if (image[sr][sc] == color) {
       return image;
     }
-    int height = image.size();
-    int width = image[0].size();
-    queue<pair<int,int>> qu;
-    qu.push({sr,sc});
-    image[sr][sc] = color;
-    while(!qu.empty()) {
-      auto [x, y] = qu.front();
-      qu.pop();
-      for (auto& [dx, dy] : dir) {
-        if (x + dx < 0 || x + dx >= height || y + dy < 0 || y + dy >= width) {
-          continue;
-        }
-        if (image[x + dx][y + dy] == ori) {
-          image[x + dx][y + dy] = color;
-          qu.push({x + dx, y + dy});
-        }
-      }
-    }
+    dfs(image, sr, sc, image[sr][sc], color);
     return image;
   }
 
-  vector<pair<int,int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+  void dfs(vector<vector<int>>& image, int x, int y,int ori_color, int tar_color) {
+    if (image[x][y] != ori_color) {
+      return;
+    }
+    image[x][y] = tar_color;
+    for (auto& [dx, dy] : dir) {
+      int nX = x + dx;
+      int nY = y + dy;
+      if (nX < 0 || nX >= image.size() || nY < 0 || nY >= image[0].size()) {
+        continue;
+      }
+      dfs(image, nX, nY, ori_color, tar_color);
+    }
+  }
 };
