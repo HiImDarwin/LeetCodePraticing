c334a7d32c35cedfdacaa563a8a97989519afdd6
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2026-09-02 20:38:32 +0800

    Time: 20 ms (49.64%), Space: 34.7 MB (86.04%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,30 +1,38 @@
 class Solution {
-  public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-      int m = mat.size(), n = mat[0].size();
-      vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
-      queue<pair<int,int>> qu;
-      for(int i = 0; i < m; ++i) {
-        for(int j = 0; j < n; ++j) {
-          if(mat[i][j] == 0) {
-            dist[i][j] = 0;
-            qu.push({i,j});
-          }
+public:
+  vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+    int m = mat.size();
+    int n = mat[0].size();
+    queue<pair<int,int>> qu;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (mat[i][j] == 0) {
+          qu.push({i, j});
+        } else {
+          mat[i][j] = -1;
         }
       }
-
-      while (!qu.empty()) {
-        auto [x,y] = qu.front(); qu.pop();
-        for(auto [dx,dy] : directions) {
-          int neiX = x + dx, neiY = y + dy;
-          if(neiX < 0 || neiX >= m || neiY < 0 || neiY >= n || dist[neiX][neiY]!=INT_MAX) continue;
-          dist[neiX][neiY] = dist[x][y]+1;
-          qu.push({neiX, neiY});
-        }
-      }
-      
-      return dist;
     }
-  private:
-    const vector<pair<int,int>> directions = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    while (!qu.empty()) {
+      int round = qu.size();
+      while (round > 0) {
+        auto [x, y] = qu.front();
+        qu.pop();
+        round--;
+        for (auto [dx, dy] : dir) {
+          int nx = x + dx;
+          int ny = y + dy;
+          if (nx < 0 || ny < 0 || nx >= m || ny >= n || mat[nx][ny] != -1) {
+            continue;
+          }
+          mat[nx][ny] = mat[x][y] + 1;
+          qu.push({nx, ny});
+        }
+      }
+    }
+    return mat; 
+  }
+
+  vector<pair<int,int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 };
