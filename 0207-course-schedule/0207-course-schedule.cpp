f5f14682d0001d8e1d05a4da7cf9588d7e7df79e
李霖 <53218645+HiImDarwin@users.noreply.github.com>
2026-09-02 22:16:42 +0800

    Time: 6 ms (50.94%), Space: 20.9 MB (8.65%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,59 +1,33 @@
 class Solution {
 public:
-    //******************************BFS***********************************
-    // bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-    //     vector<int> indegree(numCourses,0);
-    //     vector<vector<int>> adj(numCourses);
-        
-    //     for(auto &link:prerequisites) {
-    //         indegree[link[0]]++;
-    //         adj[link[1]].push_back(link[0]);
-    //     }
-        
-    //     queue<int> q;
-    //     for(int i =0; i< numCourses; ++i){
-    //         if(indegree[i] == 0) q.push(i);
-    //     }
-    //     int count=0;
-    //     while(!q.empty()){
-    //         int u = q.front();
-    //         q.pop();
-    //         count++;
-    //         for(auto &x: adj[u]){
-    //             if(--indegree[x] == 0){
-    //                 q.push(x);
-    //             }
-    //         }
-    //     }
-    //     return count==numCourses;
-    // }
-    //
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        vector<bool> visited(numCourses,false);
-        for(auto& link: prerequisites) {
-            adj[link[1]].push_back(link[0]);
+  bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<int> course(numCourses, 0);
+    queue<int>qu;
+    unordered_map<int, vector<int>> adj;
+    for(auto vec : prerequisites) {
+      course[vec[1]]++;
+      adj[vec[0]].push_back(vec[1]);
+    }
+    for (int i = 0; i < numCourses; i++) {
+      if (course[i] == 0) {
+        qu.push(i);
+      }
+    }
+    while(!qu.empty()) {
+      int cur = qu.front();
+      qu.pop();
+      numCourses--;
+      if (adj.find(cur) == adj.end()) {
+        continue;
+      }
+      for(auto &nei : adj[cur]) {
+        course[nei]--;
+        if (course[nei] == 0) {
+          qu.push(nei);
         }
-        vector<bool> onPath(numCourses, false); //this is for checking cycle
-        for(int i=0; i<numCourses;i++){
-            if(!visited[i]) {
-                if(hasCycle(i, adj, visited, onPath)) return false;
-            }
-        }
-        return true;
+      }
     }
 
-    bool hasCycle(int u, vector<vector<int>>& adj, vector<bool>& visited, vector<bool>& onPath) {
-        visited[u] = true;
-        onPath[u] = true;
-        for (int v: adj[u]){
-            if(!visited[v]) {
-                if(hasCycle(v,adj,visited,onPath)) return true;
-            } else if ( onPath[v]) {
-                return true;
-            }
-        }
-        onPath[u] = false;
-        return false;
-    }
+    return numCourses == 0 ? true : false;
+  }
 };
